aa7494abb50f6d0f9593a3f7d448be3aebb9cd51
Dominik Adam Przybysz <alien11689@gmail.com>
2016-12-11 17:58:16 +0100

    Some fixes

--- 2016/day11/part2.groovy
+++ 2016/day11/part2.groovy
@@ -1,4 +1,4 @@
-def main(List<Set<Object>> floors) {
+def main(List<Set<Integer>> floors) {
     def memory = [] as HashSet
     Queue<Move> q = new LinkedList()
     def first = new Move(0, new Stage(0, floors))
@@ -20,7 +20,7 @@ def main(List<Set<Object>> floors) {
     return null
 }
 
-def generateMoves(int elevator, List<Set<Object>> floors, def memory){
+def generateMoves(int elevator, List<Set<Integer>> floors, def memory){
     def currentFloor = floors[elevator]
     def possibleMoves = [currentFloor, currentFloor].combinations()
         .collect {it as Set}
@@ -34,18 +34,25 @@ def generateMoves(int elevator, List<Set<Object>> floors, def memory){
                 null
             }
         }.findAll {it}
-        .collectMany { m ->
-            [elevator + 1, elevator - 1].findAll {it in (0..<4)}.collect { e -> 
+        .collect { m ->
+            def newE
+            if(m.size() == 2){
+                newE = elevator + 1
+            }else {
+                newE = elevator - 1
+            }
+            if(newE in (0..<4) && (newE != 0 || !floors[0].empty) && (!(newE == 1 && floors[0].empty &&floors[1].empty)) ){
                 List<Set<String>> newFloors = floors.collect { it.collect {it} as Set}
-                newFloors[e] = newFloors[e] + m
+                newFloors[newE] = newFloors[newE] + m
                 newFloors[elevator] = newFloors[elevator] - m
-                if(isValid(newFloors[e])){
-                    def st = new Stage(e, newFloors)
+                if(isValid(newFloors[newE])){
+                    def st = new Stage(newE, newFloors)
                     st in memory ? null : st
                 }else {
                     null
                 }
-            
+            }else {
+                null
             }
         }.findAll()
 }
@@ -53,7 +60,7 @@ def generateMoves(int elevator, List<Set<Object>> floors, def memory){
 @groovy.transform.Immutable
 class Stage {
     int e
-    List<Set<Object>> floors
+    List<Set<Integer>> floors
 }
 
 @groovy.transform.Immutable
@@ -62,10 +69,11 @@ class Move {
     Stage stage
 }
 
+@groovy.transform.Memoized
 def isValid(Set<Objects> floor){
-    def gs = floor.findAll {it instanceof  G }
-    gs.empty || floor.findAll {it instanceof M}.every {m ->
-        gs.any {g -> g.type == m.type}
+    def gs = floor.findAll {it > 0 }
+    gs.empty || floor.findAll {it < 0 }.every {m ->
+        gs.any {g -> m == -g}
     }
 }
 
@@ -73,30 +81,10 @@ def isFinished(List floors){
     floors[0..2].every {it.empty}
 }
 
-
-@groovy.transform.Immutable
-class M {
-    String type
-}
-@groovy.transform.Immutable
-class G {
-    String type
-}
-
 println(main(
     [
-        [new M('H'), new M('L')] as Set,
-        [new G('H')] as Set,
-        [new G('L')] as Set,
-        [] as Set
-    ]
-))
-println()
-
-println(main(
-    [
-        [new G('Po'),new  G ('T'),new M ('T'),new G ('Pr'),new G ('R'),new M ('R'),new G ('C'),new M ('C'), new G('E'), new M('E'), new G('D'), new M('D') ]  as Set,
-        [new M ('Po'),new M ('Pr')] as Set,
+        [1,2,-2,3,4,-4,5,-5,6,-6,7,-7]  as Set,
+        [-1,-3] as Set,
         [] as Set,
         [] as Set,
     ]
