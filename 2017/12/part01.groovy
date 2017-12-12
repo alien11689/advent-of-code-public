b5a300eca76ff0e876492bae339c787c1441f5fd
Dominik Przybysz <dpr@touk.pl>
2017-12-12 07:02:50 +0100

    Working part 1 of 12/2017

--- 2017/12/part01.groovy
+++ 2017/12/part01.groovy
@@ -2,12 +2,30 @@ String text = new File('input.txt').text.trim()
 
 List routesDef = text.split('\n').collect {it.trim()}
 
-Set routes = [] as Set
+List routes = []
 
 routesDef.each {
 	List parts = it.split(' ', 3)
 	int from = parts[0] as int
 	List tos = parts[2].split(',').collect {it.trim() as int}
-	println "From $from and to $tos "
+	tos.each {
+		routes << [from, it]
+	}
 }
+
+Set know0 = [0]
+Set used = [] as Set
+boolean search = true
+
+while(search) {
+	search = false
+	routes.findAll{!(it in used)}.each {
+		if(it[0] in know0 || it[1] in know0){
+			search = true
+			used << it
+			know0.addAll(it)
+		}
+	}
+}
+println know0.size()
 
