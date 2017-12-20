3f8ab94f2ca12584ca975b48213b073e2a831063
Dominik Przybysz <dpr@touk.pl>
2017-12-20 07:07:08 +0100

    Part1 from 20/2017

--- 2017/20/part1.groovy
+++ 2017/20/part1.groovy
@@ -9,9 +9,28 @@ class Coord {
 
 @groovy.transform.ToString
 class Particle {
+	int id
 	Coord p
 	Coord v
 	Coord a
+
+	def updateV(){
+		v = new Coord(
+			x: v.x + a.x,
+			y: v.y + a.y,
+			z: v.z + a.z,
+			)
+	}
+	def updateP(){
+		p = new Coord(
+			x: v.x + p.x,
+			y: v.y + p.y,
+			z: v.z + p.z,
+			)
+	}
+	def from0(){
+		return p.x.abs() + p.y.abs() + p.z.abs()
+	}
 }
 
 Coord buildCoords(str){
@@ -19,9 +38,11 @@ Coord buildCoords(str){
 	new Coord(x: parts[0] as int, y: parts[1] as int , z: parts[2] as int)
 }
 
+int id = 0
 def particles = lines.collect { line ->
 	List parts = line.split('[<>]')
 	new Particle(
+		id: id++,
 		p: buildCoords(parts[1]),
 		v: buildCoords(parts[3]),
 		a: buildCoords(parts[5])
@@ -30,4 +51,12 @@ def particles = lines.collect { line ->
 
 particles.each {
 	println it
+}
+
+1000.times {iter ->
+	print "In iter $iter -> "
+	println (particles.each {
+		it.updateV()
+		it.updateP()
+	}.min { it.from0() }.id)
 }
