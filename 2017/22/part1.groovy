f93bde695720c07ea55280f4283c3e20d448d311
Dominik Przybysz <dpr@touk.pl>
2017-12-22 07:17:42 +0100

    Part 1 of 22/2017

--- 2017/22/part1.groovy
+++ 2017/22/part1.groovy
@@ -1,4 +1,5 @@
 String text = new File('input.txt').text
+//text = new File('input2.txt').text
 
 List lines = text.split('\n').collect {it.trim()}
 
@@ -13,11 +14,72 @@ enum Dir {
 		x = xx
 		y = yy
 	}
+
+	def left(){
+		switch(this) {
+			case Left: return Down
+			case Up: return Left
+			case Right: return Up
+			case Down: return Right
+		}
+	}
+
+	def right(){
+		switch(this) {
+			case Left: return Up
+			case Up: return Right
+			case Right: return Down
+			case Down: return Left
+		}
+	}
 }
 
 Map grid = [:]
-int curPost = (lines[0].size() + 1) / 2
 
-lines.each {
-	
+@groovy.transform.ToString
+class Virus {
+	int x
+	int y
+	Dir dir = Dir.Up
+	int infected = 0
+
+	Virus(xx,yy) {
+		x = xx
+		y = yy
+	}
+
+	def burst(grid) {
+		Boolean value = grid[[x,y]]
+		dir = value ? dir.right() : dir.left()
+		grid[[x,y]] = !value
+		if(grid[[x,y]]) {
+			infected++
+		}
+		x += dir.x
+		y += dir.y
+	}
 }
+
+int curX = (lines[0].size()) / 2
+int curY = (lines.size()) / 2
+
+int j = 0
+lines.each { row ->
+	int i = 0
+	(row as List).each { cell ->
+		if(cell == '#'){
+			grid[[i, j]] = true
+		}
+		++i	
+	}
+	++j
+		
+}
+
+def virus = new Virus(curX, curY)
+println virus
+10000.times {
+	virus.burst(grid)
+}
+
+println virus.infected
