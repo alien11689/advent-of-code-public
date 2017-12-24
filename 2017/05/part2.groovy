af8afae95742f89726185463cc91479bb489c16e
Dominik Adam Przybysz <alien11689@gmail.com>
2017-12-24 12:39:35 +0100

    Optimiz 5 ad 13

--- 2017/05/part2.groovy
+++ 2017/05/part2.groovy
@@ -5,7 +5,7 @@ List steps = text.split('\n').collect  {it.trim() as int}
 int cur = 0
 int jumps = 0
 while(cur < steps.size()){
-	println "$cur/${steps.size()}"
+//	println "$cur/${steps.size()}"
 	jumps++
 	int val = steps[cur]
 	steps[cur] = val >= 3 ? (val-1) : (val + 1)
