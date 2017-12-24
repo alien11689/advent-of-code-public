75a943378d9cc16c8245a6679d7f0f8c4aad213d
Dominik Adam Przybysz <alien11689@gmail.com>
2017-12-24 10:44:58 +0100

    Add 24/2017

---
+++ 2017/24/part1.groovy
@@ -0,0 +1,51 @@
+String input = new File('input.txt').text
+//input = new File('input2.txt').text
+
+List lines = input.split('\n').collect{it.trim()}
+
+@groovy.transform.Immutable
+class Cpu {
+	List<Integer> pins
+}
+
+@groovy.transform.Immutable
+class Chain {
+	List<Cpu> cpus
+	int lastPin
+
+	Chain connectTo(Cpu c){
+		if(c in cpus){
+			return null
+		}
+		if(!(lastPin in c.pins)){
+			return null
+		}
+		return new Chain(cpus + [c], lastPin == c.pins[0]? c.pins[1] : c.pins[0])
+	}
+
+	int length(){
+		return cpus.collectMany {it.pins}.sum()
+	}
+}
+
+List cpus = lines.collect { 
+	new Cpu(it.split('/').collect {it as int})
+}
+
+List chains = []
+Stack stack = new Stack()
+stack.push(new Chain([], 0))
+
+while(!stack.empty){
+	println "Stack size: ${stack.size()}"
+	Chain parent = stack.pop()
+	List newChains = cpus.collect {
+		parent.connectTo(it)
+	}.findAll()
+	chains.addAll newChains
+	newChains.each {
+		stack.push(it)
+	}
+}
+
+println chains.collect {it.length()}.max()
