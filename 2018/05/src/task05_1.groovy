44da65dc4a8ef4316ef3f4f040de53ba17f2f3d8
Dominik Adam Przybysz <alien11689@gmail.com>
2018-12-05 07:05:30 +0100

    05/2018

--- 2018/05/src/task05_1.groovy
+++ 2018/05/src/task05_1.groovy
@@ -1,6 +1,20 @@
-import groovy.transform.ToString
+def text = new File('input.txt').text.trim()
 
-def text = new File('input.txt').text
+def before = text
+def iteration = 0
+def curSize = text.size()
+def nextSize = 0
 
-def lines = text.split('\n')
+def changes = ('a'..'z').collectMany{[/$it${it.toUpperCase()}/, /${it.toUpperCase()}$it/]}
 
+while (curSize != nextSize) {
+    curSize = nextSize
+    println("Iteration: ${iteration++} - size ${curSize}")
+    changes.each{
+        before = before.replaceAll(it, '')
+    }
+    nextSize = before.size()
+}
+
+println(before)
+println(nextSize)
