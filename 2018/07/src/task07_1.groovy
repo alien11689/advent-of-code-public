f67e5dd3057b9ee4dd4f730be98868a0c1d3371f
Dominik Adam Przybysz <alien11689@gmail.com>
2018-12-07 07:00:09 +0100

    07/2018

--- 2018/07/src/task07_1.groovy
+++ 2018/07/src/task07_1.groovy
@@ -4,3 +4,45 @@ def text = new File('input.txt').text.trim()
 
 def lines = text.split('\n')
 
+@ToString
+class Instr {
+    String dest
+    TreeSet<String> before = [] as Set
+}
+
+def instructions = (lines.collect { it.split(' ')[7] } as Set).collect { new Instr(dest: it) }
+
+lines.each { l ->
+    String[] split = l.split(' ')
+    String dest = split[7]
+    String from = split[1]
+    Instr instr = instructions.find { it.dest == dest }
+    instr.before << from
+}
+
+
+
+(instructions.before.flatten() as Set).each { letter ->
+    if (instructions.find { it.dest == letter }) {
+        //nothing
+    } else {
+        instructions << new Instr(dest: letter)
+    }
+}
+
+println(instructions)
+
+List order = []
+
+while (!instructions.empty) {
+    println(order)
+    Instr next = instructions.findAll { it.before == [] as Set }.sort { it.dest }.first()
+    String letter = next.dest
+    order << letter
+    instructions -= next
+    instructions.findAll { it.before.contains(letter) }.each {
+        it.before.remove(letter)
+    }
+}
+
+println(order.join())
---
+++ 2018/08/src/task08_1.groovy
@@ -0,0 +1,4 @@
+def text = new File('input.txt').text.trim()
+
+def lines = text.split('\n')
+
