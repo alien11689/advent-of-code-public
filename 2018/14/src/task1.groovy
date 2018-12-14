5f3d0104ddfc01ff114756fa6fedfcc37e3a4426
Dominik Przybysz <dominik.przybysz@allegro.pl>
2018-12-14 07:07:35 +0100

    14/2018

--- 2018/14/src/task1.groovy
+++ 2018/14/src/task1.groovy
@@ -1,5 +1,26 @@
-def text = new File('input.txt').text
-//text = new File('other.txt').text
+def input = 640441
+//input = 2018
 
-def lines = text.split('\n')
+List<Integer> scores = [3, 7]
 
+int steps = 9
+
+def elves = [0, 1]
+
+int curStep = 0
+
+while (scores.size() < input + steps + 1) {
+//    println(elves)
+//    println(scores)
+//    println('----------')
+    if(scores.size() % 1000 == 0){
+        println(scores.size())
+    }
+    int newValue = scores[elves[0]] + scores[elves[1]]
+    scores.addAll(newValue.toString().collect { it as int })
+    elves[0] = (elves[0] + scores[elves[0]] + 1) % scores.size()
+    elves[1] = (elves[1] + scores[elves[1]] + 1) % scores.size()
+}
+
+println(scores)
+println(scores.subList(input, input + steps + 1).join())
