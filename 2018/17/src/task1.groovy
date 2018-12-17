df73b42bd3a23419a1aa8471bac1eefd9ae84e73
Dominik Przybysz <dominik.przybysz@allegro.pl>
2018-12-17 07:20:31 +0100

    Begin 17/2018

--- 2018/17/src/task1.groovy
+++ 2018/17/src/task1.groovy
@@ -9,7 +9,7 @@ static List<String> readInputAsLines(String file) {
 }
 
 List<String> lines = readInputAsLines('input.txt')
-lines = readInputAsLines('other.txt')
+//lines = readInputAsLines('other.txt')
 
 @Canonical
 class Clay {
@@ -45,18 +45,9 @@ List<Clay> clays = lines.collect { line ->
     new Clay(fromX, toX, fromY, toY)
 }
 
-Set<List<Integer>> claysSet = [] as Set
-for (int j = clays.fromY.min(); j <= clays.toY.max(); ++j) {
-    for (int i = clays.fromX.min(); i <= clays.toX.max(); ++i) {
-        if (clays.any { it.contains(i, j) }) {
-            claysSet << [i, j]
-        }
-    }
-}
-
 def printClays(List<Clay> clays, Set<List<Integer>> claysSet, Set<List<Integer>> water) {
     for (int j = clays.fromY.min(); j <= clays.toY.max(); ++j) {
-        for (int i = clays.fromX.min(); i <= clays.toX.max(); ++i) {
+        for (int i = clays.fromX.min() - 1; i <= clays.toX.max() + 1; ++i) {
             if (water.contains([i, j])) {
                 print('~')
             } else if (isClay(claysSet, i, j)) {
@@ -79,51 +70,123 @@ boolean isFilled(filled, x, y) {
 
 enum ActionType {
     GoUp,
-    LeakLeft,
-    LeakRight
+    Leak,
+    Nop
 }
 
+@Canonical
 class Action {
-    int curX
     ActionType type
+    int curX
 }
 
 int maxCount = 0
 
+falls = [] as Set
+
 def goDown(claysSet, filled, x, y) {
-    int maxY = claysSet.collect {it[1]}.max()
+//    println("Cur [$x,$y]")
+    int maxY = claysSet.collect { it[1] }.max()
     while (!isClay(claysSet, x, y + 1) && y + 1 <= maxY) {
         ++y
         filled << [x, y]
     }
+    if (y + 1 > maxY) {
+//        println("Reaching end $x")
+        return
+    }
+//    if (isFilled(filled, x - 1, y) || isFilled(filled, x + 1, y)) {
+//        return
+//    }
     //try to fill
-    boolean boundLeft = fillLeft(x, claysSet, y, filled)
-    boolean boundRight = fillRight(x, claysSet, y, filled)
-    while (boundLeft && boundRight) {
+//    println("In res [$x, $y]")
+//    int i = x
+//    while (!isClay(claysSet, i - 1, y)) {
+//        --x
+//    }
+//    int j = y
+//    while (isClay(claysSet, i - 1, j)) {
+//        --j
+//    }
+    Action boundLeft = fillLeft(x, claysSet, y, filled)
+    Action boundRight = fillRight(x, claysSet, y, filled)
+    while (boundLeft.type == ActionType.GoUp && boundRight.type == ActionType.GoUp) {
         --y
         boundLeft = fillLeft(x, claysSet, y, filled)
         boundRight = fillRight(x, claysSet, y, filled)
     }
-    if (boundLeft) {
-//        TODO find nearest right
-        int cur = x + 1
-        while (isClay(claysSet, cur, y + 1) || isFilled(filled, cur, y + 1)) {
-            ++cur
-        }
-        println("Going donw on $cur")
+    if (boundLeft.type == ActionType.Leak) {
+//        println("Leaking left $boundLeft.curX")
+        goDown(claysSet, filled, boundLeft.curX, y)
     }
-
-    if (boundRight) {
-//        TODO find nearest right
-        int cur = x - 1
-        while (isClay(claysSet, cur, y + 1) || isFilled(filled, cur, y + 1)) {
-            ++cur
-        }
-        println("Going donw on $cur")
+    if (boundRight.type == ActionType.Leak) {
+//        println("Leaking right $boundRight.curX")
+        goDown(claysSet, filled, boundRight.curX, y)
     }
 }
 
-for (int x = clays.fromX.min(); x <= clays.toX.max(); ++x) {
+private Action fillRight(int x, Set claysSet, int y, Set filled) {
+//    println("Filling right")
+    int cur = x
+    while (true) {
+        int nextX = cur + 1
+        if (isClay(claysSet, nextX, y)) {
+//            println("Next is clay")
+            return new Action(ActionType.GoUp)
+        }
+        filled << [nextX, y]
+        boolean nextIsClay = isClay(claysSet, nextX, y + 1)
+        boolean nextIsFilled = filled.contains([nextX, y + 1])
+        if (!nextIsClay && !nextIsFilled) {
+//            println("Falling down on right")
+            return new Action(ActionType.Leak, nextX)
+//        } else if (nextIsClay && filled.contains([nextX + 1, y + 1])) {
+////            println("Falling down on right")
+//            return new Action(ActionType.Nop)
+        }
+        ++cur
+        continue
+    }
+}
+
+private Action fillLeft(int x, Set claysSet, int y, Set filled) {
+    int cur = x
+    while (true) {
+        int prevX = cur - 1
+        if (isClay(claysSet, prevX, y)) {
+//            println("Prev is clay")
+            return new Action(ActionType.GoUp)
+        }
+        filled << [prevX, y]
+        boolean prevIsClay = isClay(claysSet, prevX, y + 1)
+        boolean prevIsFilled = filled.contains([prevX, y + 1])
+        if (!prevIsClay && !prevIsFilled) {
+//            println('Falling down on left')
+            return new Action(ActionType.Leak, prevX)
+//        } else if (prevIsClay && filled.contains([prevX - 1, y + 1])) {
+////            println("Falling down on right")
+//            return new Action(ActionType.Nop)
+        }
+        --cur
+        continue
+    }
+}
+
+
+println("Reading clays")
+Set<List<Integer>> claysSet = [] as Set
+clays.each {
+    for (int j = it.fromY; j <= it.toY; ++j) {
+        for (int i = it.fromX; i <= it.toX; ++i) {
+            claysSet << [i, j]
+        }
+    }
+}
+
+println("Calculating")
+//for (int x = clays.fromX.min() - 1; x <= clays.toX.max() + 1; ++x) {
+for (int x = 500; x <= 500; ++x) {
+    println("Checking $x")
     Set<List<Integer>> filled = [] as Set
     int y = clays.fromY.min() - 1
     goDown(claysSet, filled, x, y)
@@ -131,52 +194,7 @@ for (int x = clays.fromX.min(); x <= clays.toX.max(); ++x) {
     if (filled.size() > maxCount) {
         maxCount = filled.size()
     }
+    println("Current max is $maxCount")
     println('------------')
 }
 println(maxCount)
-
-private boolean fillRight(int x, Set claysSet, int y, Set filled) {
-    println("Filling right")
-    boolean boundRight = false
-    int cur = x
-    while (true) {
-//        println(filled)
-        int nextX = cur + 1
-        if (isClay(claysSet, nextX, y)) {
-            println("Next is clay")
-            boundRight = true
-            break
-        }
-        filled << [nextX, y]
-        if (!isClay(claysSet, nextX, y + 1) && !filled.contains([nextX, y + 1])) {
-            println("Falling down")
-            // TODO fall down
-            break
-        }
-        ++cur
-    }
-    boundRight
-}
-
-private boolean fillLeft(int x, Set claysSet, int y, Set filled) {
-    println("Filling left")
-    int cur = x
-    boolean boundLeft = false
-    while (true) {
-//        println(filled)
-        int prevX = cur - 1
-        if (isClay(claysSet, prevX, y)) {
-            println("Prev is clay")
-            boundLeft = true
-            break
-        }
-        filled << [prevX, y]
-        if (!isClay(claysSet, prevX, y + 1) && !filled.contains([prevX, y + 1])) {
-            println('Falling down')
-            // TODO fall down
-            break
-        }
-        --cur
-    }
-    boundLeft
-}
