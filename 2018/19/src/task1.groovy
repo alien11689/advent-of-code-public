5b5347b3d7a9711c8a942f31a0c71f87ea6ebaee
Dominik Przybysz <dominik.przybysz@allegro.pl>
2018-12-18 06:31:38 +0100

    Prepare for 19/2018

---
+++ 2018/19/src/task1.groovy
@@ -0,0 +1,11 @@
+static String readInput(String file) {
+    return new File(file).text
+}
+
+static List<String> readInputAsLines(String file) {
+    return readInput(file).trim().split('\n')
+}
+
+List<String> lines = readInputAsLines('input.txt')
+//lines = readInputAsLines('other.txt')
+
