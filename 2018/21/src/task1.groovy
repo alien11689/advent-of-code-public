0140d8a3f0b8ad6644b4e8dba084e6abafadebd0
Dominik Przybysz <dominik.przybysz@allegro.pl>
2018-12-21 08:26:31 +0100

    part 1 of 21/2018

--- 2018/21/src/task1.groovy
+++ 2018/21/src/task1.groovy
@@ -8,4 +8,190 @@ static List<String> readInputAsLines(String file) {
     return readInput(file).trim().split('\n')
 }
 
-String input = readInput('input.txt').trim()
+List<String> lines = readInputAsLines('input.txt')
+//lines = readInputAsLines('other.txt')
+
+@Canonical
+class Operation {
+    String name
+    int a
+    int b
+    int c
+
+    void apply(long[] registers) {
+        switch (name) {
+            case 'addr':
+                registers[c] = registers[a] + registers[b]
+                break
+            case 'addi':
+                registers[c] = registers[a] + b
+                break
+            case 'setr':
+                registers[c] = registers[a]
+                break
+            case 'seti':
+                registers[c] = a
+                break
+            case 'mulr':
+                registers[c] = registers[a] * registers[b]
+                break
+            case 'muli':
+                registers[c] = registers[a] * b
+                break
+            case 'banr':
+                registers[c] = registers[a] & registers[b]
+                break
+            case 'bani':
+                registers[c] = registers[a] & b
+                break
+            case 'borr':
+                registers[c] = registers[a] | registers[b]
+                break
+            case 'bori':
+                registers[c] = registers[a] | b
+                break
+            case 'gtir':
+                registers[c] = a > registers[b] ? 1 : 0
+                break
+            case 'gtri':
+                registers[c] = registers[a] > b ? 1 : 0
+                break
+            case 'gtrr':
+                registers[c] = registers[a] > registers[b] ? 1 : 0
+                break
+            case 'eqir':
+                registers[c] = a == registers[b] ? 1 : 0
+                break
+            case 'eqri':
+                registers[c] = registers[a] == b ? 1 : 0
+                break
+            case 'eqrr':
+                registers[c] = registers[a] == registers[b] ? 1 : 0
+                break
+        }
+    }
+
+    String toString() {
+        switch (name) {
+            case 'addr':
+                return "r$c = r$a + r$b"
+            case 'addi':
+                return "r$c = r$a + $b"
+            case 'setr':
+                return "r$c = r$a"
+            case 'seti':
+                return "r$c = $a"
+            case 'mulr':
+                return "r$c = r$a * r$b"
+            case 'muli':
+                return "r$c = r$a * $b"
+            case 'banr':
+                return "r$c = r$a & r$b"
+            case 'bani':
+                return "r$c = r$a & $b"
+            case 'borr':
+                return "r$c = r$a | r$b"
+            case 'bori':
+                return "r$c = r$a | $b"
+            case 'gtir':
+                return "r$c = $a > r$b? 1 : 0"
+            case 'gtri':
+                return "r$c = r$a > $b? 1 : 0"
+            case 'gtrr':
+                return "r$c = r$a > r$b? 1 : 0"
+            case 'eqir':
+                return "r$c = $a == r$b? 1 : 0"
+            case 'eqri':
+                return "r$c = r$a == $b? 1 : 0"
+            case 'eqrr':
+                return "r$c = r$a == r$b? 1 : 0"
+        }
+    }
+}
+
+int ip = lines[0].split(/ /)[-1] as int
+List<Operation> instructions = []
+for (int i = 1; i < lines.size(); ++i) {
+    String[] line = lines[i].split(/ /)
+    instructions << new Operation(name: line[0], a: line[1] as int, b: line[2] as int, c: line[3] as int)
+}
+
+instructions.eachWithIndex { it, i -> println("$i: $it") }
+
+int threshold = 10000
+int curMinR4 = 10000000000
+long operations = 0
+for (int i = 0; i < 1; ++i) {
+    println(i)
+    long[] registers = [i, 0, 0, 0, 0, 0, 0]
+    def count
+    Map<Integer, Integer> instrToCount = [:].withDefault { 0 }
+    while (true) {
+        ++operations
+        int cur = registers[6]
+//        println("$cur: ${instructions[cur]}")
+        registers[ip] = registers[6]
+//        print("Before $registers || ")
+//        if (cur == 20 && registers[2] <= registers[3]) {
+//            while ((registers[5] + 1) * 256 <= registers[3]) {
+//                ++registers[5]
+//            }
+//        }
+        instrToCount[cur]++
+        instructions[cur].apply(registers)
+//        println(registers)
+        if (cur == 28 && curMinR4 > registers[4]) {
+            curMinR4 = registers[4]
+            println("In iteration ${instrToCount[28]} min is $curMinR4")
+            return
+        }
+        if (registers[ip] + 1 >= instructions.size()) {
+
+            throw new RuntimeException("WIn $i")
+        }
+        registers[6] = registers[ip] + 1
+        count = instrToCount[28] > threshold
+        if (count) {
+            println("Break $i: is in loop")
+            break
+        }
+    }
+}
+//
+//println("Checking nums:")
+//List<Integer> nums = r4s.sort()
+//threshold = 100
+//println(nums)
+//for (int i : nums) {
+//    println(i)
+//    int[] registers = [i, 0, 0, 0, 0, 0, 0]
+//    def count
+//    Map<Integer, Integer> instrToCount = [:].withDefault { 0 }
+//    while (true) {
+//        int cur = registers[6]
+//        registers[ip] = registers[6]
+//        if (cur == 28) {
+////            r4s << registers[4]
+////            println(instructions[cur])
+////            print("Before $registers || ")
+//        }
+//        instrToCount[cur]++
+//        instructions[cur].apply(registers)
+//        if (cur == 28) {
+////            println(registers)
+//        }
+//        if (registers[ip] + 1 >= instructions.size()) {
+//            break
+//        }
+//        registers[6] = registers[ip] + 1
+//        count = instrToCount[28] > threshold
+//        if (count) {
+//            println("Break $i: is in loop")
+//            break
+//        }
+//    }
+//    if (!count) {
+//        break
+//    }
+//}
+//println(r4s.min())
