e33891f9b3557a38e5b6670e2e01e2b24885e760
Dominik Adam Przybysz <alien11689@gmail.com>
2018-12-23 06:07:52 +0100

    part 1 of 23/2018

--- 2018/23/src/task1.groovy
+++ 2018/23/src/task1.groovy
@@ -8,4 +8,26 @@ static List<String> readInputAsLines(String file) {
     return readInput(file).trim().split('\n')
 }
 
-String input = readInput('input.txt').trim()
+List<String> input = readInputAsLines('input.txt')
+//input = readInputAsLines('other.txt')
+
+@Canonical
+class Drone {
+    int x
+    int y
+    int z
+
+    int range
+
+    int manhattan(Drone o) {
+        Math.abs(x - o.x) + Math.abs(y - o.y) + Math.abs(z - o.z)
+    }
+}
+
+List<Drone> drones = input.collect { it.split(/[<>,=]+/) }.collect {
+    new Drone(it[1] as int, it[2] as int, it[3] as int, it[5] as int)
+}
+
+Drone max = drones.max { it.range }
+
+println(drones.count {it.manhattan(max) <= max.range})
