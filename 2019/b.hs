e4028971e8531ec16b8c5edee92f54ed19f0384a
Dominik Adam Przybysz <alien11689@gmail.com>
2019-12-04 20:54:04 +0100

    04/2019

---
+++ 2019/b.hs
@@ -0,0 +1,25 @@
+import qualified Data.Map.Strict as M
+import qualified Data.Char as C
+
+doesNotDecrease :: String -> Bool
+doesNotDecrease (x:[]) = True
+doesNotDecrease (x:y:xs) = C.digitToInt x <= C.digitToInt y && doesNotDecrease (y:xs)
+
+buildMap [] m = m
+buildMap (x:xs) m = 
+    if M.member x m 
+        then buildMap xs $ M.adjust succ x m
+        else buildMap xs $ M.insert x 1 m
+
+hasRepetition :: String -> Bool
+hasRepetition input = not $ null $ filter (\x -> snd x == 2) $ M.toList $ buildMap input M.empty
+
+isPassword:: Int -> Bool
+isPassword input = doesNotDecrease (show input) && hasRepetition (show input)
+
+findMatching:: Int
+findMatching =  length $ filter isPassword [156218..652527]
+
+main = do
+    print (findMatching)
+
