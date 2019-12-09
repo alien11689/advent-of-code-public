beebfd098059d0c388ba72bddb1017f697ad9863
Dominik Adam Przybysz <alien11689@gmail.com>
2019-12-09 20:10:27 +0100

    Optimize

--- 2019/09/a.groovy
+++ 2019/09/a.groovy
@@ -4,9 +4,7 @@ for (long i = 0; i < input.size(); ++i) {
     v[i] = input[i]
 }
 
-rel = 0
-
-long param(Map<Long, Long> v, long pos, int mode) {
+long param(Map<Long, Long> v, long pos, int mode, long rel) {
     if (mode == 0) {
         return v[v[pos]]
     } else if (mode == 1) {
@@ -28,7 +26,7 @@ int p3Mode(int op) {
     return ((op / 10000) as int) % 10
 }
 
-void assignTo(Map<Long, Long> v, long pos, int mode, long value) {
+void assignTo(Map<Long, Long> v, long pos, int mode, long value, long rel) {
     switch (mode) {
         case 0: v[v[pos]] = value; break
         case 1: v[pos] = value; break
@@ -39,6 +37,7 @@ void assignTo(Map<Long, Long> v, long pos, int mode, long value) {
 def program(State s, Queue output) {
     Map<Long, Long> v = s.v
     long pos = s.pos
+    long rel = 0
 
     while (true) {
         int op = v[pos]
@@ -47,45 +46,45 @@ def program(State s, Queue output) {
                 s.ended = true
                 return
             case 1:
-                assignTo(v, pos + 3, p3Mode(op), param(v, pos + 1, p1Mode(op)) + param(v, pos + 2, p2Mode(op)))
+                assignTo(v, pos + 3, p3Mode(op), param(v, pos + 1, p1Mode(op), rel) + param(v, pos + 2, p2Mode(op), rel), rel)
                 pos += 4
                 break
             case 2:
-                assignTo(v, pos + 3, p3Mode(op), param(v, pos + 1, p1Mode(op)) * param(v, pos + 2, p2Mode(op)))
+                assignTo(v, pos + 3, p3Mode(op), param(v, pos + 1, p1Mode(op), rel) * param(v, pos + 2, p2Mode(op), rel), rel)
                 pos += 4
                 break
             case 5:
-                if (param(v, pos + 1, p1Mode(op)) != 0) {
-                    pos = param(v, pos + 2, p2Mode(op))
+                if (param(v, pos + 1, p1Mode(op), rel) != 0) {
+                    pos = param(v, pos + 2, p2Mode(op), rel)
                 } else {
                     pos += 3
                 }
                 break
             case 6:
-                if (param(v, pos + 1, p1Mode(op)) == 0) {
-                    pos = param(v, pos + 2, p2Mode(op))
+                if (param(v, pos + 1, p1Mode(op), rel) == 0) {
+                    pos = param(v, pos + 2, p2Mode(op), rel)
                 } else {
                     pos += 3
                 }
                 break
             case 7:
-                if (param(v, pos + 1, p1Mode(op)) < param(v, pos + 2, p2Mode(op))) {
-                    assignTo(v, pos + 3, p3Mode(op), 1)
+                if (param(v, pos + 1, p1Mode(op), rel) < param(v, pos + 2, p2Mode(op), rel)) {
+                    assignTo(v, pos + 3, p3Mode(op), 1, rel)
                 } else {
-                    assignTo(v, pos + 3, p3Mode(op), 0)
+                    assignTo(v, pos + 3, p3Mode(op), 0, rel)
                 }
                 pos += 4
                 break
             case 8:
-                if (param(v, pos + 1, p1Mode(op)) == param(v, pos + 2, p2Mode(op))) {
-                    assignTo(v, pos + 3, p3Mode(op), 1)
+                if (param(v, pos + 1, p1Mode(op), rel) == param(v, pos + 2, p2Mode(op), rel)) {
+                    assignTo(v, pos + 3, p3Mode(op), 1, rel)
                 } else {
-                    assignTo(v, pos + 3, p3Mode(op), 0)
+                    assignTo(v, pos + 3, p3Mode(op), 0, rel)
                 }
                 pos += 4
                 break
             case 9:
-                long shift = param(v, pos + 1, p1Mode(op))
+                long shift = param(v, pos + 1, p1Mode(op), rel)
                 rel += shift
                 pos += 2
                 break
@@ -97,11 +96,11 @@ def program(State s, Queue output) {
                     return
                 }
                 println "Get input from $s.input"
-                assignTo(v, pos + 1, p1Mode(op), s.input.poll())
+                assignTo(v, pos + 1, p1Mode(op), s.input.poll(), rel)
                 pos += 2
                 break
             case 4:
-                long out = param(v, pos + 1, p1Mode(op))
+                long out = param(v, pos + 1, p1Mode(op), rel)
                 println "Out: $out"
                 output.offer(out)
                 pos += 2
@@ -128,5 +127,3 @@ State state = new State(
 
 program(state, output)
 println(output)
-
-//3380552197 is to low
