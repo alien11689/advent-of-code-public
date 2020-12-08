428b69a292c47d2599e90d16e2a3f19a6baf76d9
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-08 07:18:45 +0100

    Part 2 of 8/2020

--- 2020/src/main/kotlin/pl/touk/dpr/aoc2020/day08/Main.kt
+++ 2020/src/main/kotlin/pl/touk/dpr/aoc2020/day08/Main.kt
@@ -37,6 +37,52 @@ object Main {
     }
 
     private fun part2(input: String) {
+        val instr = input.lines()
+                .filter { it.isNotEmpty() }
+        var i = 0
+        while (i < instr.size) {
+            if (instr[i].contains("nop") || instr[i].contains("jmp")) {
+                val newInstr = instr.mapIndexed { idx, inst ->
+                    if (i == idx && inst.startsWith("nop")) {
+                        inst.replace("nop", "jmp")
+                    } else if (i == idx && inst.startsWith("jmp")) {
+                        inst.replace("jmp", "nop")
+                    } else {
+                        inst
+                    }
+                }
+                val (acc, loop) = runProgram(newInstr)
+                if(!loop){
+                    println(acc)
+                    break
+                }
+            }
+            ++i
+        }
+    }
 
+    private fun runProgram(instr: List<String>): Pair<Int, Boolean> {
+        val usedInstructons = mutableSetOf<Int>()
+        var pointer = 0
+        var acc = 0
+        var infiniteLoop = false
+        while (pointer < instr.size) {
+            if (pointer in usedInstructons) {
+                infiniteLoop = true
+                break
+            }
+            usedInstructons.add(pointer)
+            val ins = instr[pointer]
+            val split = ins.split(" ")
+            if (split[0] == "nop") {
+                ++pointer
+            } else if (split[0] == "acc") {
+                ++pointer
+                acc += split[1].toInt()
+            } else {
+                pointer += split[1].toInt()
+            }
+        }
+        return Pair(acc, infiniteLoop)
     }
 }
