cdf92b7c4563d37c7285d1eda4c7791a0273f0c4
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-12 20:45:45 +0100

    Prepare for 13/2020

---
+++ 2020/src/main/kotlin/pl/touk/dpr/aoc2020/day13/Main.kt
@@ -0,0 +1,21 @@
+package pl.touk.dpr.aoc2020.day13
+
+import pl.touk.dpr.aoc2020.Util
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+//        val input = Util.getNotEmptyLinesFromFile("/12/test.txt")
+        val input = Util.getNotEmptyLinesFromFile("/13/input.txt")
+        println(part1(input))
+        println(part2(input))
+    }
+
+    private fun part1(input: List<String>): Any {
+        return "TODO part 2"
+    }
+
+    private fun part2(input: List<String>): Any {
+        return "TODO part 2"
+    }
+}
