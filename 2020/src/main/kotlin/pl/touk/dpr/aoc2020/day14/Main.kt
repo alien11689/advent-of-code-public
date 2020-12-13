624f3f5de649998e1042bd2b977a07dfc290d40f
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-13 12:12:19 +0100

    Prepare for day 14/2020

---
+++ 2020/src/main/kotlin/pl/touk/dpr/aoc2020/day14/Main.kt
@@ -0,0 +1,22 @@
+package pl.touk.dpr.aoc2020.day14
+
+import pl.touk.dpr.aoc2020.Util
+import java.math.BigInteger
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val input = Util.getNotEmptyLinesFromFile("/14/input.txt")
+        println(part1(input))
+        println(part2(input))
+    }
+
+    private fun part1(input: List<String>): Any {
+        TODO()
+    }
+
+    private fun part2(input: List<String>): BigInteger {
+        TODO()
+    }
+}
+
