2ac59749974def65b635db80982a2d42d1b8364b
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-15 19:27:10 +0100

    Hide tests of 13/2020

--- 2020/src/main/kotlin/pl/touk/dpr/aoc2020/day13/Main.kt
+++ 2020/src/main/kotlin/pl/touk/dpr/aoc2020/day13/Main.kt
@@ -9,12 +9,12 @@ object Main {
 //        val input = Util.getNotEmptyLinesFromFile("/12/test.txt")
         val input = Util.getNotEmptyLinesFromFile("/13/input.txt")
         println(part1(input))
-        Util.test(part2("7,13,x,x,59,x,31,19"), 1068781.toBigInteger())
-        Util.test(part2("17,x,13,19"), 3417.toBigInteger())
-        Util.test(part2("67,7,59,61"), 754018.toBigInteger())
-        Util.test(part2("67,x,7,59,61"), 779210.toBigInteger())
-        Util.test(part2("67,7,x,59,61"), 1261476.toBigInteger())
-        Util.test(part2("1789,37,47,1889"), 1202161486.toBigInteger())
+//        Util.test(part2("7,13,x,x,59,x,31,19"), 1068781.toBigInteger())
+//        Util.test(part2("17,x,13,19"), 3417.toBigInteger())
+//        Util.test(part2("67,7,59,61"), 754018.toBigInteger())
+//        Util.test(part2("67,x,7,59,61"), 779210.toBigInteger())
+//        Util.test(part2("67,7,x,59,61"), 1261476.toBigInteger())
+//        Util.test(part2("1789,37,47,1889"), 1202161486.toBigInteger())
         println(part2(input[1]))
     }
 
