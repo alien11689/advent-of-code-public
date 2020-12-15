75f149c57f367b8606083c00ed0658a8af98ad81
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-15 07:54:35 +0100

    Day 15/2020

--- 2020/src/main/kotlin/pl/touk/dpr/aoc2020/day15/Main.kt
+++ 2020/src/main/kotlin/pl/touk/dpr/aoc2020/day15/Main.kt
@@ -1,21 +1,39 @@
 package pl.touk.dpr.aoc2020.day15
 
-import pl.touk.dpr.aoc2020.Util
-
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getNotEmptyLinesFromFile("/15/input.txt")
-        println(part1(input))
-        println(part2(input))
+        val input = "1,2,16,19,18,0"
+        println(part1And2(input))
     }
 
-    private fun part1(input: List<String>): Any {
-        TODO()
-    }
+    private fun part1And2(input: String): Any {
+        val mem = mutableMapOf<Int, Int>()
+        var current = 0
+        input.split(",").mapIndexed { index, s ->
+            mem[s.toInt()] = index + 1
+            current = s.toInt()
+        }
+        var round = mem.size
+        var res = mutableListOf<Int>()
+        while (round < 30000000 + 1) {
+            if (round == 2020 || round == 30000000) {
+//                println("round: $round, current: $current")
+                res.add(current)
+            }
+            if (current in mem) {
+                val newCurrent = round - mem[current]!!
+                mem[current] = round
+                current = newCurrent
+            } else {
+                mem[current] = round
+                current = 0
 
-    private fun part2(input: List<String>): Any {
-        TODO()
+            }
+            round++
+        }
+        return res.joinToString("\n")
+
     }
 }
 
