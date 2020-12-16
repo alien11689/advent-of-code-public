8eda8370ca7b8c06ef4dfb72b347a9264d652634
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-16 19:46:35 +0100

    Try to add memory for teleportoer

--- synacor-challenge/src/main/kotlin/pl/touk/dpr/synacorchallenge/Teleporter.kt
+++ synacor-challenge/src/main/kotlin/pl/touk/dpr/synacorchallenge/Teleporter.kt
@@ -13,25 +13,30 @@ object Teleporter {
 
     private val BASE = 32768
 
-    private fun solve(startR0: Int, startR1: Int, r7: Int, mem: MutableMap<Pair<Int, Int>, Pair<Int, Int>>): Pair<Int, Int> {
-        var r0 = startR0
-        var r1 = startR1
+    private fun solve(R0: Int, R1: Int, r7: Int, mem: MutableMap<Pair<Int, Int>, Pair<Int, Int>>): Pair<Int, Int> {
+        println("solve $R0 $R1 ${mem}")
+        val key = Pair(R0, R1)
+        if (key in mem) {
+            return mem[key]!!
+        }
+        var r0 = R0
+        var r1 = R1
 
-        if (r0 != 0) {
-            val key = Pair(r0, r1)
-            val res = if (mem.contains(key)) mem[key]!! else fun6035(r0, r1, r7, mem)
-            mem[key] = res
+        if (r0 > 0) {
+            val res = fun6035(r0, r1, r7, mem)
             r0 = res.first
             r1 = res.second
         }
         r0 = (r1 + 1) % BASE
+        mem[key] = Pair(r0, r1)
         return Pair(r0, r1)
     }
 
     private fun fun6035(R0: Int, R1: Int, r7: Int, mem: MutableMap<Pair<Int, Int>, Pair<Int, Int>>): Pair<Int, Int> {
+        println("fun6035 $R0 $R1")
         var r0 = R0
         var r1 = R1
-        if (r0 > 0) {
+        if (r1 > 0) {
             val res = fun6048(r0, r1, r7, mem)
             r0 = res.first
             r1 = res.second
@@ -42,15 +47,16 @@ object Teleporter {
     }
 
     private fun fun6048(R0: Int, R1: Int, r7: Int, mem: MutableMap<Pair<Int, Int>, Pair<Int, Int>>): Pair<Int, Int> {
+        println("fun6048 $R0 $R1")
         val x = R0
-        var r1 = (R0 + 32767) % BASE
+        var r1 = (R1 + 32767) % BASE
         var res = solve(R0, r1, r7, mem)
         var r0 = res.first
         r1 = res.second
         r1 = r0
         r0 = x
         r0 = (r0 + 32767) % BASE
-        res = solve(R0, r1, r7, mem)
+        res = solve(r0, r1, r7, mem)
         return res
     }
 }
