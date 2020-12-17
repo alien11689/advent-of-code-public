5ba8bf297d90ae8f1127c534e3d682353c8e3de5
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-17 17:08:40 +0100

    Rewrite coins in groovy

--- synacor-challenge/solveCoins.groovy
+++
@@ -1,9 +0,0 @@
-int red = 2
-int blue = 9
-int corroded = 3
-int concave = 7
-int shiny = 5
-
-println([red, blue, corroded, concave, shiny].permutations().find {
-    it[0] + it[1] * it[2]**2 + it[3]**3 - it[4] == 399
-})
---
+++ synacor-challenge/src/main/kotlin/pl/touk/dpr/synacorchallenge/Coins.kt
@@ -0,0 +1,28 @@
+package pl.touk.dpr.synacorchallenge
+
+object Coins {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val red = 2
+        val blue = 9
+        val corroded = 3
+        val concave = 7
+        val shiny = 5
+
+        val all = setOf<Int>(red, blue, corroded, concave, shiny)
+        all.forEach { c1 ->
+            all.forEach { c2 ->
+                all.forEach { c3 ->
+                    all.forEach { c4 ->
+                        all.forEach { c5 ->
+                            if (setOf(c1, c2, c3, c4, c5) == all && c1 + c2 * c3 * c3 + c4 * c4 * c4 - c5 == 399) {
+                                println("$c1 $c2 $c3 $c4 $c5")
+                                return
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
