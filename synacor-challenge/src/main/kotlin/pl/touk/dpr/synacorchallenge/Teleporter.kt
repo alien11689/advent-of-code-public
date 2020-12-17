07d1371b5b7fda8a7dcf3de64286f7d4c36943f4
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-17 08:55:17 +0100

    Add missing files for 17/2020

--- synacor-challenge/src/main/kotlin/pl/touk/dpr/synacorchallenge/Teleporter.kt
+++ synacor-challenge/src/main/kotlin/pl/touk/dpr/synacorchallenge/Teleporter.kt
@@ -1,20 +1,24 @@
 package pl.touk.dpr.synacorchallenge
 
+import java.lang.RuntimeException
+
 object Teleporter {
     @JvmStatic
     fun main(args: Array<String>) {
         val r0 = 4
         val r1 = 1
-        (1..32767).find {
+        val x = (1..32767).find {
             println("Checking $it")
-            solve(r0, r1, it, mutableMapOf()).first == 6
+            val first = solve(r0, r1, it, mutableMapOf()).first
+            first == 6
         }
+        println(x)
     }
 
     private val BASE = 32768
 
     private fun solve(R0: Int, R1: Int, r7: Int, mem: MutableMap<Pair<Int, Int>, Pair<Int, Int>>): Pair<Int, Int> {
-        println("solve $R0 $R1 ${mem}")
+//        println("solve $R0 $R1 ${mem.size}")
         val key = Pair(R0, R1)
         if (key in mem) {
             return mem[key]!!
@@ -24,8 +28,8 @@ object Teleporter {
 
         if (r0 > 0) {
             val res = fun6035(r0, r1, r7, mem)
-            r0 = res.first
-            r1 = res.second
+            mem[key] = res
+            return res
         }
         r0 = (r1 + 1) % BASE
         mem[key] = Pair(r0, r1)
@@ -33,13 +37,11 @@ object Teleporter {
     }
 
     private fun fun6035(R0: Int, R1: Int, r7: Int, mem: MutableMap<Pair<Int, Int>, Pair<Int, Int>>): Pair<Int, Int> {
-        println("fun6035 $R0 $R1")
+//        println("fun6035 $R0 $R1")
         var r0 = R0
         var r1 = R1
         if (r1 > 0) {
-            val res = fun6048(r0, r1, r7, mem)
-            r0 = res.first
-            r1 = res.second
+            return fun6048(r0, r1, r7, mem)
         }
         r0 = (r0 + 32767) % BASE
         r1 = r7
@@ -47,7 +49,7 @@ object Teleporter {
     }
 
     private fun fun6048(R0: Int, R1: Int, r7: Int, mem: MutableMap<Pair<Int, Int>, Pair<Int, Int>>): Pair<Int, Int> {
-        println("fun6048 $R0 $R1")
+//        println("fun6048 $R0 $R1")
         val x = R0
         var r1 = (R1 + 32767) % BASE
         var res = solve(R0, r1, r7, mem)
