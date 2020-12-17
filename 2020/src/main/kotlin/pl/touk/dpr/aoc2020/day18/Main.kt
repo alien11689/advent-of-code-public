4a527dc8e59ec8220190af122925d1ac5ddc63a6
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-17 12:25:40 +0100

    Prepare for day 18/2020

---
+++ 2020/src/main/kotlin/pl/touk/dpr/aoc2020/day18/Main.kt
@@ -0,0 +1,20 @@
+package pl.touk.dpr.aoc2020.day18
+
+import pl.touk.dpr.aoc2020.Util
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val input = Util.getLinesFromFile("/18/input.txt")
+        println(part1(input))
+        println(part2(input))
+    }
+
+    private fun part1(input: List<String>): Any {
+        TODO()
+    }
+
+    private fun part2(input: List<String>): Any {
+        TODO()
+    }
+}
