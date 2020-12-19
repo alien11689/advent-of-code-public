fba32515a25adfd86812cb4f48d73daf72c676d0
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-19 20:32:55 +0100

    Rewrte run all script

--- 2015/runAll.sh
+++ 2015/runAll.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+
+year=2015
+
 mvn clean package
 
 for x in `seq 1 9`; do
   echo "Day $x"
-  java -cp target/2015-1.0.0-SNAPSHOT-jar-with-dependencies.jar pl.touk.dpr.aoc2015.Day0$x
+  java -cp target/${year}-1.0.0-SNAPSHOT-jar-with-dependencies.jar pl.touk.dpr.aoc${year}.Day0$x
 done
 
 for x in `seq 10 25`; do
   echo "Day $x"
-  java -cp target/2015-1.0.0-SNAPSHOT-jar-with-dependencies.jar pl.touk.dpr.aoc2015.Day$x
+  java -cp target/${year}-1.0.0-SNAPSHOT-jar-with-dependencies.jar pl.touk.dpr.aoc${year}.Day$x
 done
--- 2018/runAll.sh
+++ 2018/runAll.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+
+year=2018
+
 mvn clean package
 
 for x in `seq 1 9`; do
   echo "Day $x"
-  java -cp target/2018-1.0.0-SNAPSHOT-jar-with-dependencies.jar pl.touk.dpr.aoc2018.day0$x.Main
+  java -cp target/${year}-1.0.0-SNAPSHOT-jar-with-dependencies.jar pl.touk.dpr.aoc${year}.Day0$x
 done
 
 for x in `seq 10 25`; do
   echo "Day $x"
-  java -cp target/2018-1.0.0-SNAPSHOT-jar-with-dependencies.jar pl.touk.dpr.aoc2018.day$x.Main
+  java -cp target/${year}-1.0.0-SNAPSHOT-jar-with-dependencies.jar pl.touk.dpr.aoc${year}.Day$x
 done
