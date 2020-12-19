18587cb1a98b784718d8a445195b2b95a029bfc5
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-19 09:39:44 +0100

    Part 2 of 19/2020

--- 2020/src/main/kotlin/pl/touk/dpr/aoc2020/day19/Main.kt
+++ 2020/src/main/kotlin/pl/touk/dpr/aoc2020/day19/Main.kt
@@ -7,7 +7,8 @@ object Main {
     fun main(args: Array<String>) {
         val input = Util.getLinesFromFile("/19/input.txt")
         println(part1(input))
-        println(part2(input))
+        val input2 = Util.getLinesFromFile("/19/input2.txt")
+        println(part2(input2))
     }
 
     private fun part1(input: List<String>): Any {
@@ -80,6 +81,54 @@ object Main {
     }
 
     private fun part2(input: List<String>): Any {
-        TODO()
+        val (rules, text) = parseInput(input)
+        println("0: ${rules[0]}")
+        println("8: ${rules[8]}")
+        println("11: ${rules[11]}")
+        val rule42 = resolveRule(rules[42]!!, rules)
+        val rule31 = resolveRule(rules[31]!!, rules)
+        println("42: ${rule42}")
+        println("31: ${rule31}")
+        println("Rule 8 -> rule42+")
+        println("Rule 11 -> rule42 ... rule31")
+        val initRegex = Regex("^($rule42)+($rule31)+$")
+        val initSet = text.filter { initRegex.matches(it) }
+        val exactRule42 = Regex("^$rule42$")
+        val exactRule31 = Regex("^$rule31$")
+        var res = 0
+        initSet.forEach { s ->
+            println("Checking $s")
+            var I = 0
+            var i = I + 1
+            var J = s.length
+            var j = J - 1
+            var ends = 0
+            while (0 < j) {
+                val toCheckEnd = s.substring(j, J)
+                if (exactRule31.matches(toCheckEnd)) {
+                    println("Matched end $toCheckEnd")
+                    J = j
+                    ++ends
+                }
+                --j
+            }
+            var begins = 0
+            while (i <= J) {
+                val toCheckBegin = s.substring(I, i)
+                if (exactRule42.matches(toCheckBegin)) {
+                    println("Matched begin $toCheckBegin")
+                    I = i
+                    ++begins
+                }
+                ++i
+            }
+            println("$begins $ends")
+            if (begins > ends) {
+                ++res
+            }
+        }
+
+        // 425 is too high
+        return res
     }
 }
