ffdb1f6a65b9b6edb1220c8dc03aca9cbac78200
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-20 11:17:20 +0100

    Dirty 20/2020

--- 2020/src/main/kotlin/pl/touk/dpr/aoc2020/Day20.kt
+++ 2020/src/main/kotlin/pl/touk/dpr/aoc2020/Day20.kt
@@ -9,10 +9,238 @@ object Day20 {
     }
 
     private fun part1(input: List<String>): Any {
-        return 0
+        val tiles = readTiles(input)
+        val borders = tiles.values.flatMap { it.borders }.groupBy { it }.mapValues { it.value.size }
+        val uniqueBorders = borders.filter { it.value == 1 }.keys.toSet()
+        val corners = tiles.values.filter { it.borders.count { it in uniqueBorders } > 3 }.map { it.id }
+        return corners.fold(1L) { acc, l -> acc * l }
+    }
+
+    private fun readTiles(input: List<String>): Map<Long, Tile> {
+        val tiles = mutableMapOf<Long, Tile>()
+        var i = 0
+        var curTile = mutableListOf<List<Char>>()
+        var curNum = -1L
+        while (i < input.size) {
+            if (i % 11 == 0) {
+                if (curNum > -1) {
+                    tiles[curNum] = Tile(curNum, curTile)
+                }
+                curNum = input[i].split(Regex("[: ]"))[1].toLong()
+                curTile = mutableListOf()
+            } else {
+                curTile.add(input[i].toCharArray().toList())
+            }
+            ++i
+        }
+        tiles[curNum] = Tile(curNum, curTile)
+        return tiles.toMap()
     }
 
     private fun part2(input: List<String>): Any {
-        return 0
+        val tiles = readTiles(input).toMutableMap()
+        val borders = tiles.values.flatMap { it.borders }.groupBy { it }.mapValues { it.value.size }
+        val uniqueBorders = borders.filter { it.value == 1 }.keys.toSet()
+        val corners = tiles.values.filter { it.borders.count { it in uniqueBorders } > 3 }
+        var cornerFirst = corners.first()
+        val wholeImage: MutableList<MutableList<Tile?>> = IntRange(0, 11).map {
+            IntRange(0, 11).map {
+                null
+            }.toMutableList<Tile?>()
+        }.toMutableList()
+        var i = 0
+        while (cornerFirst.row(0) !in uniqueBorders || cornerFirst.column(0) !in uniqueBorders) {
+            if (i % 4 == 3) {
+                cornerFirst = cornerFirst.transpose()
+            } else {
+                cornerFirst = cornerFirst.rotate()
+            }
+            ++i
+        }
+        tiles.remove(cornerFirst.id)
+        wholeImage[0][0] = cornerFirst
+        wholeImage.forEachIndexed { rowIdx, row ->
+            wholeImage.forEachIndexed { colIdx, column ->
+                if (rowIdx != 0 || colIdx != 0) { // omit first calculated before
+                    if (colIdx != 0) {
+                        val left = wholeImage[rowIdx][colIdx - 1]!!
+                        if (rowIdx == 0) {
+                            var matching = tiles.values.find { cur -> left.column(9) in cur.borders && uniqueBorders.any { it in cur.borders } }!!
+                            i = 0
+                            while (matching.row(0) !in uniqueBorders || matching.column(0) != left.column(9)) {
+                                if (i % 5 == 4) {
+                                    matching = matching.transpose()
+                                } else {
+                                    matching = matching.rotate()
+                                }
+                                ++i
+                            }
+//                            print("${matching.id} ")
+                            wholeImage[rowIdx][colIdx] = matching
+                            tiles.remove(matching.id)
+                        } else {
+                            val upper = wholeImage[rowIdx - 1][colIdx]!!
+                            var matching = tiles.values.find { cur -> left.column(9) in cur.borders && upper.row(9) in cur.borders }!!
+                            i = 0
+                            while (matching.row(0) != upper.row(9) || matching.column(0) != left.column(9)) {
+                                if (i % 5 == 4) {
+                                    matching = matching.transpose()
+                                } else {
+                                    matching = matching.rotate()
+                                }
+                                ++i
+                            }
+//                            print("${matching.id} ")
+                            wholeImage[rowIdx][colIdx] = matching
+                            tiles.remove(matching.id)
+                        }
+                    } else {
+                        val upper = wholeImage[rowIdx - 1][colIdx]!!
+                        //                            println("Possible ${possible.size}")
+                        var matching = tiles.values.find { cur -> upper.row(9) in cur.borders && uniqueBorders.any { it in cur.borders } }!!
+                        i = 0
+                        while (matching.column(0) !in uniqueBorders || matching.row(0) != upper.row(9)) {
+                            if (i % 5 == 4) {
+                                matching = matching.transpose()
+                            } else {
+                                matching = matching.rotate()
+                            }
+                            ++i
+                        }
+//                        print("${matching.id} ")
+                        wholeImage[rowIdx][colIdx] = matching
+                        tiles.remove(matching.id)
+                    }
+                } else {
+//                    print("${cornerFirst.id} ")
+                }
+            }
+//            println()
+        }
+        var whole = Tile(-1L, join(wholeImage))
+        i = 0
+        while (true) {
+            val foundDragons = lookForDragon(whole)
+            if (foundDragons > 0) {
+                val all = whole.image.map { it.filter { it == '#' }.count() }.sum()
+                return all - foundDragons * dragonPoints().size
+            }
+            if (i % 4 == 3) {
+                whole = whole.transpose()
+            } else {
+                whole = whole.rotate()
+            }
+            ++i
+        }
+    }
+
+    private fun lookForDragon(whole: Tile): Int {
+        val dragon = dragonPoints()
+        var found = 0
+        (0..(whole.image.size - 3)).forEach { rowId ->
+            (0..(whole.image[rowId].size - 20)).forEach { colId ->
+                if (dragon.map { p -> Pair(p.first + rowId, p.second + colId) }.all { whole.image[it.first][it.second] == '#' }) {
+                    ++found
+                }
+            }
+        }
+        return found
+    }
+
+    private fun dragonPoints(): Set<Pair<Int, Int>> {
+        //                  #
+        //#    ##    ##    ###
+        // #  #  #  #  #  #
+        return setOf(
+                Pair(0, 18),
+                Pair(1, 0),
+                Pair(1, 5),
+                Pair(1, 6),
+                Pair(1, 11),
+                Pair(1, 12),
+                Pair(1, 17),
+                Pair(1, 18),
+                Pair(1, 19),
+                Pair(2, 1),
+                Pair(2, 4),
+                Pair(2, 7),
+                Pair(2, 10),
+                Pair(2, 13),
+                Pair(2, 16)
+        )
+    }
+
+    private fun join(wholeImage: List<List<Tile?>>): List<List<Char>> {
+        val ml = mutableListOf<List<Char>>()
+        wholeImage.forEach { rowTiles ->
+            (0..7).forEach { insideRow ->
+                val newRow = mutableListOf<Char>()
+                rowTiles.forEach { tile ->
+                    newRow.addAll(tile!!.withoutBorders().row(insideRow))
+                }
+                ml.add(newRow.toList())
+            }
+        }
+        return ml.toList()
+    }
+
+    data class Tile(val id: Long, val image: List<List<Char>>) {
+        val borders: List<List<Char>> = listOf(
+                image[0],
+                image[image.size - 1],
+                column(0),
+                column(image[0].size - 1),
+                image[0].reversed(),
+                image[image.size - 1].reversed(),
+                column(0).reversed(),
+                column(image[0].size - 1).reversed(),
+        )
+
+        fun column(col: Int): List<Char> {
+            val ml = mutableListOf<Char>()
+            image.forEach { ml.add(it[col]) }
+            return ml.toList()
+        }
+
+        fun row(r: Int): List<Char> {
+            return image[r]
+        }
+
+        fun transpose(): Tile {
+            val newImage = IntRange(0, image.size - 1).map {
+                column(it)
+            }
+            return copy(image = newImage)
+        }
+
+        fun rotate(): Tile {
+            return copy(image = IntRange(0, image.size - 1).map {
+                column(it).reversed()
+            })
+        }
+
+        fun print() {
+            image.forEach {
+                it.forEach { print(it) }
+                println()
+            }
+        }
+
+        fun withoutBorders(): Tile {
+            val newImage = image.mapIndexed { rowIdx: Int, row: List<Char> ->
+                if (rowIdx == 0 || rowIdx == 9) {
+                    listOf()
+                } else {
+                    row.flatMapIndexed { colIdx: Int, col: Char ->
+                        if (colIdx == 0 || colIdx == 9) {
+                            listOf()
+                        } else {
+                            listOf(col)
+                        }
+                    }
+                }
+            }.filter { it.isNotEmpty() }
+            return copy(image = newImage)
+        }
     }
 }
