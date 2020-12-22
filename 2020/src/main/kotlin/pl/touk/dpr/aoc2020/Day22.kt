4c1be4a1720dae6736c3970fde598106a01d419d
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-22 08:46:33 +0100

    Dirty 22/2020

--- 2020/src/main/kotlin/pl/touk/dpr/aoc2020/Day22.kt
+++ 2020/src/main/kotlin/pl/touk/dpr/aoc2020/Day22.kt
@@ -7,6 +7,7 @@ object Day22 {
     @JvmStatic
     fun main(args: Array<String>) {
 //        val input = Util.getNotEmptyLinesFromFile("/22/sample.txt")
+//        val input = Util.getNotEmptyLinesFromFile("/22/sample2.txt")
         val input = Util.getNotEmptyLinesFromFile("/22/input.txt")
         println(part1(input))
         println(part2(input))
@@ -51,30 +52,38 @@ object Day22 {
 
     private fun part2(input: List<String>): Any {
         val (player1: Queue<Int>, player2: Queue<Int>) = readPlayers(input)
-        val (p1, p2) = play(player1, player2)
+        val (p1, p2) = play(player1, player2, 1, mutableMapOf())
         val winner = if (p1.isEmpty()) p2 else p1
         return winner.reversed().foldIndexed(0L) { index, acc, i -> acc + (index + 1) * i.toLong() }
     }
 
-    private fun play(initPlayer1: Queue<Int>, initPlayer2: Queue<Int>, level: Int = 1): Pair<Queue<Int>, Queue<Int>> {
+    private fun play(initPlayer1: Queue<Int>, initPlayer2: Queue<Int>, level: Int, mem: MutableMap<Pair<List<Int>, List<Int>>, Pair<List<Int>, List<Int>>>): Pair<List<Int>, List<Int>> {
         println("Current level is $level with $initPlayer1 $initPlayer2")
+        val key = Pair(initPlayer1.toList(), initPlayer2.toList())
+        if (mem.containsKey(key)) {
+            println("FROM MEM")
+            return mem[key]!!
+        }
         val player1 = LinkedList(initPlayer1)
         val player2 = LinkedList(initPlayer2)
-        val prev = mutableSetOf<Pair<List<Int>, List<Int>>>()
+        val prev: MutableSet<Pair<List<Int>, List<Int>>> = mutableSetOf()
         while (player1.isNotEmpty() && player2.isNotEmpty()) {
             val curState = Pair(player1.toList(), player2.toList())
             if (prev.contains(curState)) {
+//                println("Prev state used")
                 while (player2.isNotEmpty()) {
                     player1.offer(player2.poll())
                 }
-                return Pair(player1, player2)
+                val res = Pair(player1.toList(), player2.toList())
+                mem[key] = res
+                return res
             }
             prev.add(curState)
             val p1 = player1.poll()
             val p2 = player2.poll()
             if (p1 <= player1.size && p2 <= player2.size) {
-                println("Starting subgame because of $p1 $p2")
-                val (pp1, pp2) = play(player1, player2, level + 1)
+//                println("Starting subgame because of $p1 $p2")
+                val (pp1, pp2) = play(LinkedList(player1.take(p1)), LinkedList(player2.take(p2)), level + 1, mem)
                 if (pp1.isNotEmpty()) {
                     player1.offer(p1)
                     player1.offer(p2)
@@ -82,8 +91,9 @@ object Day22 {
                     player2.offer(p2)
                     player2.offer(p1)
                 }
-                println("Return to level $level with $player1 $player2")
+//                println("Return to level $level with $player1 $player2")
             } else {
+//                println("Normal game $p1 $p2")
                 if (p1 > p2) {
                     player1.offer(p1)
                     player1.offer(p2)
@@ -93,6 +103,8 @@ object Day22 {
                 }
             }
         }
-        return Pair(player1, player2)
+        val res = Pair(player1.toList(), player2.toList())
+        mem[key] = res
+        return res
     }
 }
