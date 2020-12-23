111baa79f8475e3573836b11a44b9db0a2c7debe
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-23 07:27:09 +0100

    Part2 of 23/2020

--- 2020/src/main/kotlin/pl/touk/dpr/aoc2020/Day23.kt
+++ 2020/src/main/kotlin/pl/touk/dpr/aoc2020/Day23.kt
@@ -11,18 +11,16 @@ object Day23 {
 
     private fun part1(input: String): Any {
         var cups = input.map { it.toString().toInt() }.toList()
-        var current = 0
         (1..100).forEach {
-            cups = round(cups, current)
+            cups = round(cups)
         }
-
         val res = cups.subList(cups.indexOf(1) + 1, cups.size) + cups.subList(0, cups.indexOf(1))
 
         return res.joinToString("") { it.toString() }
     }
 
-    private fun round(cups: List<Int>, current: Int): List<Int> {
-        val curVal = cups[current]
+    private fun round(cups: List<Int>): List<Int> {
+        val curVal = cups[0]
         val pickedUp = (1..3).map { cups[it % cups.size] }
         var destination = if (curVal == 1) 9 else (curVal - 1)
         while (destination in pickedUp) {
@@ -42,16 +40,46 @@ object Day23 {
 
     private fun part2(input: String): Any {
         val initCups = input.map { it.toString().toInt() }.toList()
-        var cups = initCups + ((initCups.max()!! + 1)..1000000)
-        val current = 0
-        (1..10000000).forEach {
-//            if (it % 10000 == 0) {
-            println("Move $it")
-//            }
-            cups = round(cups, current)
-////            println("After round $it: $cups")
+        val elements = initCups + ((initCups.max()!! + 1)..1000000)
+        var begin: Node? = null
+        var current: Node? = null
+        val mem = mutableMapOf<Int, Node>()
+        elements.forEach {
+            val next = Node(it, null)
+            mem[it] = next
+            if (current == null) {
+                current = next
+                begin = next
+            } else {
+                current!!.next = next
+                current = next
+            }
         }
-//        return cups.joinToString("") { it.toString() }
-        return 0
+        current!!.next = begin
+        current = begin
+        val max = 1000000
+        (1..10000000).forEach {
+            current = move(current!!, max, mem)
+        }
+        val node1: Node = mem[1]!!
+        return node1.next!!.value.toLong() * node1.next!!.next!!.value.toLong()
     }
+
+    private fun move(current: Node, max: Int, mem: Map<Int, Node>): Node {
+        val pickedUpNodeBegin = current.next!!
+        val pickedUpValues = setOf(current.next!!.value, current.next!!.next!!.value, current.next!!.next!!.next!!.value)
+        val nextNode = current.next!!.next!!.next!!.next!!
+        var destination = if (current.value == 1) max else (current.value - 1)
+        while (destination in pickedUpValues) {
+            destination = if (destination == 1) max else (destination - 1)
+        }
+        val destinationNode: Node = mem[destination]!!
+        current.next = nextNode
+        val afterDestinationNode = destinationNode.next
+        destinationNode.next = pickedUpNodeBegin
+        pickedUpNodeBegin.next!!.next!!.next = afterDestinationNode
+        return nextNode
+    }
+
+    data class Node(val value: Int, var next: Node? = null)
 }
