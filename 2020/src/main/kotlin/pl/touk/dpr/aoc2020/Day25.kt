91878b3b91e8fec51ce33a9c46023fa07ad76dd3
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-25 06:30:58 +0100

    25/2020

--- 2020/src/main/kotlin/pl/touk/dpr/aoc2020/Day25.kt
+++ 2020/src/main/kotlin/pl/touk/dpr/aoc2020/Day25.kt
@@ -5,16 +5,33 @@ object Day25 {
     fun main(args: Array<String>) {
         val input = Util.getNotEmptyLinesFromFile("/25/input.txt")
         println(part1(input))
-        println(part2(input))
     }
 
     private fun part1(input: List<String>): Any {
-
-        return 0
-    }
-
-    private fun part2(input: List<String>): Any {
-
-        return 0
+        val publicKey1 = input[0].toLong()
+        val publicKey2 = input[1].toLong()
+        val pn = 20201227
+        val subject = 7
+        var i = 1
+        var res = 1L
+        val ml = mutableMapOf<Long, Int>()
+        while (ml.size != 1) {
+            res = res * subject % pn
+//            println("$i - $res $ml")
+            if (res == publicKey1 || res == publicKey2) {
+                ml[res] = i
+            }
+            ++i
+        }
+        i = 1
+        res = 1L
+        val keyToTransform = if (ml.containsKey(publicKey1)) publicKey2 else publicKey1
+        val loop = ml.values.first()
+        while (i <= loop) {
+            res = res * keyToTransform % pn
+//            println("$i - $res $ml")
+            ++i
+        }
+        return res
     }
 }
