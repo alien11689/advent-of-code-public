1f11f612c37d3a96e474cd876339ce4e4083e75a
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-26 13:59:06 +0100

    Add empty kotlin files for 2016-2019

---
+++ 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Util.kt
@@ -0,0 +1,15 @@
+package pl.touk.dpr.aoc2016
+
+object Util {
+    fun getFileContent(fileName: String): String = javaClass.getResource(fileName).readText()
+    fun getLinesFromFile(fileName: String): List<String> = getFileContent(fileName).lines()
+    fun getNotEmptyLinesFromFile(fileName: String): List<String> = getLinesFromFile(fileName).filter { it.isNotEmpty() }
+
+    fun <A> test(given: A, expected: A) {
+        if (given != expected) {
+            throw RuntimeException("$given != $expected")
+        } else {
+            println("Passed $given == $expected")
+        }
+    }
+}
---
+++ 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day22.kt
@@ -0,0 +1,18 @@
+package pl.touk.dpr.aoc2017
+
+object Day22 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val input = Util.getFileContent("/01/input.txt").trim()
+        println(part1(input))
+        println(part2(input))
+    }
+
+    private fun part1(input: String): Any {
+        TODO()
+    }
+
+    private fun part2(input: String): Any {
+        TODO()
+    }
+}
