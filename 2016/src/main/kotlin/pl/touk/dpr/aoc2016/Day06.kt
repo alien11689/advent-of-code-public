3a81a5875244c2495797f1a0ff6ad7edba5159ab
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-27 21:43:57 +0100

    Rewrite 6/2016

--- 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day06.kt
+++ 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day06.kt
@@ -3,16 +3,28 @@ package pl.touk.dpr.aoc2016
 object Day06 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = Util.getNotEmptyLinesFromFile("/06/input.txt")
         println(part1(input))
         println(part2(input))
     }
 
-    private fun part1(input: String): Any {
-        TODO()
+    private fun part1(input: List<String>): Any {
+        return (0..(input[0].length -1 )).map { col ->
+            input.map { it[col] }
+                    .groupingBy { it }
+                    .eachCount()
+                    .maxByOrNull { it.value }!!
+                    .key
+        }.joinToString("")
     }
 
-    private fun part2(input: String): Any {
-        TODO()
+    private fun part2(input: List<String>): Any {
+        return (0..(input[0].length -1 )).map { col ->
+            input.map { it[col] }
+                    .groupingBy { it }
+                    .eachCount()
+                    .minByOrNull { it.value }!!
+                    .key
+        }.joinToString("")
     }
 }
