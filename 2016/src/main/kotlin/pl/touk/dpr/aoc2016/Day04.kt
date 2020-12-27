fadaaf0c9a32e69eed788035dcafef205526d77e
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-27 21:09:07 +0100

    Rewrite 4/2016

--- 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day04.kt
+++ 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day04.kt
@@ -3,16 +3,50 @@ package pl.touk.dpr.aoc2016
 object Day04 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = Util.getNotEmptyLinesFromFile("/04/input.txt")
         println(part1(input))
         println(part2(input))
     }
 
-    private fun part1(input: String): Any {
-        TODO()
+    private fun part1(input: List<String>): Any {
+        return input.map { Line.from(it.split(Regex("[-\\[\\]]+")).filter { it.isNotEmpty() }) }
+                .filter { it.isValid() }
+                .sumBy { it.id }
     }
 
-    private fun part2(input: String): Any {
-        TODO()
+    private fun part2(input: List<String>): Any {
+        return input.map { Line.from(it.split(Regex("[-\\[\\]]+")).filter { it.isNotEmpty() }) }
+                .first { it.decrypt().contains("north") }
+                .id
+    }
+
+    data class Line(val words: List<String>, val id: Int, val checksum: String) {
+        companion object {
+            fun from(parts: List<String>): Line {
+                return Line(parts.take(parts.size - 2), parts[parts.size - 2].toInt(), parts[parts.size - 1])
+            }
+        }
+
+        fun isValid(): Boolean {
+            return words
+                    .flatMap { it.toCharArray().toList() }
+                    .fold(mapOf<Char, Int>()) { acc, c ->
+                        acc + Pair(c, if (acc.containsKey(c)) acc[c]!! + 1 else 1)
+                    }
+                    .toList()
+                    .sortedWith { o1, o2 ->
+                        if (o1.second == o2.second) o1.first - o2.first else o2.second - o1.second
+                    }
+                    .take(checksum.length)
+                    .map { it.first }
+                    .joinToString(separator = "") == checksum
+
+        }
+
+        fun decrypt(): String {
+            return words.joinToString("").map {
+                ((it.toInt() - 97 + id ) % 26 + 97).toChar()
+            }.joinToString("")
+        }
     }
 }
