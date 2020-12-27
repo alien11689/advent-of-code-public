e77f6c422463756ebee7774893fb24c2d379cfe4
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-27 21:34:51 +0100

    Rewrite 5/2016

--- 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day05.kt
+++ 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day05.kt
@@ -1,18 +1,52 @@
 package pl.touk.dpr.aoc2016
 
+import java.math.BigInteger
+import java.security.MessageDigest
+
 object Day05 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = "ojvtpuvg"
         println(part1(input))
         println(part2(input))
     }
 
     private fun part1(input: String): Any {
-        TODO()
+        return generateSequence(0) { it + 1 }
+                .map {
+                    md5.reset()
+                    val toHash = input + it
+                    Pair(it, String.format("%032x", BigInteger(1, md5.digest(toHash.toByteArray()))))
+                }
+                .onEach { mem[it.first] = it.second }
+                .filter { it.second.startsWith("00000") }
+                .map { it.second[5] }
+                .take(8)
+                .joinToString("")
     }
 
     private fun part2(input: String): Any {
-        TODO()
+        var i = 0
+        val mutableList: MutableList<Char?> = (0..7).map { null }.toMutableList()
+        while (true) {
+            val hash = if (i in mem) {
+                mem[i]!!
+            } else {
+                md5.reset()
+                val toHash = input + i
+                String.format("%032x", BigInteger(1, md5.digest(toHash.toByteArray())))
+            }
+            if (hash.startsWith("00000") && hash[5] in ('0'..'7') && mutableList[hash[5].toString().toInt()] == null) {
+                mutableList[hash[5].toString().toInt()] = hash[6]
+                if (mutableList.all { it != null }) {
+                    return mutableList.joinToString("")
+                }
+            }
+            ++i
+        }
     }
+
+    private val md5 = MessageDigest.getInstance("MD5")
+    private val mem = mutableMapOf<Int, String>()
+
 }
