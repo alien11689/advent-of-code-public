d02d33116fba9d4f7e080e46d378a74326132ad1
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-28 19:30:37 +0100

    Rewrite 14/2016

--- 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day14.kt
+++ 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day14.kt
@@ -1,18 +1,98 @@
 package pl.touk.dpr.aoc2016
 
+import java.math.BigInteger
+import java.security.MessageDigest
+
 object Day14 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = "yjdafjpo"
         println(part1(input))
         println(part2(input))
     }
 
     private fun part1(input: String): Any {
-        TODO()
+        val res = mutableListOf<Int>()
+        val mem = mutableMapOf<Int, String>()
+        var i = 0
+        while (true) {
+            val hash = getMd5(input, i, mem)
+            val triple = hasTriple(hash)
+            if (triple != null) {
+                if (((i + 1)..(i + 1000)).any { hasFive(getMd5(input, it, mem), triple) }) {
+                    res.add(i)
+                }
+            }
+            if (res.size == 64) {
+                return res.last()
+            }
+            ++i
+        }
+    }
+
+    private fun hasTriple(hash: String): Char? {
+        var i = 0
+        while (i < hash.length - 2) {
+            if (hash[i] == hash[i + 1] && hash[i] == hash[i + 2]) {
+                return hash[i]
+            }
+            ++i
+        }
+        return null
+    }
+
+    private fun hasFive(hash: String, c: Char): Boolean {
+        var i = 0
+        while (i < hash.length - 4) {
+            if (hash[i] == c && hash[i] == hash[i + 1] && hash[i] == hash[i + 2] && hash[i] == hash[i + 3] && hash[i] == hash[i + 4]) {
+                return true
+            }
+            ++i
+        }
+        return false
     }
 
     private fun part2(input: String): Any {
-        TODO()
+        val res = mutableListOf<Int>()
+        val mem = mutableMapOf<Int, String>()
+        var i = 0
+        while (true) {
+            val hash = getMd5_2017(input, i, mem)
+            val triple = hasTriple(hash)
+            if (triple != null) {
+                if (((i + 1)..(i + 1000)).any { hasFive(getMd5_2017(input, it, mem), triple) }) {
+                    res.add(i)
+                }
+            }
+            if (res.size == 64) {
+                return res.last()
+            }
+            ++i
+        }
     }
+
+    private fun getMd5(input: String, idx: Int, mem: MutableMap<Int, String>): String {
+        if (idx in mem) {
+            return mem[idx]!!
+        }
+        md5.reset()
+        val v = String.format("%032x", BigInteger(1, md5.digest("$input$idx".toByteArray())))
+        mem[idx] = v
+        return v
+    }
+
+    private fun getMd5_2017(input: String, idx: Int, mem: MutableMap<Int, String>): String {
+        if (idx in mem) {
+            return mem[idx]!!
+        }
+        var res = "$input$idx"
+        (1..2017).forEach {
+            md5.reset()
+            res = String.format("%032x", BigInteger(1, md5.digest(res.toByteArray())))
+        }
+        mem[idx] = res
+        return res
+    }
+
+    private val md5 = MessageDigest.getInstance("MD5")
 }
