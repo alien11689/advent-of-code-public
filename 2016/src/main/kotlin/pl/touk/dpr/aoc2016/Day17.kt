8c8d42f1b9e3f4748faadcccbce602a074af2081
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-28 21:21:55 +0100

    Rewrite 17/2016

--- 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day17.kt
+++ 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day17.kt
@@ -1,18 +1,86 @@
 package pl.touk.dpr.aoc2016
 
+import java.math.BigInteger
+import java.security.MessageDigest
+import java.util.LinkedList
+
 object Day17 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = "mmsxrhfx"
         println(part1(input))
         println(part2(input))
     }
 
     private fun part1(input: String): Any {
-        TODO()
+        val pos = Pos(path = input)
+        val q = LinkedList<Pos>()
+        q.add(pos)
+        while (true) {
+            val cur = q.poll()
+            if (cur.x == 3 && cur.y == 3) {
+                return cur.path.substring(input.length)
+            }
+            val available = md5(cur.path).slice((0..3)).map {
+                it in setOf('b', 'c', 'd', 'e', 'f')
+            }
+            val moves = listOf(
+                    if (available[0]) 'U' else null,
+                    if (available[1]) 'D' else null,
+                    if (available[2]) 'L' else null,
+                    if (available[3]) 'R' else null,
+            ).filterNotNull()
+
+            moves.map { newPos(cur, it) }.filter { it.isValid() }.forEach {
+                q.offer(it)
+            }
+        }
     }
 
     private fun part2(input: String): Any {
-        TODO()
+        var pos = Pos(path = input)
+        val q = LinkedList<Pos>()
+        q.add(pos)
+        while (q.isNotEmpty()) {
+            val cur = q.poll()
+            if (cur.x == 3 && cur.y == 3) {
+                pos = cur
+                continue
+            }
+            val available = md5(cur.path).slice((0..3)).map {
+                it in setOf('b', 'c', 'd', 'e', 'f')
+            }
+            val moves = listOf(
+                    if (available[0]) 'U' else null,
+                    if (available[1]) 'D' else null,
+                    if (available[2]) 'L' else null,
+                    if (available[3]) 'R' else null,
+            ).filterNotNull()
+
+            moves.map { newPos(cur, it) }.filter { it.isValid() }.forEach {
+                q.offer(it)
+            }
+        }
+        return pos.path.substring(input.length).length
     }
+
+    private fun newPos(pos: Pos, x: Char): Pos {
+        return Pos(
+                x = pos.x + (if (x == 'L') -1 else if (x == 'R') 1 else 0),
+                y = pos.y + if (x == 'U') -1 else if (x == 'D') 1 else 0,
+                path = pos.path + x,
+                size = pos.size + 1
+        )
+    }
+
+    data class Pos(val x: Int = 0, val y: Int = 0, val path: String = "", val size: Int = 0) {
+        fun isValid() = x in (0..3) && y in (0..3)
+    }
+
+    private fun md5(s: String): String {
+        md.reset()
+        return String.format("%032x", BigInteger(1, md.digest(s.toByteArray())))
+    }
+
+    private val md = MessageDigest.getInstance("MD5")
 }
