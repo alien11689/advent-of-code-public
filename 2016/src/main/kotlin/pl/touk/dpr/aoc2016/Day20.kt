0171cb21b9564d2310641fb16f25f872a1a8098b
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-28 17:42:21 +0100

    Rewrte 20/2016

--- 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day20.kt
+++ 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day20.kt
@@ -3,16 +3,51 @@ package pl.touk.dpr.aoc2016
 object Day20 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = Util.getNotEmptyLinesFromFile("/20/input.txt")
         println(part1(input))
         println(part2(input))
     }
 
-    private fun part1(input: String): Any {
-        TODO()
+    private fun part1(input: List<String>): Any {
+        val ranges = parseInput(input)
+        var minNotExcluded = 0L
+        ranges.forEach { range ->
+            if (range.from <= minNotExcluded && range.to >= minNotExcluded) {
+                minNotExcluded = range.to + 1
+            }
+            if (range.from > minNotExcluded) {
+                return minNotExcluded
+            }
+        }
+        return -1
     }
 
-    private fun part2(input: String): Any {
-        TODO()
+    private fun parseInput(input: List<String>) = input.map {
+        val parts = it.split("-")
+        IpRange(parts[0].toLong(), parts[1].toLong())
+    }.sorted()
+
+    private fun part2(input: List<String>): Any {
+        val ranges = parseInput(input)
+        var minNotExcluded = 0L
+        val notExcluded = mutableListOf<Long>()
+        ranges.forEach { range ->
+            if (range.from <= minNotExcluded && range.to >= minNotExcluded) {
+                minNotExcluded = range.to + 1
+            } else if (range.from > minNotExcluded) {
+                while (range.from > minNotExcluded) {
+                    notExcluded.add(minNotExcluded)
+                    ++minNotExcluded
+                }
+                minNotExcluded = range.to + 1
+            }
+        }
+        return notExcluded.size
+    }
+
+    data class IpRange(val from: Long, val to: Long) : Comparable<IpRange> {
+        override fun compareTo(other: IpRange): Int {
+            return from.compareTo(other.from)
+        }
     }
 }
