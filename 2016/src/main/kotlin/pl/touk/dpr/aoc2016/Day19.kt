b5b2f63691f95be4a3b00dc676243bf8d03da859
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-28 22:44:37 +0100

    Rewrite 19/2016

--- 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day19.kt
+++ 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day19.kt
@@ -3,16 +3,53 @@ package pl.touk.dpr.aoc2016
 object Day19 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = 3017957
         println(part1(input))
         println(part2(input))
     }
 
-    private fun part1(input: String): Any {
-        TODO()
+    private fun part1(input: Int): Any {
+        val elvesWithoutPresent = mutableSetOf<Int>()
+        var i = 0
+        while (elvesWithoutPresent.size < input - 1) {
+            val next = findNextWith(i, input, elvesWithoutPresent)
+            elvesWithoutPresent.add(next)
+            i = findNextWith(i, input, elvesWithoutPresent)
+        }
+        return i + 1
     }
 
-    private fun part2(input: String): Any {
-        TODO()
+    private fun findNextWith(cur: Int, all: Int, ewp: Set<Int>): Int {
+        var prev = cur
+        while (true) {
+            val next = (prev + 1) % all
+            if (next == cur || next !in ewp) {
+                return next
+            }
+            prev = next
+        }
     }
+
+    private fun part2(input: Int): Any {
+        val elves = ArrayList<Int>()
+        elves.addAll((1..input))
+        var i = 0
+        while (elves.size > 1) {
+            val across = findAcross(i, elves.size)
+//            println("Elves: ${elves.size},  Current $i: -> elf ${elves[i]}, Across $across: -> elf ${elves[across]}")
+            elves.removeAt(across)
+            if (across > i) {
+                ++i
+            }
+            i = if (i >= elves.size) 0 else i
+        }
+        return elves.first()
+    }
+
+    private fun findAcross(cur: Int, size: Int): Int {
+        val step = (if (size % 2 == 1) size - 1 else size) / 2
+        return (cur + step) % size
+    }
+
+    data class Node(val x:Int, val next: Node? = null, val prev: Node? = null)
 }
