75bae3388ca1a745ec7307f7e920cbd1716be5bc
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-28 11:28:56 +0100

    Rewrite 8/2016

--- 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day08.kt
+++ 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Day08.kt
@@ -3,16 +3,70 @@ package pl.touk.dpr.aoc2016
 object Day08 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
-        println(part1(input))
-        println(part2(input))
+        val input = Util.getNotEmptyLinesFromFile("/08/input.txt")
+        part1And2(input)
     }
 
-    private fun part1(input: String): Any {
-        TODO()
-    }
-
-    private fun part2(input: String): Any {
-        TODO()
+    private fun part1And2(input: List<String>) {
+        val board = (0..5).map {
+            (0..49).map { false }
+        }
+        val res = input.fold(board) { acc, command ->
+            val parts = command.split(Regex("[ xy=]+"))
+            when (parts[0]) {
+                "rect" -> {
+                    acc.mapIndexed { y, row ->
+                        acc[y].mapIndexed { x, cell ->
+                            if (x < parts[1].toInt() && y < parts[2].toInt()) {
+                                true
+                            } else {
+                                cell
+                            }
+                        }
+                    }
+                }
+                "rotate" -> {
+                    val which = parts[2].toInt()
+                    val by = parts[4].toInt()
+                    when (parts[1]) {
+                        "column" -> {
+                            acc.mapIndexed { y, row ->
+                                acc[y].mapIndexed { x, cell ->
+                                    if (x == which) {
+                                        acc[(acc.size + y - by) % acc.size][x]
+                                    } else {
+                                        cell
+                                    }
+                                }
+                            }
+                        }
+                        "row" -> {
+                            acc.mapIndexed { y, row ->
+                                acc[y].mapIndexed { x, cell ->
+                                    if (y == which) {
+                                        acc[y][(acc[y].size + x - by) % acc[y].size]
+                                    } else {
+                                        cell
+                                    }
+                                }
+                            }
+                        }
+                        else -> {
+                            acc
+                        }
+                    }
+                }
+                else -> {
+                    acc
+                }
+            }
+        }
+        println(res.flatten().count { it })
+        (0..5).forEach { y ->
+            (0..49).forEach { x ->
+                print(if (res[y][x]) '#' else ' ')
+            }
+            println()
+        }
     }
 }
