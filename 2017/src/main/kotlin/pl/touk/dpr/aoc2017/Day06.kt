518a411d673878a44ef1a871b6210d9be8822336
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-29 18:00:09 +0100

    Rewrite 6/2017

--- 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day06.kt
+++ 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day06.kt
@@ -3,16 +3,61 @@ package pl.touk.dpr.aoc2017
 object Day06 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = Util.getFileContent("/06/input.txt").trim()
         println(part1(input))
         println(part2(input))
     }
 
     private fun part1(input: String): Any {
-        TODO()
+        val blocks = input.split("\t").map { it.toInt() }.toMutableList()
+        val memory = mutableSetOf(blocks.toList())
+        var re = 0
+        while (true) {
+            re++
+            var (max, maxI) = findMax(blocks)
+            blocks[maxI] = 0
+            while (max > 0) {
+                --max
+                maxI = (maxI + 1) % blocks.size
+                blocks[maxI]++
+            }
+            if (blocks in memory) {
+                return re
+            }
+            memory.add(blocks.toList())
+        }
     }
 
+
     private fun part2(input: String): Any {
-        TODO()
+        val blocks = input.split("\t").map { it.toInt() }.toMutableList()
+        val memory = mutableMapOf(Pair(blocks.toList(), 0))
+        var re = 0
+        while (true) {
+            re++
+            var (max, maxI) = findMax(blocks)
+            blocks[maxI] = 0
+            while (max > 0) {
+                --max
+                maxI = (maxI + 1) % blocks.size
+                blocks[maxI]++
+            }
+            if (blocks in memory) {
+                return re - memory[blocks.toList()]!!
+            }
+            memory[blocks.toList()] = re
+        }
+    }
+
+    private fun findMax(b: List<Int>): Pair<Int, Int> {
+        var max = b[0]
+        var maxI = 0
+        b.forEachIndexed { index, i ->
+            if (i > max) {
+                max = i
+                maxI = index
+            }
+        }
+        return Pair(max, maxI)
     }
 }
