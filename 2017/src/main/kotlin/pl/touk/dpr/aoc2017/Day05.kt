5854c0255cca3114a81dbffe098f063ab3efb03f
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-29 17:17:19 +0100

    Rewrite 5/2017

--- 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day05.kt
+++ 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day05.kt
@@ -3,16 +3,34 @@ package pl.touk.dpr.aoc2017
 object Day05 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = Util.getNotEmptyLinesFromFile("/05/input.txt")
         println(part1(input))
         println(part2(input))
     }
 
-    private fun part1(input: String): Any {
-        TODO()
+    private fun part1(input: List<String>): Any {
+        val steps = input.map { it.toInt() }.toMutableList()
+        var cur = 0
+        var jumps = 0
+        while (cur < steps.size) {
+            jumps++
+            val next = steps[cur]
+            steps[cur] = next + 1
+            cur += next
+        }
+        return jumps
     }
 
-    private fun part2(input: String): Any {
-        TODO()
+    private fun part2(input: List<String>): Any {
+        val steps = input.map { it.toInt() }.toMutableList()
+        var cur = 0
+        var jumps = 0
+        while (cur < steps.size) {
+            jumps++
+            val next = steps[cur]
+            steps[cur] = next + if (next >= 3) -1 else 1
+            cur += next
+        }
+        return jumps
     }
 }
