f854af551fe0a3cc2d7d215aaf7f517662f5e833
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-29 13:29:27 +0100

    Rewrite 2/2017

--- 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day02.kt
+++ 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day02.kt
@@ -3,16 +3,35 @@ package pl.touk.dpr.aoc2017
 object Day02 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = Util.getNotEmptyLinesFromFile("/02/input.txt")
         println(part1(input))
         println(part2(input))
     }
 
-    private fun part1(input: String): Any {
-        TODO()
+    private fun part1(input: List<String>): Any {
+        return input
+                .map { it.split('\t').map { it.toInt() } }
+                .map { it.maxOrNull()!! - it.minOrNull()!! }
+                .sum()
     }
 
-    private fun part2(input: String): Any {
-        TODO()
+    private fun part2(input: List<String>): Any {
+        return input
+                .map { it.split('\t').map { it.toInt() }.sorted().reversed() }
+                .map { findDivisible(it) }
+                .sum()
     }
+
+    private fun findDivisible(nums: List<Int>): Int {
+        for (i in nums.indices) {
+            val first = nums[i]
+            for (j in ((i + 1) until nums.size)) {
+                if (first % nums[j] == 0) {
+                    return first / nums[j]
+                }
+            }
+        }
+        return 0
+    }
+
 }
--- 2017/src/main/resources/02/part1.groovy
+++
@@ -1,4 +0,0 @@
-def text = new File('input.txt').text
-
-List lines = text.split('\n')
-println lines.collect {it.split('\t').collect {it as int} }.collect { it.max() - it.min()}.sum()
