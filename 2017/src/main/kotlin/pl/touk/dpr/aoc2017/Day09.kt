379be63ee678a5ece81fd2b8128067822aa8411c
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-30 09:39:14 +0100

    Rewrite 9/2017

--- 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day09.kt
+++ 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day09.kt
@@ -3,16 +3,33 @@ package pl.touk.dpr.aoc2017
 object Day09 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
-        println(part1(input))
-        println(part2(input))
+        val input = Util.getFileContent("/09/input.txt").trim()
+        part1And2(input).forEach { println(it) }
     }
 
-    private fun part1(input: String): Any {
-        TODO()
-    }
-
-    private fun part2(input: String): Any {
-        TODO()
+    private fun part1And2(input: String): Collection<Any> {
+        var score = 0
+        var sum = 0
+        var garbage = false
+        var garbageAmount = 0
+        var i = 0
+        while (i < input.length) {
+            when (input[i]) {
+                '!' -> ++i
+                '<' -> if (!garbage) garbage = true else ++garbageAmount
+                '{' ->
+                    if (!garbage) {
+                        ++score
+                        sum += score
+                    } else {
+                        ++garbageAmount
+                    }
+                '>' -> garbage = false
+                '}' -> if (!garbage) --score else ++garbageAmount
+                else -> if (garbage) garbageAmount++
+            }
+            ++i
+        }
+        return listOf(sum, garbageAmount)
     }
 }
