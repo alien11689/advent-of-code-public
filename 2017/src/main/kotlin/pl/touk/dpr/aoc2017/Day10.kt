0a9cc34549f6569425b8b30e0b9b16be7714b7c0
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-30 09:53:39 +0100

    Rewrite 10/2017

--- 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day10.kt
+++ 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day10.kt
@@ -3,16 +3,58 @@ package pl.touk.dpr.aoc2017
 object Day10 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = Util.getFileContent("/10/input.txt").trim()
         println(part1(input))
         println(part2(input))
     }
 
     private fun part1(input: String): Any {
-        TODO()
+        val lengths = input.split(',').map { it.toInt() }
+
+        var curPos = 0
+        var skipSize = 0
+
+        val inp = (0..255).toMutableList()
+
+        lengths.forEach { length ->
+            val reversed = (0 until length).map {
+                inp[(curPos + it) % inp.size]
+            }.reversed()
+            (0 until length).forEach {
+                inp[(curPos + it) % inp.size] = reversed[it]
+            }
+            curPos = (curPos + length + skipSize) % inp.size
+            ++skipSize
+        }
+
+        return inp[0] * inp[1]
     }
 
-    private fun part2(input: String): Any {
-        TODO()
+    private fun part2(text: String): Any {
+        val lengths = text.map { it.toInt() } + listOf(17, 31, 73, 47, 23)
+
+        var curPos = 0
+        var skipSize = 0
+
+        val inp = (0..255).toMutableList()
+
+        (1..64).forEach {
+            lengths.forEach { length ->
+                val reversed = (0 until length).map {
+                    inp[(curPos + it) % inp.size]
+                }.reversed()
+                (0 until length).forEach {
+                    inp[(curPos + it) % inp.size] = reversed[it]
+                }
+                curPos = (curPos + length + skipSize) % inp.size
+                ++skipSize
+            }
+
+        }
+
+        return inp.chunked(16)
+                .map { it.reduce() { a, b -> a.xor(b) } }
+                .joinToString("") { String.format("%02x", it) }
+
     }
 }
