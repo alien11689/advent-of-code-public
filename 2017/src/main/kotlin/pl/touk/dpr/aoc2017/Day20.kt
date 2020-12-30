6846a7c46acc11985fee2faf42f918b87a84eab4
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-30 17:19:03 +0100

    Rewrite 20/2017

--- 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day20.kt
+++ 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day20.kt
@@ -1,18 +1,82 @@
 package pl.touk.dpr.aoc2017
 
+import kotlin.math.absoluteValue
+
 object Day20 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = Util.getNotEmptyLinesFromFile("/20/input.txt")
         println(part1(input))
         println(part2(input))
     }
 
-    private fun part1(input: String): Any {
-        TODO()
+    private fun part1(input: List<String>): Any {
+        val particles = readInput(input)
+
+        (0 until 1000).forEach { iter ->
+            particles.forEach {
+                it.updateV()
+                it.updateP()
+            }
+        }
+        return particles.minByOrNull { it.from0() }!!.id
     }
 
-    private fun part2(input: String): Any {
-        TODO()
+    private fun readInput(input: List<String>): List<Particle> {
+        return input.mapIndexed { index, line ->
+            val parts = line.split(Regex("[<>]"))
+            Particle(
+                    index,
+                    p = Coord.from(parts[1]),
+                    v = Coord.from(parts[3]),
+                    a = Coord.from(parts[5])
+            )
+        }
+    }
+
+    private fun part2(input: List<String>): Any {
+        var particles = readInput(input)
+        (0 until 1000).forEach { iter ->
+            val hits = mutableMapOf<Coord, Int>()
+            particles.forEach {
+                it.updateV()
+                it.updateP()
+                hits[it.p] = 1 + (hits[it.p] ?: 0)
+            }
+            val doubles = hits.filter { it.value > 1 }.keys
+            particles = particles.filter { !(it.p in doubles) }
+        }
+        return particles.size
+    }
+
+    data class Coord(val x: Int, val y: Int, val z: Int) {
+        companion object {
+            fun from(str: String): Coord {
+                val parts = str.split(",")
+                return Coord(parts[0].toInt(), parts[1].toInt(), parts[2].toInt())
+            }
+        }
+    }
+
+    data class Particle(val id: Int, var p: Coord, var v: Coord, val a: Coord) {
+        fun updateV() {
+            v = Coord(
+                    x = v.x + a.x,
+                    y = v.y + a.y,
+                    z = v.z + a.z,
+            )
+        }
+
+        fun updateP() {
+            p = Coord(
+                    x = v.x + p.x,
+                    y = v.y + p.y,
+                    z = v.z + p.z,
+            )
+        }
+
+        fun from0(): Int {
+            return p.x.absoluteValue + p.y.absoluteValue + p.z.absoluteValue
+        }
     }
 }
