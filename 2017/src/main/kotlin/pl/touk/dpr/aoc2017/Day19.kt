8aa75c2f74e765e16d24290d74611550939c144b
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-30 16:30:52 +0100

    Rewrite 19/2017

--- 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day19.kt
+++ 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day19.kt
@@ -3,16 +3,43 @@ package pl.touk.dpr.aoc2017
 object Day19 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
-        println(part1(input))
-        println(part2(input))
+        val input = Util.getNotEmptyLinesFromFile("/19/input.txt")
+        part1And2(input).forEach { println(it) }
     }
 
-    private fun part1(input: String): Any {
-        TODO()
+    private fun part1And2(grid: List<String>): Collection<Any> {
+        var dir = Pair(0, 1)
+        var cur = Pair(grid[0].indexOf('|'), 0)
+
+        val letters = mutableListOf<Char>()
+
+        var steps = 0
+        while (true) {
+            val oldCur = cur
+            ++steps
+            cur = Pair(cur.first + dir.first, cur.second + dir.second)
+            val sign = grid[cur.second][cur.first]
+            if (sign !in setOf('-', '+', '|')) {
+                letters.add(sign)
+            }
+            if (sign == '+') {
+                dir = changeDir(grid, dir, oldCur, cur)
+            }
+            if (sign == ' ') {
+                return listOf(letters.joinToString(""), steps)
+            }
+        }
     }
 
-    private fun part2(input: String): Any {
-        TODO()
+    private fun changeDir(grid: List<String>, dir: Pair<Int, Int>, oldCur: Pair<Int, Int>, cur: Pair<Int, Int>): Pair<Int, Int> {
+        return listOf(
+                Pair(cur.first + 1, cur.second),
+                Pair(cur.first - 1, cur.second),
+                Pair(cur.first, cur.second + 1),
+                Pair(cur.first, cur.second - 1)
+        ).filter { it != oldCur }
+                .filter { grid[it.second][it.first] != ' ' }
+                .map { Pair(it.first - cur.first, it.second - cur.second) }
+                .first()
     }
 }
