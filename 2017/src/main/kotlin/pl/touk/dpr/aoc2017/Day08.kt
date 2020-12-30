8982e468d3d084caeeda10be0fa588b63cc398d1
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-30 09:28:01 +0100

    Rewrite 8/2017

--- 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day08.kt
+++ 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day08.kt
@@ -3,16 +3,56 @@ package pl.touk.dpr.aoc2017
 object Day08 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
-        println(part1(input))
-        println(part2(input))
+        val input = Util.getNotEmptyLinesFromFile("/08/input.txt")
+        part1And2(input).forEach { println(it) }
     }
 
-    private fun part1(input: String): Any {
-        TODO()
+    private fun part1And2(input: List<String>): Collection<Int> {
+        val registers = mutableMapOf<String, Int>().withDefault { 0 }
+        val highestEver = input.map { it.split(' ') }.map {
+            Instruction(
+                    register = it[0],
+                    inc = it[1] == "inc",
+                    amount = it[2].toInt(),
+                    condition = Condition(
+                            register = it[4],
+                            sign = it[5],
+                            amount = it[6].toInt()
+                    )
+            )
+        }.fold(Integer.MIN_VALUE) { acc, instruction ->
+            instruction.apply(registers, acc)
+        }
+        return listOf(registers.values.maxOrNull()!!, highestEver)
     }
 
-    private fun part2(input: String): Any {
-        TODO()
+    data class Instruction(val register: String, val inc: Boolean, val amount: Int, val condition: Condition) {
+        fun apply(reg: MutableMap<String, Int>, highestEver: Int): Int {
+            if (condition.match(reg)) {
+                val curReg = reg[register] ?: 0
+                if (inc) {
+                    reg[register] = curReg + amount
+                } else {
+                    reg[register] = curReg - amount
+                }
+                return listOf(reg[register]!!, highestEver).maxOrNull()!!
+            }
+            return highestEver
+        }
+    }
+
+    data class Condition(val register: String, val sign: String, val amount: Int) {
+        fun match(reg: Map<String, Int>): Boolean {
+            val reg = reg[register] ?: 0
+            return when (sign) {
+                "==" -> reg == amount
+                "!=" -> reg != amount
+                ">=" -> reg >= amount
+                "<=" -> reg <= amount
+                ">" -> reg > amount
+                "<" -> reg < amount
+                else -> throw RuntimeException(sign)
+            }
+        }
     }
 }
