d18aa629fc398e6ba22ccdeccda9ec9b84a5972b
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-30 14:56:37 +0100

    Rewrite 17/2017

--- 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day17.kt
+++ 2017/src/main/kotlin/pl/touk/dpr/aoc2017/Day17.kt
@@ -3,16 +3,44 @@ package pl.touk.dpr.aoc2017
 object Day17 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
-        println(part1(input))
-        println(part2(input))
+        println(part1())
+        println(part2())
     }
 
-    private fun part1(input: String): Any {
-        TODO()
+    private fun part1(): Any {
+        val input = 344
+
+        val buf = mutableListOf(0)
+        var cur = 0
+
+        (1..2017).forEach { i ->
+            var curInput = input % buf.size
+            while (curInput > 0) {
+                cur = (cur + 1) % buf.size
+                --curInput
+            }
+            buf.add(++cur, i)
+        }
+        return buf[cur + 1]
     }
 
-    private fun part2(input: String): Any {
-        TODO()
+    private fun part2(): Any {
+        val input = 344
+        val amount = 50000000
+
+        var size = 1
+        var cur = 0
+        var nextToZero = -1
+
+        (1..amount).forEach { i ->
+            val curInput = input % size
+            cur = (cur + curInput) % size
+            ++cur
+            ++size
+            if (cur == 1) {
+                nextToZero = i
+            }
+        }
+        return nextToZero
     }
 }
