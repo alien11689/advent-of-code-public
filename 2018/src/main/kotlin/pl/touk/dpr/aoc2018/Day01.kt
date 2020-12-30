737e33b3260b2a1f259d1cc44e65842fbf69a325
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-30 19:24:06 +0100

    Rewrite 1/2018

--- 2018/src/main/kotlin/pl/touk/dpr/aoc2018/Day01.kt
+++ 2018/src/main/kotlin/pl/touk/dpr/aoc2018/Day01.kt
@@ -3,16 +3,28 @@ package pl.touk.dpr.aoc2018
 object Day01 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = Util.getNotEmptyLinesFromFile("/01/input.txt")
         println(part1(input))
         println(part2(input))
     }
 
-    private fun part1(input: String): Any {
-        TODO()
+    private fun part1(input: List<String>): Any {
+        return input.sumBy { it.toInt() }
     }
 
-    private fun part2(input: String): Any {
-        TODO()
+    private fun part2(input: List<String>): Any {
+        val numbers = input.map { it.toInt() }
+        val m = mutableSetOf<Int>()
+        var start = 0
+        while (true) {
+            start = numbers.fold(start) { acc, cur ->
+                val next = acc + cur
+                if (next in m) {
+                    return next
+                }
+                m.add(next)
+                next
+            }
+        }
     }
 }
--- 2018/src/main/resources/01/task1.groovy
+++
@@ -1,5 +0,0 @@
-def text = new File('input.txt').text
-
-def sum = text.split('\n').collect { it as int }.sum()
-
-println sum
