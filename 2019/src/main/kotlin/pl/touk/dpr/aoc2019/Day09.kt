0764fbf83350f3e38e24bccb8b10bfd0c7ce8a93
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-31 13:39:46 +0100

    Rewrite 9/2019

--- 2019/src/main/kotlin/pl/touk/dpr/aoc2019/Day09.kt
+++ 2019/src/main/kotlin/pl/touk/dpr/aoc2019/Day09.kt
@@ -1,18 +1,32 @@
 package pl.touk.dpr.aoc2019
 
+import pl.touk.dpr.aoc2019.intcode.IntCodeComputer
+import pl.touk.dpr.aoc2019.intcode.IntCodeComputerState
+import java.util.LinkedList
+
 object Day09 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = Util.getFileContent("/09/input.txt").trim()
         println(part1(input))
         println(part2(input))
     }
 
     private fun part1(input: String): Any {
-        TODO()
+        val v = IntCodeComputer.parseInput(input)
+        val output = LinkedList<Long>()
+        val inputQ = LinkedList<Long>()
+        inputQ.offer(1L)
+        IntCodeComputer.program(IntCodeComputerState(v, input = inputQ), output)
+        return output.last
     }
 
     private fun part2(input: String): Any {
-        TODO()
+        val v = IntCodeComputer.parseInput(input)
+        val output = LinkedList<Long>()
+        val inputQ = LinkedList<Long>()
+        inputQ.offer(2L)
+        IntCodeComputer.program(IntCodeComputerState(v, input = inputQ), output)
+        return output.last
     }
 }
