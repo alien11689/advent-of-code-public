0bc3f9923fb01b7572625a6e914f2bb720a52e5e
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-31 13:56:06 +0100

    Rewrite 1/2019

--- 2019/src/main/kotlin/pl/touk/dpr/aoc2019/Day01.kt
+++ 2019/src/main/kotlin/pl/touk/dpr/aoc2019/Day01.kt
@@ -3,16 +3,33 @@ package pl.touk.dpr.aoc2019
 object Day01 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = Util.getNotEmptyLinesFromFile("/01/input.txt")
         println(part1(input))
         println(part2(input))
     }
 
-    private fun part1(input: String): Any {
-        TODO()
+    private fun part1(input: List<String>): Any {
+        return input.fold(0) { a, v ->
+            a + calculateFuel(v.toInt())
+        }
     }
 
-    private fun part2(input: String): Any {
-        TODO()
+    private fun part2(input: List<String>): Any {
+        var sum = 0
+        input.forEach { vv ->
+            var v = vv.toInt()
+            while (v > 0) {
+                val newV = calculateFuel(v)
+                if (newV > 0) {
+                    sum += newV
+                }
+                v = newV
+            }
+        }
+        return sum
+    }
+
+    fun calculateFuel(value: Int): Int {
+        return value / 3 - 2
     }
 }
