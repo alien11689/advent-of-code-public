6d54b6cbbb84a557f35c9f1018f2bc13af3439ec
Dominik Adam Przybysz <alien11689@gmail.com>
2020-12-31 10:56:13 +0100

    Add more info to count to rewrite

--- countToRewrite.sh
+++ countToRewrite.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo -n "Days: "
+for x in `find . -name "*.groovy" | grep -v target`; do wc -l $x ;done | sort -k 2 | grep "2018\|2019" | cut -d\  -f2 | cut -d/ -f 2,6 | sort -u | wc -l
+
 echo -n "Files: "
 find . -name "*.groovy" | grep -v target| wc -l
 echo -n "Lines: "
 for x in `find . -name "*.groovy" | grep -v target`; do cat $x; done | wc -l
+
