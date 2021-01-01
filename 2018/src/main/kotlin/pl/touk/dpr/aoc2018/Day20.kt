6a7316d7808787498ecc019b518d59206a14a353
Dominik Adam Przybysz <alien11689@gmail.com>
2021-01-01 11:10:04 +0100

    Rewrite 20/2018

--- 2018/src/main/kotlin/pl/touk/dpr/aoc2018/Day20.kt
+++ 2018/src/main/kotlin/pl/touk/dpr/aoc2018/Day20.kt
@@ -1,18 +1,68 @@
 package pl.touk.dpr.aoc2018
 
+import java.util.Stack
+
 object Day20 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
-        println(part1(input))
-        println(part2(input))
+        val input = Util.getFileContent("/20/input.txt").trim()
+        part1And2(input).forEach { println(it) }
     }
 
-    private fun part1(input: String): Any {
-        TODO()
+    private fun part1And2(input: String): Collection<Any> {
+        val cross = Stack<Pos>()
+        val posToDist = mutableMapOf<Pos, Int>()
+        var curPos = Pos(0, 0)
+        posToDist[curPos] = 0
+        input.substring(1).forEach { c ->
+            if (c == '(') {
+                cross.push(curPos)
+            } else if (c == '$') {
+            } else if (c == ')') {
+                curPos = cross.pop()
+            } else if (c == '|') {
+                curPos = cross.peek()
+            } else {
+                val distance = posToDist[curPos]!!
+                curPos = when (c) {
+                    'E' -> curPos.E()
+                    'W' -> curPos.W()
+                    'N' -> curPos.N()
+                    'S' -> curPos.S()
+                    else -> throw RuntimeException()
+                }
+                if (posToDist[curPos] != null) {
+                    if (posToDist[curPos]!! > distance + 1) {
+                        posToDist[curPos] = distance + 1
+                    }
+                } else {
+                    posToDist[curPos] = distance + 1
+                }
+            }
+        }
+
+        return listOf(
+            posToDist.values.maxOrNull()!!,
+            posToDist.values.count { it >= 1000 }
+        )
+
     }
 
-    private fun part2(input: String): Any {
-        TODO()
+    data class Pos(val x: Int, val y: Int) {
+        fun E(): Pos {
+            return Pos(x + 1, y)
+        }
+
+        fun W(): Pos {
+            return Pos(x - 1, y)
+        }
+
+        fun N(): Pos {
+            return Pos(x, y - 1)
+        }
+
+        fun S(): Pos {
+            return Pos(x, y + 1)
+        }
     }
 }
