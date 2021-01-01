449708c2fb5147efd78c2c7a571c531dfcc7365b
Dominik Adam Przybysz <alien11689@gmail.com>
2021-01-01 11:21:27 +0100

    Rewrite 16/2019

--- 2019/src/main/kotlin/pl/touk/dpr/aoc2019/Day16.kt
+++ 2019/src/main/kotlin/pl/touk/dpr/aoc2019/Day16.kt
@@ -1,18 +1,83 @@
 package pl.touk.dpr.aoc2019
 
+import kotlin.math.absoluteValue
+
 object Day16 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = Util.getFileContent("/01/input.txt").trim()
+        val input = Util.getFileContent("/16/input.txt").trim()
         println(part1(input))
         println(part2(input))
     }
 
     private fun part1(input: String): Any {
-        TODO()
+        var nums = input.map { it.toString().toInt() }
+        val phases = 100
+
+        val basePattern = listOf(0, 1, 0, -1)
+
+        var p = 0
+
+        while (p < phases) {
+            ++p
+            var iter = 1
+            val output = mutableListOf<Int>()
+            while (output.size < nums.size) {
+                val pattern = generatePattern(basePattern, iter)
+                var sum = 0
+                for (i in nums.indices) {
+                    sum += nums[i] * pattern[(i + 1) % pattern.size]
+                }
+                output.add(sum.absoluteValue % 10)
+                ++iter
+            }
+            //println("$output")
+            nums = output
+        }
+        return nums.subList(0, 8).joinToString("")
+
+    }
+
+    private fun generatePattern(base: List<Int>, iter: Int): List<Int> {
+        val pattern = mutableListOf<Int>()
+        base.forEach { p ->
+            (0 until iter).forEach() {
+                pattern.add(p)
+            }
+        }
+        return pattern
     }
 
     private fun part2(input: String): Any {
-        TODO()
+        var nums = input.map { it.toString().toLong() }
+        var offset = nums.subList(0, 7).dropWhile { it == 0L }.joinToString("").toInt()
+
+        val times = 10000 - offset / nums.size
+
+        val phases = 100
+        offset = offset % nums.size
+
+        val realInput = mutableListOf<Long>()
+        (0 until times).forEach() {
+            realInput.addAll(nums)
+        }
+
+        nums = realInput
+
+        var p = 0
+
+        while (p < phases) {
+            ++p
+            var sum = nums.fold(0L) { a, b -> a + b }
+            val output = mutableListOf<Long>()
+            for(i in nums.indices){
+                if (i > 0) {
+                    sum -= nums[i - 1]
+                }
+                output.add(sum % 10)
+            }
+            nums = output
+        }
+        return nums.subList(offset, offset + 8).joinToString("")
     }
 }
