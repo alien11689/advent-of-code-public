b3b3dc199ece92a450716dfd1891a074e2140b31
Dominik Przybysz <dpr@touk.pl>
2021-12-01 06:48:56 +0100

    Prepare for day 02

---
+++ 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day02.kt
@@ -0,0 +1,10 @@
+package pl.touk.dpr.aoc2021
+
+object Day02 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val lines = Util.getNotEmptyLinesFromFile("/02/input.txt")
+        println(lines)
+    }
+}
+
