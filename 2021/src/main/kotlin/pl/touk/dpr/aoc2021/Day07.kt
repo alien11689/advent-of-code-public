fad03868b48c6d3f4d225aa96c759710ff7e2602
Dominik Przybysz <dpr@touk.pl>
2021-12-07 06:31:34 +0100

    Finish 07 and prepare for 08

--- 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day07.kt
+++ 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day07.kt
@@ -1,5 +1,7 @@
 package pl.touk.dpr.aoc2021
 
+import kotlin.math.absoluteValue
+
 object Day07 {
     @JvmStatic
     fun main(args: Array<String>) {
@@ -8,12 +10,40 @@ object Day07 {
         println(part2(lines))
     }
 
-    private fun part1(lines: List<String>): Int {
-        return -1
+    private fun part1(lines: List<String>): Long {
+        val positions = lines[0].split(",").map { it.toInt() }
+        var minFuel = positions.maxOrNull()!!.toLong() * positions.size
+        for (i in positions.minOrNull()!!..positions.maxOrNull()!!) {
+            val fuel = positions.map { (it - i).absoluteValue.toLong() }.sum()
+            if (fuel < minFuel) {
+                minFuel = fuel
+                println("On $i there is $fuel")
+            }
+        }
+        return minFuel
     }
 
-    private fun part2(lines: List<String>): Int {
-        return -1
+    private fun part2(lines: List<String>): Long {
+        val positions = lines[0].split(",").map { it.toInt() }
+        var minFuel: Long? = null
+        for (i in positions.minOrNull()!!..positions.maxOrNull()!!) {
+            var fuel = 0L
+            for (p in positions) {
+                if (i == p) {
+                    continue
+                }
+                var cur = 0
+                for (x in 1..(p - i).absoluteValue) {
+                    ++cur
+                    fuel += cur
+                }
+            }
+            if (minFuel == null || fuel < minFuel) {
+                minFuel = fuel
+                println("On $i there is $fuel")
+            }
+        }
+        return minFuel!!
     }
 
 }
---
+++ 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day08.kt
@@ -0,0 +1,20 @@
+package pl.touk.dpr.aoc2021
+
+object Day08 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val lines = Util.getNotEmptyLinesFromFile("/08/input.txt")
+        println(part1(lines))
+        println(part2(lines))
+    }
+
+    private fun part1(lines: List<String>): Long {
+        return -1
+    }
+
+    private fun part2(lines: List<String>): Long {
+        return -1
+    }
+
+}
+
