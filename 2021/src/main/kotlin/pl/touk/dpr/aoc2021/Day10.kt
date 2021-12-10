08740c2f95193367adeb03e9e1aea97c444e3cc1
Dominik Przybysz <dpr@touk.pl>
2021-12-10 06:33:24 +0100

    Finish 10 and prepare for 11

--- 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day10.kt
+++ 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day10.kt
@@ -1,19 +1,80 @@
 package pl.touk.dpr.aoc2021
 
+import java.util.Stack
+
 object Day10 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val lines = Util.getNotEmptyLinesFromFile("/09/input.txt")
+        val lines = Util.getNotEmptyLinesFromFile("/10/input.txt")
         println(part1(lines))
         println(part2(lines))
     }
 
     private fun part1(lines: List<String>): Any {
-        return -1
+        return lines.map { findIllegalCaracter(it).second }.mapNotNull {
+            when (it) {
+                ')' -> 3L
+                '}' -> 1197L
+                ']' -> 57L
+                '>' -> 25137L
+                else -> 0L
+            }
+        }.sum()
+    }
+
+    private fun findIllegalCaracter(line: String): Pair<Stack<Char>, Char?> {
+        val stack = Stack<Char>()
+        line.forEach {
+            when {
+                it in setOf('(', '[', '{', '<') -> stack.push(it)
+                it == ')' -> {
+                    val last = stack.peek()
+                    if (last != '(') return Pair(stack, it)
+                    stack.pop()
+                }
+                it == '}' -> {
+                    val last = stack.peek()
+                    if (last != '{') return Pair(stack, it)
+                    stack.pop()
+                }
+                it == '>' -> {
+                    val last = stack.peek()
+                    if (last != '<') return Pair(stack, it)
+                    stack.pop()
+                }
+                it == ']' -> {
+                    val last = stack.peek()
+                    if (last != '[') return Pair(stack, it)
+                    stack.pop()
+                }
+            }
+        }
+        return Pair(stack, null)
     }
 
     private fun part2(lines: List<String>): Any {
-        return -1
+        val results = lines.map { findIllegalCaracter(it) }
+            .filter { it.second == null }
+            .map { score(it.first) }
+            .sorted()
+//        println(results)
+        return results[results.size / 2]
+    }
+
+    private fun score(stack: Stack<Char>): Long {
+        var sum = 0L
+        while (stack.isNotEmpty()) {
+            val cur = stack.pop()
+            sum *= 5
+            sum += when (cur) {
+                '(' -> 1L
+                '[' -> 2L
+                '{' -> 3L
+                '<' -> 4L
+                else -> throw RuntimeException()
+            }
+        }
+        return sum
     }
 }
 
---
+++ 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day11.kt
@@ -0,0 +1,19 @@
+package pl.touk.dpr.aoc2021
+
+object Day11 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val lines = Util.getNotEmptyLinesFromFile("/11/input.txt")
+        println(part1(lines))
+        println(part2(lines))
+    }
+
+    private fun part1(lines: List<String>): Any {
+        return -1
+    }
+
+    private fun part2(lines: List<String>): Any {
+        return -1
+    }
+}
+
