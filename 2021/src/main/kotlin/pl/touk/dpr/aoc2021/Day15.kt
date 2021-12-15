2e6cb258d136477fdcd491c5fc56f2807e46cb4d
Dominik Przybysz <dpr@touk.pl>
2021-12-15 06:59:03 +0100

    Working part 1 of 15, not working part 2

--- 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day15.kt
+++ 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day15.kt
@@ -1,5 +1,8 @@
 package pl.touk.dpr.aoc2021
 
+import java.util.PriorityQueue
+import kotlin.math.absoluteValue
+
 object Day15 {
     @JvmStatic
     fun main(args: Array<String>) {
@@ -9,11 +12,96 @@ object Day15 {
     }
 
     private fun part1(lines: List<String>): Any {
+        val numbers = lines.map { it.map { it.toString().toInt() } }
+        val init = Pos(0, 0)
+        val pq = PriorityQueue<Path>()
+        val maxX = numbers[0].indices.last
+        val maxY = numbers.indices.last
+        val dest = Pos(maxX, maxY)
+        pq.offer(Path(init, 0, dest))
+        var maxRisk = -1
+        val visited = mutableSetOf<Pos>()
+        while (!pq.isEmpty()) {
+            val cur = pq.poll()
+            if (cur.pos == dest) {
+                return cur.risk
+            }
+            visited.add(cur.pos)
+            if (cur.risk > maxRisk) {
+                maxRisk = cur.risk
+//                println("Current risk is ${cur.risk}")
+            }
+            neigh(cur.pos.x, cur.pos.y)
+                .filter { it.x in (0..maxX) && it.y in (0..maxY) }
+                .filter { it !in visited }
+                .forEach {
+                    pq.offer(Path(it, cur.risk + numbers[it.y][it.x], dest))
+                }
+        }
         return -1
     }
 
+    data class Pos(val x: Int, val y: Int) {
+        fun manhattan(dest: Pos): Int {
+            return (dest.x - x).absoluteValue + (dest.y - y).absoluteValue
+        }
+    }
+
+    data class Path(val pos: Pos, val risk: Int = 0, val dest: Pos) : Comparable<Path> {
+        override fun compareTo(other: Path): Int {
+            val riskCompare = risk.compareTo(other.risk)
+            return if (riskCompare != 0) riskCompare else pos.manhattan(dest).compareTo(other.pos.manhattan(dest))
+        }
+    }
+
+    private fun neigh(i: Int, j: Int): Set<Pos> {
+        return setOf(
+            Pos(i, j + 1),
+            Pos(i, j - 1),
+            Pos(i - 1, j),
+            Pos(i + 1, j),
+        )
+    }
+
     private fun part2(lines: List<String>): Any {
+        val board = Board(lines.map { it.map { it.toString().toInt() } })
+        val init = Pos(0, 0)
+        val pq = PriorityQueue<Path>()
+        val maxX = lines[0].length * 5 - 1
+        val maxY = lines.size * 5 - 1
+        val dest = Pos(maxX, maxY)
+        pq.offer(Path(init, 0, dest))
+        var maxRisk = -1
+        val visited = mutableSetOf<Pos>()
+        while (!pq.isEmpty()) {
+            val cur = pq.poll()
+            if (cur.pos == dest) {
+                return cur.risk
+            }
+            visited.add(cur.pos)
+            if (cur.risk > maxRisk) {
+                maxRisk = cur.risk
+                println("Current risk is ${cur.risk}")
+            }
+            neigh(cur.pos.x, cur.pos.y)
+                .filter { it.x in (0..maxX) && it.y in (0..maxY) }
+                .filter { it !in visited }
+                .forEach {
+                    pq.offer(Path(it, cur.risk + board.valueOn(it), dest))
+                }
+        }
         return -1
+    }
+
+    data class Board(val init: List<List<Int>>) {
+        fun valueOn(pos: Pos): Int {
+            val initX = pos.x % init.size
+            val rX = pos.x / init.size
+            val initY = pos.y % init.size
+            val rY = pos.y / init.size
+            val normal = init[initY][initX] + rX + rY
+            return if (normal >= 10) normal % 10 + 1 else normal
+        }
     }
 }
 
