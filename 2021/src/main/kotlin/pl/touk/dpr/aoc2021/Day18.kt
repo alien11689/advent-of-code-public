03b5dd71cb7852a82220ff430a8519a164e497ed
Dominik Przybysz <dpr@touk.pl>
2021-12-18 10:01:47 +0100

    Second approach

--- 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day18.kt
+++ 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day18.kt
@@ -1,86 +1,133 @@
 package pl.touk.dpr.aoc2021
 
-import java.util.Stack
-
 object Day18 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val lines = Util.getNotEmptyLinesFromFile("/18/input1.txt")
+        val lines = Util.getNotEmptyLinesFromFile("/18/input6.txt")
         println(part1(lines))
         println(part2(lines))
     }
 
     private fun part1(lines: List<String>): Any {
         lines.map { line -> readSnumber(line) }
-//            .map { snumber -> snumber.reduce() }
-            .forEach(::println)
+            .map { snumbers ->
+                fullReduce(snumbers)
+            }
+            .reduce { first, second ->
+                val mm = mutableListOf<Snumber>()
+                mm.add(Snumber.LeftParent)
+                mm.addAll(first)
+                mm.addAll(second)
+                mm.add(Snumber.RightParent)
+                val res = fullReduce(mm)
+                printExpr(res)
+                res
+            }
+//            .forEach(::println)
 
         return -1
     }
 
-    private fun readSnumber(line: String): Snumber {
-        var i = 0
-        val stack = Stack<Snumber>()
-        while (i < line.length) {
-            if (line[i] == '[') {
-                stack.add(Snumber(null, listOf()))
-            } else if (line[i] == ']') {
-                val cur = stack.pop()
-                if (stack.isEmpty()) {
-                    println("line $line => $cur")
-                    break
-                } else {
-                    val parent = stack.pop()
-                    stack.push(parent.copy(values = parent.values + cur))
-                }
-            } else if (line[i] == ',') {
-                val cur = stack.pop()
-                val parent = stack.pop()
-                stack.push(parent.copy(values = parent.values + cur))
-            } else { // numbers
-                val n = line[i].toString().toInt()
-                stack.push(Snumber(n))
+    private fun printExpr(res: MutableList<Snumber>) {
+        println(res.joinToString(",").replace(",]", "]").replace("[,", "["))
+    }
+
+    private fun fullReduce(snumbers: MutableList<Snumber>): MutableList<Snumber> {
+        var before = snumbers.toList()
+        while (true) {
+            reduce(snumbers)
+            val after = snumbers.toList()
+            if (after == before) {
+                break
+            } else {
+                before = after
             }
-            ++i
         }
-        return stack.pop()
+        return snumbers
     }
 
     private fun part2(lines: List<String>): Any {
         return -1
     }
 
-    data class Snumber(val value: Int?, val values: List<Snumber> = listOf(), val parent: Snumber? = null) {
-        override fun toString(): String {
-            if (value != null) {
-                return value.toString()
-            } else {
-                return values.joinToString(",", "[", "]")
+    sealed interface Snumber {
+        object LeftParent : Snumber {
+            override fun toString(): String {
+                return "["
             }
         }
 
-        fun isPair(): Boolean {
-            return value == null && values.size == 2
+        object RightParent : Snumber {
+            override fun toString(): String {
+                return "]"
+            }
         }
 
-        fun isValue(): Boolean = value != null
+        data class Num(var n: Int) : Snumber {
+            override fun toString(): String {
+                return n.toString()
+            }
+        }
+    }
 
-        fun reduce(nestCount: Int = 0, left: Int? = null, right: Int? = null): Pair<Snumber, Boolean> {
-            if (value != null) {
-                if (value >= 10) {
-                    return Pair(Snumber(null, listOf(Snumber(value / 2), Snumber((value + 1) / 2))), true)
+    private fun readSnumber(line: String): MutableList<Snumber> {
+        return line.mapNotNull { c ->
+            when (c) {
+                '[' -> Snumber.LeftParent
+                ']' -> Snumber.RightParent
+                ',' -> null
+                else -> Snumber.Num(c.toString().toInt())
+            }
+        }.toMutableList()
+    }
+
+    private fun reduce(snumbers: MutableList<Snumber>) {
+        print("Input: ")
+        printExpr(snumbers)
+        var i = 0
+        var parenCount = 0
+        while (i < snumbers.size) {
+            val cur = snumbers[i]
+            if (cur == Snumber.LeftParent) {
+                ++parenCount
+                ++i
+            } else if (cur == Snumber.RightParent) {
+                --parenCount
+                ++i
+            } else if (cur is Snumber.Num) { //number
+                if (cur.n >= 10) {
+                    println("Split $cur")
+                    snumbers.removeAt(i)
+                    snumbers.addAll(i, listOf(Snumber.LeftParent, Snumber.Num(cur.n / 2), Snumber.Num((cur.n + 1) / 2), Snumber.RightParent))
+                    return
                 } else {
-                    return Pair(this, false)
+                    if (parenCount > 4) {
+                        val rawNext = snumbers[i + 1]
+                        if (rawNext !is Snumber.Num) {
+                            ++i
+                            continue
+                        }
+                        val next = snumbers[i + 1] as Snumber.Num
+                        println("Explode on parentLevel $parenCount: [$cur,$next]")
+                        val firstNumLeft = snumbers.take(i - 1).filter { it is Snumber.Num }.lastOrNull() as Snumber.Num?
+                        if (firstNumLeft != null) {
+                            firstNumLeft.n += cur.n
+                        }
+                        val firstNumRight = snumbers.drop(i + 3).filter { it is Snumber.Num }.firstOrNull() as Snumber.Num?
+                        if (firstNumRight != null) {
+                            firstNumRight.n += next.n
+                        }
+                        snumbers.removeAt(i + 2)
+                        snumbers.removeAt(i + 1)
+                        snumbers.removeAt(i)
+                        snumbers.removeAt(i - 1)
+                        snumbers.add(i - 1, Snumber.Num(0))
+                        return
+                    } else {
+                        ++i
+                    }
                 }
-            } else {
-                var changed = false
-                for (i in values.indices) {
-
-                    if (changed)
-                }
-                if (cha)
             }
-            TODO("Not yet implemented")
         }
     }
 }
