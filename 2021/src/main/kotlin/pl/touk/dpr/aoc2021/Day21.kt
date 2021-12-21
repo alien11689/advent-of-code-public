068365c64c928242e41dacf046a9c9e4e78c1f35
Dominik Przybysz <dpr@touk.pl>
2021-12-21 07:04:24 +0100

    Part 2 looks fine but it is wrong for example...

--- 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day21.kt
+++ 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day21.kt
@@ -1,5 +1,7 @@
 package pl.touk.dpr.aoc2021
 
+import java.util.PriorityQueue
+
 object Day21 {
     @JvmStatic
     fun main(args: Array<String>) {
@@ -54,27 +56,60 @@ object Day21 {
 
     private fun part2(lines: List<String>): Any {
 // test
-//        val player1 = Player(1, 3, 0) // one less pos
-//        val player2 = Player(2, 7, 0) // one less pos
+//        val player1 = PlayerV2(1, 3, 0) // one less pos
+//        val player2 = PlayerV2(2, 7, 0) // one less pos
         // my
-        val player1 = Player(1, 2, 0) // one less pos
-        val player2 = Player(2, 6, 0) // one less pos
+        val player1 = PlayerV2(1, 2, 0) // one less pos
+        val player2 = PlayerV2(2, 6, 0) // one less pos
 
-        val deterministicDice = DeterministicDie()
-        while (player1.score < 1000 || player2.score < 1000) {
-            val rolls1 = listOf(deterministicDice.roll(), deterministicDice.roll(), deterministicDice.roll()).sum()
-            player1.move(rolls1)
-            if (player1.score >= 1000) {
-                return 1L * player2.score * deterministicDice.rolls
-            }
+        val wins = mutableMapOf(1 to 0L, 2 to 0L)
+        val finalScore = 21
+        val pq = PriorityQueue<Game>()
+        pq.offer(Game(listOf(player1, player2)))
+        while (!pq.isEmpty()) {
+            val game = pq.poll()
+            val player1 = game.players.first()
+            val player2 = game.players.last()
+            val rollsToUniverses = mapOf(
+                3 to 1,
+                4 to 3,
+                5 to 6,
+                6 to 7,
+                7 to 6,
+                8 to 3,
+                9 to 1
+            )
 
-            val rolls2 = listOf(deterministicDice.roll(), deterministicDice.roll(), deterministicDice.roll()).sum()
-            player2.move(rolls2)
-            if (player2.score >= 1000) {
-                return 1L * player1.score * deterministicDice.rolls
+            for (rolls in rollsToUniverses) {
+                val newP1 = player1.addRolls(rolls.key, rolls.value)
+                if (newP1.score >= finalScore) {
+                    wins[newP1.id] = wins[newP1.id]!! + newP1.universes
+                    println(wins)
+                } else {
+                    pq.offer(Game(listOf(player2, newP1)))
+                }
             }
         }
-        return -1
+
+        return wins.values.maxOrNull()!!
+    }
+
+    data class Game(val players: List<PlayerV2>) : Comparable<Game> {
+        val fullScore = players.sumOf { it.score }
+
+        override fun compareTo(other: Game): Int {
+            return -1 * (fullScore.compareTo(other.fullScore))
+        }
+
+    }
+
+    data class PlayerV2(val id: Int, val pos: Int, val score: Int, val universes: Long = 1) {
+        fun addRolls(rolls: Int, inUniverses: Int): PlayerV2 {
+            val newPos = (pos + rolls) % 10
+            val newScore = score + newPos + 1
+            val newUniverses = universes * inUniverses
+            return copy(pos = newPos, score = newScore, universes = newUniverses)
+        }
     }
 }
 
