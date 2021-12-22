c5ab72f0f232f63d434c7d41a3bdf2ad7f65720f
Dominik Przybysz <dpr@touk.pl>
2021-12-22 14:28:11 +0100

    Fix print answers

--- 2021/printAnswers.sh
+++ 2021/printAnswers.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-grep -ir "answer was" src/main/resources | cut -d ">" -f3 | cut -d "<" -f1
+
+for x in `seq 1 9`; do
+  echo "Day $x"
+  grep -ir "answer was" src/main/resources/0$x | cut -d ">" -f3 | cut -d "<" -f1
+done
+
+for x in `seq 10 25`; do
+  echo "Day $x"
+  grep -ir "answer was" src/main/resources/$x | cut -d ">" -f3 | cut -d "<" -f1
+done
+
