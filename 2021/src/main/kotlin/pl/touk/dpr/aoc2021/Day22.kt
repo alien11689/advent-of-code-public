f0b3ff3afca4f8725b67ae18e5d6a213885000e2
Dominik Przybysz <dpr@touk.pl>
2021-12-22 06:26:04 +0100

    too slow part 1 of 22

--- 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day22.kt
+++ 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day22.kt
@@ -9,13 +9,50 @@ object Day22 {
     }
 
     private fun part1(lines: List<String>): Any {
+        val init = setOf<Triple<Int, Int, Int>>()
+        var step = 1
+        val cubes = lines.map { it.split(" ", ",", ".", "=") }
+            .map {
+                Instr(
+                    if (it[0] == "on") Oper.on else Oper.off,
+                    it[2].toInt()..it[4].toInt(),
+                    it[6].toInt()..it[8].toInt(),
+                    it[10].toInt()..it[12].toInt(),
+                )
+            }
+            .fold(init) { acc, instr ->
+                val newS = if (instr.oper == Oper.off) {
+                    acc.filterNot { it.first in instr.xRange && it.second in instr.yRange && it.third in instr.zRange }.toSet()
+                } else {
+                    acc + instr.generateTriples()
+                }
+//                println(newS)
+                println("On is ${newS.size} on ${++step}")
 
-        return -1
+                newS
+            }
+        return cubes.size
     }
 
     private fun part2(lines: List<String>): Any {
 
         return -1
+    }
+
+    enum class Oper { on, off }
+
+    data class Instr(val oper: Oper, val xRange: IntRange, val yRange: IntRange, val zRange: IntRange) {
+        fun generateTriples(): Set<Triple<Int, Int, Int>> {
+            val points = mutableSetOf<Triple<Int, Int, Int>>()
+            for (x in xRange) {
+                for (y in yRange) {
+                    for (z in zRange) {
+                        points.add(Triple(x, y, z))
+                    }
+                }
+            }
+            return points
+        }
     }
 }
 
