8c18c163b933eb332d53f5b7ddfc1b9a6bba1337
Dominik Przybysz <dpr@touk.pl>
2021-12-22 15:40:58 +0100

    Refactor 22

--- 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day22.kt
+++ 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day22.kt
@@ -1,7 +1,5 @@
 package pl.touk.dpr.aoc2021
 
-typealias Cubicle = Triple<IntRange, IntRange, IntRange>
-
 object Day22 {
     @JvmStatic
     fun main(args: Array<String>) {
@@ -30,24 +28,24 @@ object Day22 {
                 } else {
                     val curCub = instr.cubicle
                     cubicles = cubicles.flatMap { oldCubicle ->
-                        fullSplit(oldCubicle, curCub)
+                        oldCubicle.fullSplit(curCub)
                     }.toSet()
                     if (instr.oper == Oper.on) {
-                        cubicles = cubicles.filter { !contains(curCub, it) }.toSet() + curCub
+                        cubicles = cubicles.filter { !curCub.contains(it) }.toSet() + curCub
                     } else {
-                        cubicles = cubicles.filter { !contains(curCub, it) }.toSet()
+                        cubicles = cubicles.filter { !curCub.contains(it) }.toSet()
                     }
                 }
 //                println("Cubicles size is ${cubicles.size} and volume is ${cubicles.sumOf { volume(it) }}")
             }
-        return cubicles.sumOf { volume(it) }
+        return cubicles.sumOf { it.volume() }
     }
 
     private fun readInstructions(lines: List<String>) = lines.map { it.split(" ", ",", ".", "=") }
         .map {
             Instr(
                 if (it[0] == "on") Oper.on else Oper.off,
-                Triple(
+                Cubicle(
                     it[2].toInt()..it[4].toInt(),
                     it[6].toInt()..it[8].toInt(),
                     it[10].toInt()..it[12].toInt()
@@ -58,84 +56,86 @@ object Day22 {
     enum class Oper { on, off }
 
     data class Instr(val oper: Oper, val cubicle: Cubicle) {
-        fun isInit(): Boolean = contains(initCubicle, cubicle)
+        fun isInit(): Boolean = initCubicle.contains(cubicle)
 
         companion object {
             val initCubicle = Cubicle(-50..50, -50..50, -50..50)
         }
     }
 
-    fun splitX(cubicle: Cubicle, x: Int): Set<Cubicle> {
-        return if (cubicle.first.contains(x)) {
-            setOf(
-                cubicle.copy(first = cubicle.first.first..(x - 1)),
-                cubicle.copy(first = x..x),
-                cubicle.copy(first = (x + 1)..cubicle.first.last),
-            )
-                .filterNot { it.first.isEmpty() }
+    data class Cubicle(val xRange: IntRange, val yRange: IntRange, val zRange: IntRange) {
+        fun volume(): Long =
+            1L * (xRange.last - xRange.first + 1) *
+                    (yRange.last - yRange.first + 1) *
+                    (zRange.last - zRange.first + 1)
+
+        fun contains(other: Cubicle): Boolean {
+            return this.xRange.contains(other.xRange.first) && this.xRange.contains(other.xRange.last)
+                    && this.yRange.contains(other.yRange.first) && this.yRange.contains(other.yRange.last)
+                    && this.zRange.contains(other.zRange.first) && this.zRange.contains(other.zRange.last)
+        }
+
+        fun overLap(other: Cubicle): Boolean {
+            return (this.xRange.contains(other.xRange.first) || this.xRange.contains(other.xRange.last) || other.xRange.contains(this.xRange.first) || other.xRange.contains(this.xRange.last))
+                    && (this.yRange.contains(other.yRange.first) || this.yRange.contains(other.yRange.last) || other.yRange.contains(this.yRange.first) || other.yRange.contains(this.yRange.last))
+                    && (this.zRange.contains(other.zRange.first) || this.zRange.contains(other.zRange.last) || other.zRange.contains(this.zRange.first) || other.zRange.contains(this.zRange.last))
+        }
+
+        fun splitX(x: Int): Set<Cubicle> {
+            return if (xRange.contains(x)) {
+                setOf(
+                    copy(xRange = xRange.first..(x - 1)),
+                    copy(xRange = x..x),
+                    copy(xRange = (x + 1)..xRange.last),
+                )
+                    .filterNot { it.xRange.isEmpty() }
+                    .toSet()
+            } else {
+                setOf(this)
+            }
+        }
+
+        fun splitY(y: Int): Set<Cubicle> {
+            return if (yRange.contains(y)) {
+                setOf(
+                    copy(yRange = yRange.first..(y - 1)),
+                    copy(yRange = y..y),
+                    copy(yRange = (y + 1)..yRange.last),
+                )
+                    .filterNot { it.yRange.isEmpty() }
+                    .toSet()
+            } else {
+                setOf(this)
+            }
+        }
+
+        fun splitZ(z: Int): Set<Cubicle> {
+            return if (zRange.contains(z)) {
+                setOf(
+                    copy(zRange = zRange.first..(z - 1)),
+                    copy(zRange = z..z),
+                    copy(zRange = (z + 1)..zRange.last),
+                )
+                    .filterNot { it.zRange.isEmpty() }
+                    .toSet()
+            } else {
+                setOf(this)
+            }
+        }
+
+        fun fullSplit(other: Cubicle): Set<Cubicle> {
+            if (!overLap(other)) {
+                return setOf(this)
+            }
+            return setOf(this)
+                .flatMap { it.splitX(other.xRange.first) }
+                .flatMap { it.splitX(other.xRange.last) }
+                .flatMap { it.splitY(other.yRange.first) }
+                .flatMap { it.splitY(other.yRange.last) }
+                .flatMap { it.splitZ(other.zRange.first) }
+                .flatMap { it.splitZ(other.zRange.last) }
                 .toSet()
-        } else {
-            setOf(cubicle)
         }
-    }
-
-    fun splitY(cubicle: Cubicle, y: Int): Set<Cubicle> {
-        return if (cubicle.second.contains(y)) {
-            setOf(
-                cubicle.copy(second = cubicle.second.first..(y - 1)),
-                cubicle.copy(second = y..y),
-                cubicle.copy(second = (y + 1)..cubicle.second.last),
-            )
-                .filterNot { it.second.isEmpty() }
-                .toSet()
-        } else {
-            setOf(cubicle)
-        }
-    }
-
-    fun splitZ(cubicle: Cubicle, z: Int): Set<Cubicle> {
-        return if (cubicle.third.contains(z)) {
-            setOf(
-                cubicle.copy(third = cubicle.third.first..(z - 1)),
-                cubicle.copy(third = z..z),
-                cubicle.copy(third = (z + 1)..cubicle.third.last),
-            )
-                .filterNot { it.third.isEmpty() }
-                .toSet()
-        } else {
-            setOf(cubicle)
-        }
-    }
-
-    fun fullSplit(cubicle: Cubicle, forCubicle: Cubicle): Set<Cubicle> {
-        if (!overLap(cubicle, forCubicle)) {
-            return setOf(cubicle)
-        }
-        return setOf(cubicle)
-            .flatMap { splitX(it, forCubicle.first.first) }
-            .flatMap { splitX(it, forCubicle.first.last) }
-            .flatMap { splitY(it, forCubicle.second.first) }
-            .flatMap { splitY(it, forCubicle.second.last) }
-            .flatMap { splitZ(it, forCubicle.third.first) }
-            .flatMap { splitZ(it, forCubicle.third.last) }
-            .toSet()
-    }
-
-    private fun overLap(c1: Cubicle, c2: Cubicle): Boolean {
-        return (c1.first.contains(c2.first.first) || c1.first.contains(c2.first.last) || c2.first.contains(c1.first.first) || c2.first.contains(c1.first.last))
-                && (c1.second.contains(c2.second.first) || c1.second.contains(c2.second.last) || c2.second.contains(c1.second.first) || c2.second.contains(c1.second.last))
-                && (c1.third.contains(c2.third.first) || c1.third.contains(c2.third.last) || c2.third.contains(c1.third.first) || c2.third.contains(c1.third.last))
-    }
-
-    fun volume(cubicle: Cubicle): Long =
-        1L * (cubicle.first.last - cubicle.first.first + 1) *
-                (cubicle.second.last - cubicle.second.first + 1) *
-                (cubicle.third.last - cubicle.third.first + 1)
-
-    fun contains(c1: Cubicle, c2: Cubicle): Boolean {
-        return c1.first.contains(c2.first.first) && c1.first.contains(c2.first.last)
-                && c1.second.contains(c2.second.first) && c1.second.contains(c2.second.last)
-                && c1.third.contains(c2.third.first) && c1.third.contains(c2.third.last)
     }
 
 }
