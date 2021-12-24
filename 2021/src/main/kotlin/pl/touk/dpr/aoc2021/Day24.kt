17ad7101620d538e5ba8c31cdc15206f1e286bf5
Dominik Przybysz <dpr@touk.pl>
2021-12-24 06:49:09 +0100

    Really too slow...

--- 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day24.kt
+++ 2021/src/main/kotlin/pl/touk/dpr/aoc2021/Day24.kt
@@ -6,21 +6,37 @@ import java.util.Queue
 object Day24 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val lines = Util.getNotEmptyLinesFromFile("/24/input3.txt")
+        val lines = Util.getNotEmptyLinesFromFile("/24/input.txt")
         println(part1(lines))
         println(part2(lines))
     }
 
     private fun part1(lines: List<String>): Any {
         val instructions = lines.map { Instruction(it.split(' ')) }
+        var n = 99999999999999L
+        while (n > 10000000000000) {
+            val res = runProgram(n.toString(), instructions)
+            val z = res['z']!!
+            if (z == 0L) {
+                return n
+            }
+//            println("For $n z is $z")
+            --n
+            while (n.toString().contains('0')) {
+                --n
+            }
+        }
+        return -1
+    }
+
+    private fun runProgram(inp: String, instructions: List<Instruction>): MutableMap<Char, Long> {
+        val inputQueue = LinkedList<Int>()
+        inp.forEach { inputQueue.offer(it.toString().toInt()) }
         val units = mutableMapOf<Char, Long>('w' to 0, 'x' to 0, 'y' to 0, 'z' to 0)
-        val inputQueue = LinkedList<Long>()
-        inputQueue.add(10)
         val res = instructions.fold(units) { acc, instr ->
             instr.run(acc, inputQueue)
         }
-        println(res)
-        return -1
+        return res
     }
 
     data class Instruction(val name: String, val op1: Char, val op2AsLong: Long?, val op2AsUnit: Char?) {
@@ -30,9 +46,9 @@ object Day24 {
             if (parts.size > 2 && parts[2] in setOf("w", "x", "y", "z")) parts[2].first() else null
         )
 
-        fun run(map: MutableMap<Char, Long>, inputQueue: Queue<Long>): MutableMap<Char, Long> {
+        fun run(map: MutableMap<Char, Long>, inputQueue: Queue<Int>): MutableMap<Char, Long> {
             when (name) {
-                "inp" -> map[op1] = inputQueue.poll()
+                "inp" -> map[op1] = inputQueue.poll().toLong()
                 "add" -> map[op1] = map[op1]!! + (if (op2AsUnit != null) map[op2AsUnit]!! else op2AsLong!!)
                 "mul" -> map[op1] = map[op1]!! * (if (op2AsUnit != null) map[op2AsUnit]!! else op2AsLong!!)
                 "div" -> map[op1] = map[op1]!! / (if (op2AsUnit != null) map[op2AsUnit]!! else op2AsLong!!)
