bf728dcad2f5ad1f5387af1d21877378f51b9778
Dominik Adam Przybysz <alien11689@gmail.com>
2021-12-24 15:33:39 +0100

    Create better run all in kt

--- 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Util.kt
+++ 2016/src/main/kotlin/pl/touk/dpr/aoc2016/Util.kt
@@ -1,5 +1,8 @@
 package pl.touk.dpr.aoc2016
 
+import java.time.Duration
+import java.util.function.Supplier
+
 object Util {
     fun getFileContent(fileName: String): String = javaClass.getResource(fileName).readText()
     fun getLinesFromFile(fileName: String): List<String> = getFileContent(fileName).lines()
@@ -11,5 +14,22 @@ object Util {
         } else {
             println("Passed $given == $expected")
         }
+    }
+
+    fun measureTimeAndPrint(r: Supplier<Any?>) {
+        val start = System.currentTimeMillis();
+        val res = r.get()
+        val end = System.currentTimeMillis();
+        if (res != null) {
+            println(res)
+        }
+        println("Finished in ${Duration.ofMillis(end - start)}")
+    }
+
+    fun measureTime(r: Runnable) {
+        val start = System.currentTimeMillis();
+        r.run()
+        val end = System.currentTimeMillis();
+        println("Finished in ${Duration.ofMillis(end - start)}")
     }
 }
---
+++ 2020/src/main/kotlin/pl/touk/dpr/aoc2020/All.kt
@@ -0,0 +1,32 @@
+package pl.touk.dpr.aoc2020
+
+object All {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Util.measureTime { Day01.main(args) }
+        Util.measureTime { Day02.main(args) }
+        Util.measureTime { Day03.main(args) }
+        Util.measureTime { Day04.main(args) }
+        Util.measureTime { Day05.main(args) }
+        Util.measureTime { Day06.main(args) }
+        Util.measureTime { Day07.main(args) }
+        Util.measureTime { Day08.main(args) }
+        Util.measureTime { Day09.main(args) }
+        Util.measureTime { Day10.main(args) }
+        Util.measureTime { Day11.main(args) }
+        Util.measureTime { Day12.main(args) }
+        Util.measureTime { Day13.main(args) }
+        Util.measureTime { Day14.main(args) }
+        Util.measureTime { Day15.main(args) }
+        Util.measureTime { Day16.main(args) }
+        Util.measureTime { Day17.main(args) }
+        Util.measureTime { Day18.main(args) }
+        Util.measureTime { Day19.main(args) }
+        Util.measureTime { Day20.main(args) }
+        Util.measureTime { Day21.main(args) }
+        Util.measureTime { Day22.main(args) }
+        Util.measureTime { Day23.main(args) }
+        Util.measureTime { Day24.main(args) }
+        Util.measureTime { Day25.main(args) }
+    }
+}
