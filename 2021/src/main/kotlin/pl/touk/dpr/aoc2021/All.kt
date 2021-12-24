b95dcce737e3e776cdbd9fa3a15c7fcf8934a249
Dominik Adam Przybysz <alien11689@gmail.com>
2021-12-24 11:51:14 +0100

    Create new run all

---
+++ 2021/src/main/kotlin/pl/touk/dpr/aoc2021/All.kt
@@ -0,0 +1,32 @@
+package pl.touk.dpr.aoc2021
+
+object All {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Util.measureTimeAndPrint { Day01.main(args) }
+        Util.measureTimeAndPrint { Day02.main(args) }
+        Util.measureTimeAndPrint { Day03.main(args) }
+        Util.measureTimeAndPrint { Day04.main(args) }
+        Util.measureTimeAndPrint { Day05.main(args) }
+        Util.measureTimeAndPrint { Day06.main(args) }
+        Util.measureTimeAndPrint { Day07.main(args) }
+        Util.measureTimeAndPrint { Day08.main(args) }
+        Util.measureTimeAndPrint { Day09.main(args) }
+        Util.measureTimeAndPrint { Day10.main(args) }
+        Util.measureTimeAndPrint { Day11.main(args) }
+        Util.measureTimeAndPrint { Day12.main(args) }
+        Util.measureTimeAndPrint { Day13.main(args) }
+        Util.measureTimeAndPrint { Day14.main(args) }
+        Util.measureTimeAndPrint { Day15.main(args) }
+        Util.measureTimeAndPrint { Day16.main(args) }
+        Util.measureTimeAndPrint { Day17.main(args) }
+        Util.measureTimeAndPrint { Day18.main(args) }
+        Util.measureTimeAndPrint { Day19.main(args) }
+        Util.measureTimeAndPrint { Day20.main(args) }
+        Util.measureTimeAndPrint { Day21.main(args) }
+        Util.measureTimeAndPrint { Day22.main(args) }
+        Util.measureTimeAndPrint { Day23.main(args) }
+        Util.measureTimeAndPrint { Day24.main(args) }
+        // Util.measureTimeAndPrint { Day25.main(args) }
+    }
+}
