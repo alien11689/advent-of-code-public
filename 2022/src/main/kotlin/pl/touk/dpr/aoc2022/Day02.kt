eba6b0b0e4e64f9609ff0d92154c4d5bf1a2ea57
Dominik Przybysz <dpr@touk.pl>
2022-12-02 06:52:58 +0100

    Day 02/2022

--- 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day02.kt
+++ 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day02.kt
@@ -3,19 +3,79 @@ package pl.touk.dpr.aoc2022
 object Day02 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val lines = Util.getNotEmptyLinesFromFile("/01/input.txt")
+        val lines = Util.getNotEmptyLinesFromFile("/02/input.txt")
         println("Part 1:")
         println(part1(lines))
         println("Part 2:")
         println(part2(lines))
     }
 
-    private fun part1(lines: List<String>): Int {
-        TODO()
+    private fun part1(lines: List<String>): Any {
+        return lines.map {
+            val (e, m) = it.split(" ")
+            val enemy = when (e) {
+                "A" -> Option.ROCK
+                "B" -> Option.PAPER
+                "C" -> Option.SCISSORS
+                else -> throw RuntimeException()
+            }
+            val me = when (m) {
+                "X" -> Option.ROCK
+                "Y" -> Option.PAPER
+                "Z" -> Option.SCISSORS
+                else -> throw RuntimeException()
+            }
+            result(enemy, me)
+        }.sum()
     }
 
-    private fun part2(lines: List<String>): Int {
-        TODO()
+    private fun result(enemy: Option, my: Option): Int {
+        return when (Pair(enemy, my)) {
+            Pair(Option.ROCK, Option.ROCK) -> 3
+            Pair(Option.ROCK, Option.PAPER) -> 6
+            Pair(Option.ROCK, Option.SCISSORS) -> 0
+            Pair(Option.PAPER, Option.ROCK) -> 0
+            Pair(Option.PAPER, Option.PAPER) -> 3
+            Pair(Option.PAPER, Option.SCISSORS) -> 6
+            Pair(Option.SCISSORS, Option.ROCK) -> 6
+            Pair(Option.SCISSORS, Option.PAPER) -> 0
+            Pair(Option.SCISSORS, Option.SCISSORS) -> 3
+            else -> throw RuntimeException()
+        } + my.score
+    }
+
+    enum class Option(val score: Int) {
+        ROCK(1), PAPER(2), SCISSORS(3),
+    }
+
+    private fun part2(lines: List<String>): Any {
+        return lines
+            .map {
+                val (e, m) = it.split(" ")
+                val enemy = when (e) {
+                    "A" -> Option.ROCK
+                    "B" -> Option.PAPER
+                    "C" -> Option.SCISSORS
+                    else -> throw RuntimeException()
+                }
+                val me = when (m) {
+                    "X" -> when (enemy) {
+                        Option.ROCK -> Option.SCISSORS
+                        Option.PAPER -> Option.ROCK
+                        Option.SCISSORS -> Option.PAPER
+                    }
+
+                    "Y" -> enemy
+                    "Z" -> when (enemy) {
+                        Option.ROCK -> Option.PAPER
+                        Option.PAPER -> Option.SCISSORS
+                        Option.SCISSORS -> Option.ROCK
+                    }
+                    else -> throw RuntimeException()
+                }
+                result(enemy, me)
+            }
+            .sum()
     }
 }
 
