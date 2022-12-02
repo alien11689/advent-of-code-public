ce1ccc8a413d9dd08aba70f637f43376faf65a9d
Dominik Przybysz <dpr@touk.pl>
2022-12-02 09:40:53 +0100

    Prepare templates for next days

--- 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day20.kt
+++ 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day20.kt
@@ -3,18 +3,18 @@ package pl.touk.dpr.aoc2022
 object Day20 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val lines = Util.getNotEmptyLinesFromFile("/01/input.txt")
+        val lines = Util.getNotEmptyLinesFromFile("/20/input.txt")
         println("Part 1:")
         println(part1(lines))
         println("Part 2:")
         println(part2(lines))
     }
 
-    private fun part1(lines: List<String>): Int {
+    private fun part1(lines: List<String>): Any {
         TODO()
     }
 
-    private fun part2(lines: List<String>): Int {
+    private fun part2(lines: List<String>): Any {
         TODO()
     }
 }
