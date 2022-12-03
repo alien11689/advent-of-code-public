69468812c350c83d357558adcdde3f981b8a159c
Dominik Przybysz <dpr@touk.pl>
2022-12-03 09:05:57 +0100

    Day 3/2022

--- 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day03.kt
+++ 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day03.kt
@@ -10,12 +10,22 @@ object Day03 {
         println(part2(lines))
     }
 
+    val priorities = ('a'..'z') + ('A'..'Z')
+
     private fun part1(lines: List<String>): Any {
-        TODO()
+        return lines.map {
+            val left = it.substring(0, it.length / 2)
+            val right = it.substring(it.length / 2)
+            val char = left.toSet().intersect(right.toSet()).first()
+            priorities.indexOf(char) + 1L
+        }.sum()
     }
 
     private fun part2(lines: List<String>): Any {
-        TODO()
+        return lines.chunked(3).map {
+            val char = it.map { it.toSet() }.reduce { a, b -> a.intersect(b) }.first()
+            priorities.indexOf(char) + 1L
+        }.sum()
     }
 }
 
