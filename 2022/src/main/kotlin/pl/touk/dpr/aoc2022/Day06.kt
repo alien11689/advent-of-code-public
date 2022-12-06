40a44df1bf948c69b120a24d6ab15161851c7842
Dominik Przybysz <dpr@touk.pl>
2022-12-06 06:41:45 +0100

    Cleanup 6/2022

--- 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day06.kt
+++ 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day06.kt
@@ -5,36 +5,25 @@ object Day06 {
     fun main(args: Array<String>) {
         val lines = Util.getNotEmptyLinesFromFile("/06/input.txt")
         println("Part 1:")
-//        println(part1(listOf("mjqjpqmgbljsphdztnvjfqwrcgsmlb")))
         println(part1(lines))
         println("Part 2:")
-//        println(part2(listOf("bvwbjplbgvbhsrlpgdmjqwftvncz")))
         println(part2(lines))
     }
 
     private fun part1(lines: List<String>): Any {
-        val signal = lines.first()
-        var i = 0
-        while (i < signal.length) {
-            val subsignal = signal.substring(i, i + 4)
-//            println("Checking $i and signal $subsignal")
-            if (subsignal.toSet().size == 4) {
-                return i + 4
-            }
-            ++i
-        }
-        return -1
+        return getFirstIndexAfterDistinctSequence(lines, 4)
     }
 
     private fun part2(lines: List<String>): Any {
+        return getFirstIndexAfterDistinctSequence(lines, 14)
+    }
+
+    private fun getFirstIndexAfterDistinctSequence(lines: List<String>, distinctSize: Int): Int {
         val signal = lines.first().toList()
         var i = 0
-        while (i < signal.size) {
-//            println("Checking $i of ${signal.size}")
-            val subsignal = signal.subList(i, i + 14)
-//            println("Checking $i and signal $subsignal")
-            if (subsignal.toSet().size == 14) {
-                return i + 14
+        signal.windowed(distinctSize, 1).forEach {
+            if (it.toSet().size == distinctSize) {
+                return i + distinctSize
             }
             ++i
         }
