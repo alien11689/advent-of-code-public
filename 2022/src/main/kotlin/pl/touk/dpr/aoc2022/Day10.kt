61320bbbcc91ab1894e447da2c7eedf46f14f7d5
Dominik Przybysz <dpr@touk.pl>
2022-12-10 08:48:04 +0100

    part 2 of 10/2022

--- 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day10.kt
+++ 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day10.kt
@@ -5,16 +5,24 @@ object Day10 {
     fun main(args: Array<String>) {
         val lines = Util.getNotEmptyLinesFromFile("/10/input.txt")
         println("Part 1:")
-        println(part1(Util.getNotEmptyLinesFromFile("/10/test1.txt")))
+//        println(part1(Util.getNotEmptyLinesFromFile("/10/test1.txt")))
         println(part1(lines))
         println("Part 2:")
+//        println(part2(Util.getNotEmptyLinesFromFile("/10/test1.txt")))
         println(part2(lines))
     }
 
     private fun part1(lines: List<String>): Any {
+        val values = getCyclesValues(lines)
+        return values.filter { it.key in setOf(20, 60, 100, 140, 180, 220) }
+            .map { it.key * it.value }
+            .sum()
+    }
+
+    private fun getCyclesValues(lines: List<String>): MutableMap<Int, Long> {
         var x = 1L
         var cycle = 1
-        var values = mutableMapOf<Int, Long>()
+        val values = mutableMapOf<Int, Long>()
         values[cycle] = x
         lines.forEach {
             if (it == "noop") {
@@ -29,13 +37,23 @@ object Day10 {
                 values[cycle] = x
             }
         }
-        return values.filter { it.key in setOf(20, 60, 100, 140, 180, 220) }
-            .map { it.key * it.value }
-            .sum()
+        return values
     }
 
     private fun part2(lines: List<String>): Any {
-        TODO()
+        val cyclesValues = getCyclesValues(lines)
+        for (i in 1..240) {
+            val curValue = cyclesValues[i]!!.toInt()
+            if ((i - 1) % 40 in setOf(curValue - 1, curValue, curValue + 1)) {
+                print("#")
+            } else {
+                print(".")
+            }
+            if (i % 40 == 0) {
+                println()
+            }
+        }
+        return ""
     }
 }
 
