6055ea44a5fc50a712699acbd524b7841fe4b930
Dominik Przybysz <dpr@touk.pl>
2022-12-12 10:15:58 +0100

    Fix run all 2022

--- 2022/src/main/kotlin/pl/touk/dpr/aoc2022/All.kt
+++ 2022/src/main/kotlin/pl/touk/dpr/aoc2022/All.kt
@@ -4,31 +4,56 @@ object All {
     @JvmStatic
     fun main(args: Array<String>) {
         Util.measureTime {
-        Util.measureTime { Day17.main(args) }
-        Util.measureTime { Day02.main(args) }
-        Util.measureTime { Day03.main(args) }
-        Util.measureTime { Day04.main(args) }
-        Util.measureTime { Day05.main(args) }
-        Util.measureTime { Day06.main(args) }
-        Util.measureTime { Day07.main(args) }
-        Util.measureTime { Day08.main(args) }
-        Util.measureTime { Day09.main(args) }
-        Util.measureTime { Day10.main(args) }
-        Util.measureTime { Day11.main(args) }
-        Util.measureTime { Day12.main(args) }
-        Util.measureTime { Day13.main(args) }
-        Util.measureTime { Day14.main(args) }
-        Util.measureTime { Day15.main(args) }
-        Util.measureTime { Day16.main(args) }
-        Util.measureTime { Day17.main(args) }
-        Util.measureTime { Day18.main(args) }
-        Util.measureTime { Day19.main(args) }
-        Util.measureTime { Day20.main(args) }
-        Util.measureTime { Day21.main(args) }
-        Util.measureTime { Day22.main(args) }
-        Util.measureTime { Day23.main(args) }
-        Util.measureTime { Day24.main(args) }
-        Util.measureTime { Day25.main(args) }
+            println("Day01")
+            Util.measureTime { Day01.main(args) }
+            println("Day02")
+            Util.measureTime { Day02.main(args) }
+            println("Day03")
+            Util.measureTime { Day03.main(args) }
+            println("Day04")
+            Util.measureTime { Day04.main(args) }
+            println("Day05")
+            Util.measureTime { Day05.main(args) }
+            println("Day06")
+            Util.measureTime { Day06.main(args) }
+            println("Day07")
+            Util.measureTime { Day07.main(args) }
+            println("Day08")
+            Util.measureTime { Day08.main(args) }
+            println("Day09")
+            Util.measureTime { Day09.main(args) }
+            println("Day10")
+            Util.measureTime { Day10.main(args) }
+            println("Day11")
+            Util.measureTime { Day11.main(args) }
+            println("Day12")
+            Util.measureTime { Day12.main(args) }
+            println("Day13")
+            Util.measureTime { Day13.main(args) }
+            println("Day14")
+            Util.measureTime { Day14.main(args) }
+            println("Day15")
+            Util.measureTime { Day15.main(args) }
+            println("Day16")
+            Util.measureTime { Day16.main(args) }
+            println("Day17")
+            Util.measureTime { Day17.main(args) }
+            println("Day18")
+            Util.measureTime { Day18.main(args) }
+            println("Day19")
+            Util.measureTime { Day19.main(args) }
+            println("Day20")
+            Util.measureTime { Day20.main(args) }
+            println("Day21")
+            Util.measureTime { Day21.main(args) }
+            println("Day22")
+            Util.measureTime { Day22.main(args) }
+            println("Day23")
+            Util.measureTime { Day23.main(args) }
+            println("Day24")
+            Util.measureTime { Day24.main(args) }
+            println("Day25")
+            Util.measureTime { Day25.main(args) }
         }
     }
 }
