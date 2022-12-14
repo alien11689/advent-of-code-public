e8c4c2cfbdb20524821c75ddd92e2a8a6b0e0404
Dominik Przybysz <dpr@touk.pl>
2022-12-14 06:47:41 +0100

    Finished part 1 of 14/2022

--- 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day14.kt
+++ 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day14.kt
@@ -12,8 +12,70 @@ object Day14 {
         println(part2(lines))
     }
 
+    data class Point(val x: Int, val y: Int) {
+        fun down(): Point = copy(y = y + 1)
+        fun left(): Point = copy(x = x - 1, y = y + 1)
+        fun right(): Point = copy(x = x + 1, y = y + 1)
+    }
+
     private fun part1(lines: List<String>): Any {
-        TODO()
+        val points = mutableSetOf<Point>()
+        lines.forEach { line ->
+            val parts = line.split(" -> ")
+            var (curX, curY) = parts[0].split(",").map { it.toInt() }
+//            println("Filling $curX, $curY")
+            points.add(Point(curX, curY))
+            var i = 1
+            while (i < parts.size) {
+                val (destX, destY) = parts[i].split(",").map { it.toInt() }
+                val dx = if (curX == destX) 0 else if (curX < destX) 1 else -1
+                val dy = if (curY == destY) 0 else if (curY < destY) 1 else -1
+//                println("Moving from $curX, $curY to $destX, $destY and moving $dx, $dy")
+                while (curX != destX || curY != destY) {
+                    curX += dx
+                    curY += dy
+//                    println("Filling $curX, $curY")
+                    points.add(Point(curX, curY))
+                }
+                ++i
+            }
+        }
+        val minX = points.map { it.x }.min()
+        val maxX = points.map { it.x }.max()
+        val minY = points.map { it.y }.min()
+        val maxY = points.maxOfOrNull { it.y }!!
+        for (y in minY..maxY) {
+            for (x in minX..maxX) {
+                print(if (Point(x, y) in points) "#" else ".")
+            }
+            println()
+        }
+        var curSand = Point(500, 0)
+        var seen = 0
+        while (true) {
+//            println("Checking $curSand")
+            val down = curSand.down()
+            val left = curSand.left()
+            val right = curSand.right()
+            if (down in points && left in points && right in points) {
+                points.add(curSand)
+                ++seen
+                curSand = Point(500, 0)
+                continue
+            } else if (down !in points) {
+                curSand = down
+            } else if (left !in points) {
+                curSand = left
+            } else if (right !in points) {
+                curSand = right
+            } else {
+                throw RuntimeException()
+            }
+            if (curSand.y == maxY) {
+                break
+            }
+        }
+        return seen
     }
 
     private fun part2(lines: List<String>): Any {
