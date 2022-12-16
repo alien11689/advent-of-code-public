fffa8917aa0c6a17768b9b8b0c8cc222a5812cca
Dominik Przybysz <dpr@touk.pl>
2022-12-16 08:31:06 +0100

    Part 2 still too slow

--- 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day16.kt
+++ 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day16.kt
@@ -90,34 +90,45 @@ object Day16 {
         var maxPresure = 0L
         val pq = PriorityQueue<State2>()
         pq.offer(State2("AA", "AA", 26, valves))
-//        var globalMemory = mutableSetOf<GlobalMem>()
+        val globalMemory = mutableSetOf<GlobalMem2>()
+        var generation = 0
         while (pq.isNotEmpty()) {
             val cur = pq.poll()
-//            println("Checking ${cur.presure}: ${cur.room} on time ${cur.time} with notOpenValves ${cur.notOpenValves}")
+//            val globalKey = GlobalMem2(setOf(cur.room1, cur.room2), cur.notOpenValves, cur.presure, cur.time)
+//            if (globalKey in globalMemory) {
+//                continue
+//            }
+            if (++generation % 100000 == 0) {
+                println("PQ size is ${pq.size} with time: ${cur.time}")
+            }
             cur.nexts(transitions)
                 .forEach {
                     if (it.presure > maxPresure) {
-//                        println("New leader ${it.presure}: ${it.room1},${it.room2} on time ${it.time} with notOpenValves ${it.notOpenValves}, pq size is ${pq.size}")
+                        println("New leader ${it.presure}: ${it.room1},${it.room2} on time ${it.time} with notOpenValves ${it.notOpenValves}, pq size is ${pq.size}")
                         maxPresure = it.presure
                     }
                     if (it.notOpenValves.values.sumOf { r -> r * (it.time - 1) } + it.presure >= maxPresure) {
-                        pq.offer(it)
+                        val globalKey = GlobalMem2(setOf(it.room1, it.room2), it.notOpenValves, it.presure, it.time)
+                        if (globalKey !in globalMemory) {
+                            globalMemory.add(globalKey)
+                            pq.offer(it)
+                        }
                     }
-//                    val globalKey = GlobalMem(it.room, it.notOpenValves, it.presure)
-//                    if (globalKey !in globalMemory) {
-//                        globalMemory.add(globalKey)
-
-//                    }
                 }
         }
         return maxPresure
     }
 
-    data class Mem2(val room: String, val room2: String, val notOpenValves: Map<String, Int>)
+    data class GlobalMem2(val room: Set<String>, val notOpenValves: Map<String, Int>, val presure: Long, val time: Int)
+
+    data class Mem2(val rooms: Set<String>, val notOpenValves: Map<String, Int>)
 
     data class State2(val room1: String, val room2: String, val time: Int, val notOpenValves: Map<String, Int>, val presure: Long = 0, val memory: Set<Mem2> = emptySet()) :
         Comparable<State2> {
-        override fun compareTo(other: State2): Int = time - other.time
+        override fun compareTo(other: State2): Int = if (other.time == time) {
+            other.presure.compareTo(presure)
+        } else other.time - time
+
         fun nexts(transitions: Map<String, List<String>>): List<State2> {
             if (time == 0 || notOpenValves.isEmpty()) {
                 return emptyList()
@@ -129,7 +140,7 @@ object Day16 {
                 val rate2 = notOpenValves[room2]!!.toLong()
                 val newNotOpenValves = notOpenValves - room1 - room2
                 val newPresure = presure + newTime * rate1 + newTime * rate2
-                options.add(this.copy(time = newTime, notOpenValves = newNotOpenValves, presure = newPresure, memory = memory + Mem2(room1, room2, newNotOpenValves)))
+                options.add(this.copy(time = newTime, notOpenValves = newNotOpenValves, presure = newPresure, memory = memory + Mem2(setOf(room1, room2), newNotOpenValves)))
             }
             if (room1 in notOpenValves) {
                 val newTime = time - 1
@@ -137,7 +148,15 @@ object Day16 {
                 val newNotOpenValves = notOpenValves - room1
                 val newPresure = presure + newTime * rate1
                 transitions[room2]!!.forEach { newRoom2 ->
-                    options.add(this.copy(room2 = newRoom2, time = newTime, notOpenValves = newNotOpenValves, presure = newPresure, memory = memory + Mem2(room1, newRoom2, newNotOpenValves)))
+                    options.add(
+                        this.copy(
+                            room2 = newRoom2,
+                            time = newTime,
+                            notOpenValves = newNotOpenValves,
+                            presure = newPresure,
+                            memory = memory + Mem2(setOf(room1, newRoom2), newNotOpenValves)
+                        )
+                    )
                 }
             }
             if (room2 in notOpenValves) {
@@ -146,12 +165,20 @@ object Day16 {
                 val newNotOpenValves = notOpenValves - room2
                 val newPresure = presure + newTime * rate2
                 transitions[room1]!!.forEach { newRoom1 ->
-                    options.add(this.copy(room1 = newRoom1, time = newTime, notOpenValves = newNotOpenValves, presure = newPresure, memory = memory + Mem2(newRoom1, room2, newNotOpenValves)))
+                    options.add(
+                        this.copy(
+                            room1 = newRoom1,
+                            time = newTime,
+                            notOpenValves = newNotOpenValves,
+                            presure = newPresure,
+                            memory = memory + Mem2(setOf(newRoom1, room2), newNotOpenValves)
+                        )
+                    )
                 }
             }
             transitions[room1]!!.forEach { newRoom1 ->
                 transitions[room2]!!.forEach { newRoom2 ->
-                    val key = Mem2(newRoom1, newRoom2, notOpenValves)
+                    val key = Mem2(setOf(newRoom1, newRoom2), notOpenValves)
                     if (key !in memory) {
                         options.add(copy(room1 = newRoom1, room2 = newRoom2, time = time - 1, memory = memory + key))
                     }
