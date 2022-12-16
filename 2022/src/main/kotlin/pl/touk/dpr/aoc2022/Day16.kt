80700470113ece27392c05cf06e1db7dc43530c7
Dominik Przybysz <dpr@touk.pl>
2022-12-16 07:31:32 +0100

    Part 1 of 16/2022

--- 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day16.kt
+++ 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day16.kt
@@ -1,19 +1,86 @@
 package pl.touk.dpr.aoc2022
 
+import java.util.PriorityQueue
+
 object Day16 {
     @JvmStatic
     fun main(args: Array<String>) = Util.measureTime {
         val lines = Util.getNotEmptyLinesFromFile("/16/input.txt")
         println("Part 1:")
         println(part1(Util.getNotEmptyLinesFromFile("/16/test1.txt")))
+        // 1234 is wrong
+        // 1691 is wrong
         println(part1(lines))
         println("Part 2:")
-        println(part2(Util.getNotEmptyLinesFromFile("/16/test1.txt")))
-        println(part2(lines))
+//        println(part2(Util.getNotEmptyLinesFromFile("/16/test1.txt")))
+//        println(part2(lines))
+    }
+
+    data class Room(val name: String, val rate: Int, val targets: List<String>)
+
+    data class Mem(val room: String, val notOpenValves: Map<String, Int>)
+    data class GlobalMem(val room: String, val notOpenValves: Map<String, Int>, val presure: Long)
+
+    data class State(val room: String, val time: Int, val notOpenValves: Map<String, Int>, val presure: Long = 0, val memory: Set<Mem> = emptySet()) : Comparable<State> {
+        override fun compareTo(other: State): Int = time - other.time
+        fun nexts(transitions: Map<String, List<String>>): List<State> {
+            if (time == 0 || notOpenValves.isEmpty()) {
+                return emptyList()
+            }
+            val options = mutableListOf<State>()
+            if (room in notOpenValves) {
+                val rate = notOpenValves[room]!!.toLong()
+                val newTime = time - 1
+                val newNotOpenValves = notOpenValves - room
+                options.add(this.copy(time = newTime, notOpenValves = newNotOpenValves, presure = presure + newTime * rate, memory = memory + Mem(room, newNotOpenValves)))
+            }
+            transitions[room]!!.forEach { newRoom ->
+                val key = Mem(newRoom, notOpenValves)
+                if (key !in memory) {
+                    options.add(copy(room = newRoom, time = time - 1, memory = memory + key))
+                }
+            }
+            return options
+        }
+
     }
 
     private fun part1(lines: List<String>): Any {
-        TODO()
+        val rooms = readRooms(lines)
+        val valves = rooms.filter { it.rate > 0 }.associate { it.name to it.rate }
+        val transitions = rooms.associate { it.name to it.targets }
+        var maxPresure = 0L
+        val pq = PriorityQueue<State>()
+        pq.offer(State("AA", 30, valves))
+        var globalMemory = mutableSetOf<GlobalMem>()
+        while (pq.isNotEmpty()) {
+            val cur = pq.poll()
+//            println("Checking ${cur.presure}: ${cur.room} on time ${cur.time} with notOpenValves ${cur.notOpenValves}")
+            cur.nexts(transitions)
+                .forEach {
+                    if (it.presure > maxPresure) {
+//                        println("New leader ${it.presure}: ${it.room} on time ${it.time} with notOpenValves ${it.notOpenValves}")
+                        maxPresure = it.presure
+                    }
+                    if (it.notOpenValves.values.map { r -> r * (it.time - 1) }.sum() + it.presure >= maxPresure) {
+                        pq.offer(it)
+                    }
+//                    val globalKey = GlobalMem(it.room, it.notOpenValves, it.presure)
+//                    if (globalKey !in globalMemory) {
+//                        globalMemory.add(globalKey)
+
+//                    }
+                }
+        }
+        return maxPresure
+    }
+
+    private fun readRooms(lines: List<String>) = lines.map { line ->
+        val parts = line.split(";", ",", "=", " ")
+        val name = parts[1]
+        val rate = parts[5].toInt()
+        val targets = parts.drop(11).take(100).filter { it.isNotBlank() }
+        Room(name, rate, targets)
     }
 
     private fun part2(lines: List<String>): Any {
