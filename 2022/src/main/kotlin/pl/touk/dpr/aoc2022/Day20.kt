6c9eaa903eb91bc936fb3935c6a7f5380f96e101
Dominik Przybysz <dpr@touk.pl>
2022-12-20 08:35:10 +0100

    Working part 2 of 20/2022

--- 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day20.kt
+++ 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day20.kt
@@ -38,7 +38,7 @@ object Day20 {
                     (maxIdx * 2 + curPos + shift) % maxIdx
                 }
             } else curPos
-            println("Applying $curElem: target pos is $targetPos and curPos is $curPos")
+//            println("Applying $curElem: target pos is $targetPos and curPos is $curPos")
             require(targetPos in elements.keys)
             elements = if (curPos < targetPos) {
                 elements.map {
@@ -68,8 +68,62 @@ object Day20 {
         return elements[(origin + 1000) % elements.size]!!.v + elements[(origin + 2000) % elements.size]!!.v + elements[(origin + 3000) % elements.size]!!.v
     }
 
+    data class Elem2(val v: Long, val origin: Long)
+
     private fun part2(lines: List<String>): Any {
-        TODO()
+        val encryptionKey = 811589153L
+        var elements = lines.mapIndexed { i, v -> i.toLong() to Elem2(v.toLong() * encryptionKey, i.toLong()) }.toMap()
+        repeat(10) {
+            val maxIdx = (elements.size - 1).toLong()
+            (0 until elements.size).forEach { i ->
+                val curElem = elements.entries.first { it.value.origin == i.toLong() }
+                val curPos = curElem.key
+                val value = curElem.value
+                val shift = value.v
+                val targetPos: Long = if (shift > 0) {
+                    if (curPos + shift <= maxIdx) {
+                        curPos + shift
+                    } else {
+                        (curPos + shift) % maxIdx
+                    }
+                } else if (shift < 0) {
+                    if (curPos + shift > 0) {
+                        curPos + shift
+                    } else if (curPos + shift == 0L) {
+                        maxIdx
+                    } else {
+                        (maxIdx * 10000000000 + curPos + shift) % maxIdx
+                    }
+                } else curPos
+//                println("Applying $curElem: target pos is $targetPos and curPos is $curPos")
+                require(targetPos in elements.keys)
+                elements = if (curPos < targetPos) {
+                    elements.map {
+                        val x = when {
+                            it.key < curPos || it.key > targetPos -> it.key to it.value
+                            it.key == targetPos -> it.key to value
+                            else -> it.key to elements[it.key + 1]!!
+                        }
+//                    println("Moving $it to $x")
+                        x
+                    }.toMap()
+                } else if (curPos > targetPos) {
+                    elements.map {
+                        val x = when {
+                            it.key > curPos || it.key < targetPos -> it.key to it.value
+                            it.key == targetPos -> it.key to value
+                            else -> it.key to elements[it.key - 1]!!
+                        }
+//                    println("Moving $it to $x")
+                        x
+                    }.toMap()
+                } else elements
+//            println(elements.entries.sortedBy { it.key }.map { it.value.v }.joinToString(", "))
+//            println()
+            }
+        }
+        val origin = elements.entries.first { it.value.v == 0L }.key
+        return elements[(origin + 1000) % elements.size]!!.v + elements[(origin + 2000) % elements.size]!!.v + elements[(origin + 3000) % elements.size]!!.v
     }
 }
 
