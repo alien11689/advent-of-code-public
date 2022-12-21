6e4dd3df2011535727f1b67cd5fa5494ed8640cf
Dominik Przybysz <dpr@touk.pl>
2022-12-21 08:34:30 +0100

    21/2022

--- 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day21.kt
+++ 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day21.kt
@@ -5,10 +5,10 @@ object Day21 {
     fun main(args: Array<String>) = Util.measureTime {
         val lines = Util.getNotEmptyLinesFromFile("/21/input.txt")
         println("Part 1:")
-        println(part1(Util.getNotEmptyLinesFromFile("/21/test1.txt")))
+//        println(part1(Util.getNotEmptyLinesFromFile("/21/test1.txt")))
         println(part1(lines))
         println("Part 2:")
-        println(part2(Util.getNotEmptyLinesFromFile("/21/test1.txt")))
+//        println(part2(Util.getNotEmptyLinesFromFile("/21/test1.txt")))
         println(part2(lines))
     }
 
@@ -23,6 +23,34 @@ object Day21 {
                     else -> throw RuntimeException("unknown $left $right $operation")
                 }
             } else null
+        }
+
+        fun match(values: Map<String, Long>, result: Long): Pair<String, Long> {
+            return if (left in values) {
+                val v1 = values[left]!!
+                when (operation) {
+                    "+" -> Pair(right, result - v1) // v1 + x = res => x = res - v1
+                    "-" -> Pair(right, v1 - result) // v1 - x = res => x = v1 - res
+                    "*" -> Pair(right, result / v1) // v1 * x = result => x = result / v1
+                    "/" -> Pair(right, v1 / result) // v1 / x = result => v1 = resulut * x => x = v1/result
+                    else -> throw RuntimeException("unknown $left $right $operation")
+                }
+            } else if (right in values) {
+                val v2 = values[right]!!
+                when (operation) {
+                    "+" -> Pair(left, result - v2) // x + v2 = res => x = res - v2
+                    "-" -> Pair(left, result + v2) // x - v2 = res => x = res + v2
+                    "*" -> Pair(left, result / v2) // x * v2 = res => x = res / v2
+                    "/" -> Pair(left, result * v2) // x / v2 = res => x = res * v2
+                    else -> throw RuntimeException("unknown $left $right $operation")
+                }
+            } else {
+                throw RuntimeException("???")
+            }
+        }
+
+        override fun toString(): String {
+            return "($left $operation $right)"
         }
     }
 
@@ -53,7 +81,64 @@ object Day21 {
     }
 
     private fun part2(lines: List<String>): Any {
-        TODO()
+        val values = mutableMapOf<String, Long>()
+        val monkeys = mutableMapOf<String, Monkey>()
+        var rootComparison: Pair<String, String>? = null
+        lines.forEach { line ->
+            val (monkey, expression) = line.split(":").map { it.trim() }
+            if (monkey != "humn") {
+                val parts = expression.split(" ")
+                if (parts.size == 1) {
+                    values[monkey] = parts[0].toLong()
+                } else {
+                    if (monkey == "root") {
+                        rootComparison = Pair(parts[0], parts[2])
+                    } else monkeys[monkey] = Monkey(parts[0], parts[2], parts[1])
+                }
+            }
+        }
+        while (true) {
+            val monkeysToDelete = mutableSetOf<String>()
+            monkeys.entries.forEach { (name, monkey) ->
+                val res = monkey.calculate(values)
+                if (res != null) {
+                    values[name] = res
+                    monkeysToDelete.add(name)
+                }
+            }
+            monkeysToDelete.forEach { monkeys.remove(it) }
+            if (monkeysToDelete.isEmpty()) {
+                break
+            }
+        }
+//        println(values)
+//        monkeys.forEach { println("$it") }
+//        println(rootComparison)
+//        var expression = "${rootComparison!!.first} == ${values[rootComparison!!.second]}"
+//        while (true) {
+//            val expressionBefore = expression
+//            values.forEach {
+//                if (expression.contains(it.key)) {
+//                    expression = expression.replace(it.key, it.value.toString())
+//                }
+//            }
+//            monkeys.forEach {
+//                if (expression.contains(it.key)) {
+//                    expression = expression.replace(it.key, it.value.toString())
+//                }
+//            }
+//            if (expression == expressionBefore) {
+//                break
+//            }
+//        }
+//        return expression
+        var cur = monkeys[rootComparison!!.first]!!.match(values, values[rootComparison!!.second]!!)
+        while (cur.first != "humn") {
+//            println(cur)
+            cur = monkeys[cur.first]!!.match(values, cur.second)
+        }
+
+        return cur.second
     }
 }
 
