1941e35e0e9f738080ea4241ef9c5dca71630c48
Dominik Przybysz <dpr@touk.pl>
2022-12-21 19:14:57 +0100

    Simplify 9/2022

--- 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day09.kt
+++ 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day09.kt
@@ -15,14 +15,11 @@ object Day09 {
     }
 
     private fun part1(lines: List<String>): Any {
-        var state = State(Point(0, 0), Point(0, 0))
-//        println("Head: ${state.head}, tail: ${state.tail}")
+        var state = State(List(2) { Point(0, 0) })
         lines.forEach {
-//            println("Moving $it")
             val (dir, step) = it.split(" ")
-            (1..(step.toInt())).forEach {
+            repeat(step.toInt()) {
                 state = state.move(dir)
-//                println("Head: ${state.head}, tail: ${state.tail}")
             }
         }
         return state.tailVisited.size
@@ -30,33 +27,33 @@ object Day09 {
 
     data class Point(val x: Int, val y: Int) {
         fun move(dir: String) = when (dir) {
-            "R" -> copy(x + 1, y)
-            "L" -> copy(x - 1, y)
-            "U" -> copy(x, y + 1)
-            "D" -> copy(x, y - 1)
+            "R" -> copy(x = x + 1)
+            "L" -> copy(x = x - 1)
+            "U" -> copy(y = y + 1)
+            "D" -> copy(y = y - 1)
             else -> throw RuntimeException("unknown $dir")
         }
 
         fun moveToward(newHead: Point): Point {
-            if (newHead.x == x || newHead.y == y) {
-//                println("Moving $this towards $newHead, possible moves lines: ${neighboursLines()}")
-                return newHead.neighbours().intersect(this.neighboursLines()).first()
+            return if (newHead.x == x || newHead.y == y) {
+                //                println("Moving $this towards $newHead, possible moves lines: ${neighboursLines()}")
+                newHead.neighbours().intersect(this.neighboursLines()).first()
             } else {
-//                println("Moving $this towards $newHead, possible moves diagonal: ${neighboursDiagonal()}")
-                return newHead.neighbours().intersect(this.neighboursDiagonal()).first()
+                //                println("Moving $this towards $newHead, possible moves diagonal: ${neighboursDiagonal()}")
+                newHead.neighbours().intersect(this.neighboursDiagonal()).first()
             }
         }
 
-        fun neighbours(): Set<Point> = setOf(this) + neighboursDiagonal() + neighboursLines()
+        fun neighbours(): Set<Point> = neighboursDiagonal() + neighboursLines() + this
 
-        fun neighboursLines(): Set<Point> = setOf(
-            copy(x = x, y = y + 1),
-            copy(x = x, y = y - 1),
-            copy(x = x + 1, y = y),
-            copy(x = x - 1, y = y),
+        private fun neighboursLines(): Set<Point> = setOf(
+            copy(y = y + 1),
+            copy(y = y - 1),
+            copy(x = x + 1),
+            copy(x = x - 1),
         )
 
-        fun neighboursDiagonal(): Set<Point> = setOf(
+        private fun neighboursDiagonal(): Set<Point> = setOf(
             copy(x = x - 1, y = y + 1),
             copy(x = x - 1, y = y - 1),
             copy(x = x + 1, y = y + 1),
@@ -64,23 +61,13 @@ object Day09 {
         )
     }
 
-    data class State(val head: Point, val tail: Point, val tailVisited: Set<Point> = setOf(tail)) {
-        fun move(dir: String): State {
-            val newHead = head.move(dir)
-            val newTail = if (newHead in tail.neighbours()) tail else {
-                tail.moveToward(newHead)
-            }
-            return copy(head = newHead, tail = newTail, tailVisited = tailVisited + newTail)
-        }
-    }
-
     private fun part2(lines: List<String>): Any {
-        var state = State2((1..10).map { Point(0, 0) })
+        var state = State(List(10) { Point(0, 0) })
 //        println("Head: ${state.head}, tail: ${state.tail}")
         lines.forEach {
 //            println("Moving $it")
             val (dir, step) = it.split(" ")
-            (1..(step.toInt())).forEach {
+            repeat(step.toInt()) {
                 state = state.move(dir)
 //                println("Head: ${state.head}, tail: ${state.tail}")
             }
@@ -89,50 +76,37 @@ object Day09 {
         return state.tailVisited.size
     }
 
-    private fun draw(state: State2) {
-        for (i in 20 downTo -10) {
-            for (j in -20..20) {
-                if (Point(j, i) in state.points) {
-                    print("#")
-                } else if (j == 0 && i == 0) {
-                    print('s')
-                } else {
-                    print(".")
-                }
-            }
-            println()
-        }
-    }
+//    private fun draw(state: State) {
+//        for (i in 20 downTo -10) {
+//            for (j in -20..20) {
+//                if (Point(j, i) in state.points) {
+//                    print("#")
+//                } else if (j == 0 && i == 0) {
+//                    print('s')
+//                } else {
+//                    print(".")
+//                }
+//            }
+//            println()
+//        }
+//    }
 
-    data class State2(val points: List<Point>, val tailVisited: Set<Point> = setOf(points.last())) {
-        fun move(dir: String): State2 {
+    data class State(val points: List<Point>, val tailVisited: Set<Point> = setOf(points.last())) {
+        fun move(dir: String): State {
             var newHead = points.first().move(dir)
             val newPoints = mutableListOf(newHead)
-            var moved = true
             for (i in (1 until points.size)) {
                 val cur = points[i]
-                if (moved) {
-                    val newCur = if (newHead in cur.neighbours()) cur else {
-                        cur.moveToward(newHead)
-                    }
-                    newPoints.add(newCur)
-                    newHead = newCur
-                    if (newCur == cur) {
-                        moved = false
-                    }
-                } else {
-                    newPoints.add(cur)
+                val newCur = if (newHead in cur.neighbours()) cur else cur.moveToward(newHead)
+                newPoints.add(newCur)
+                newHead = newCur
+                if (newCur == cur) {
+                    newPoints.addAll(points.drop(i + 1))
+                    break
                 }
             }
-            if (newPoints.size != 10) {
-                throw RuntimeException()
-            }
-//            if (points[8] != newPoints[8]) {
-//                println("Moving tail to ${newPoints[8]}")
-//            }
-            return copy(newPoints, tailVisited = tailVisited + newPoints.last())
+            return copy(points = newPoints, tailVisited = tailVisited + newPoints.last())
         }
     }
-
 }
 
