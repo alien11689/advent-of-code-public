34d325272ac9d643160a0c239a135d072b7d024d
Dominik Przybysz <dpr@touk.pl>
2022-12-25 06:34:39 +0100

    Semi working example

--- 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day25.kt
+++ 2022/src/main/kotlin/pl/touk/dpr/aoc2022/Day25.kt
@@ -10,6 +10,33 @@ object Day25 {
     }
 
     private fun part1(lines: List<String>): Any {
+        val sum = lines.sumOf { line ->
+            var base = 1L
+            var value = 0L
+//            println("reading line $line as ${line.reversed().toCharArray().map { it }}")
+            line.reversed().toCharArray().forEach {
+                val number = when (it) {
+                    '1' -> 1
+                    '2' -> 2
+                    '0' -> 0
+                    '-' -> -1
+                    '=' -> -2
+                    else -> throw RuntimeException()
+                }
+//                println("Current $value plus $base * $number")
+                value += base * number
+                base *= 5
+            }
+//            println("$line => $value ")
+            value
+        }
+        println(sum)
+        var base = 1L
+        while (sum > base) {
+            base *= 5
+        }
+        base -= 1
+        println("Base $base")
         TODO()
     }
 }
