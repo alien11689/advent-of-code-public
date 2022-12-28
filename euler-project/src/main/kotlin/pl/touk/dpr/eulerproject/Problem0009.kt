c8e2b66293d7b4c71acabc8f66f22cfefa026480
Dominik Przybysz <dpr@touk.pl>
2022-12-28 18:29:02 +0100

    Solve euler 0009

---
+++ euler-project/src/main/kotlin/pl/touk/dpr/eulerproject/Problem0009.kt
@@ -0,0 +1,19 @@
+package pl.touk.dpr.eulerproject
+
+object Problem0009 {
+    @JvmStatic
+    fun main(args: Array<String>) = Util.measureTime {
+        for (a in 1..1000) {
+            for (b in a..1000) {
+                val c = 1000 - a - b
+                if (c <= b) {
+                    break
+                }
+                if (c * c == a * a + b * b) {
+                    println(a * b * c)
+                    return@measureTime
+                }
+            }
+        }
+    }
+}
