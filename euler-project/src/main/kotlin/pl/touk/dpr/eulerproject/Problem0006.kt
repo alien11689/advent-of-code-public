bef4ce3fa6ff97e98405ffea9ab1fb53989d0144
Dominik Przybysz <dpr@touk.pl>
2022-12-28 17:14:20 +0100

    Solve euler 0006

---
+++ euler-project/src/main/kotlin/pl/touk/dpr/eulerproject/Problem0006.kt
@@ -0,0 +1,13 @@
+package pl.touk.dpr.eulerproject
+
+import kotlin.math.absoluteValue
+
+object Problem0006 {
+    @JvmStatic
+    fun main(args: Array<String>) = Util.measureTime {
+        val sum100 = (1..100L).sum()
+        val s1 = sum100 * sum100
+        val s2 = (1..100L).sumOf { it * it }
+        println((s2 - s1).absoluteValue)
+    }
+}
