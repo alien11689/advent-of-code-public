eccfbc07416ea5103e24aa75f42a296c6c3bab1f
Dominik Przybysz <dpr@touk.pl>
2022-12-28 16:45:54 +0100

    Solve euler 0002

---
+++ euler-project/src/main/kotlin/pl/touk/dpr/eulerproject/Problem0002.kt
@@ -0,0 +1,12 @@
+package pl.touk.dpr.eulerproject
+
+object Problem0002 {
+    @JvmStatic
+    fun main(args: Array<String>) = Util.measureTime {
+        val res = generateSequence(0 to 1) { (it.second to (it.first + it.second)) }
+                .takeWhile { it.second < 4_000_000 }
+                .filter { it.second % 2 == 0 }
+                .sumOf { it.second.toLong() }
+        println(res)
+    }
+}
