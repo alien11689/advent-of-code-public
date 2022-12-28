ea9401a60f1cfa2d325e09f7d90fdd852c1cfee9
Dominik Przybysz <dpr@touk.pl>
2022-12-28 18:05:15 +0100

    Solve euler 0049

---
+++ euler-project/src/main/kotlin/pl/touk/dpr/eulerproject/Problem0049.kt
@@ -0,0 +1,34 @@
+package pl.touk.dpr.eulerproject
+
+object Problem0049 {
+    @JvmStatic
+    fun main(args: Array<String>) = Util.measureTime {
+        val primes = generateSequence(1000L) { it + 1 }
+                .filter { Util.isPrime(it) }
+                .takeWhile { it < 10000 }
+                .toList()
+
+        val groupedByDigits = primes.groupBy { it.toString().toList().sorted() }
+        val values = groupedByDigits.filter { it.value.size >= 3 }.values
+        values.forEach {
+            for (x in it.indices) {
+                if (x < it.size - 2) {
+                    if (it[x] == 1487L) {
+                        continue
+                    }
+                    val first = it[x]
+                    for (y in it.indices) {
+                        if (x < y) {
+                            val second = it[y]
+                            val third = second + second - first
+                            if (third in it) {
+                                println("$first$second$third")
+                                return@measureTime
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
