74b6f46eea9875b1674d4a368bfa9015d153face
Dominik Przybysz <dpr@touk.pl>
2022-12-31 10:26:19 +0100

    Solve euler 0048

---
+++ euler-project/src/main/kotlin/pl/touk/dpr/eulerproject/Problem0048.kt
@@ -0,0 +1,15 @@
+package pl.touk.dpr.eulerproject
+
+import java.math.BigInteger
+
+object Problem0048 {
+    @JvmStatic
+    fun main(args: Array<String>) = Util.measureTime {
+        val lastDigits = BigInteger.valueOf(1_000_000_000_0L)
+        val res = generateSequence(1L) { it + 1 }
+                .takeWhile { it <= 1000 }
+                .sumOf { BigInteger.valueOf(it).modPow(BigInteger.valueOf(it), lastDigits) }
+                .mod(lastDigits)
+        println(res)
+    }
+}
