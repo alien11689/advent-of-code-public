3d5e5df733844ac7c752d4d8a568afb5e0a86bd7
Dominik Przybysz <dpr@touk.pl>
2023-01-02 18:27:55 +0100

    Solve euler 46

---
+++ euler-project/src/main/kotlin/pl/touk/dpr/eulerproject/Problem0046.kt
@@ -0,0 +1,31 @@
+package pl.touk.dpr.eulerproject
+
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+object Problem0046 {
+    @JvmStatic
+    fun main(args: Array<String>) = Util.measureTime {
+        val primes = mutableListOf<Long>()
+        var i = 1L
+        while(i < 100_000_000L){
+            i += 2
+            if(Util.isPrime(i)){
+                primes.add(i)
+//                println("Found prime $i")
+                continue
+            }
+            val match = primes.none {
+//                println("   Checking with $it as prime")
+                val x = (i - it) / 2
+                val sq = sqrt(x.toDouble()).toLong()
+                sq * sq == x
+            }
+            if (match){
+                println(i)
+                break
+            }
+        }
+
+    }
+}
