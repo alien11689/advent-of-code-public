0d11bb01410a7fd11cc12a532ab642aa5d605650
Dominik Przybysz <dpr@touk.pl>
2023-04-17 22:25:17 +0200

    Bump kotlin and update answers times

---
+++ runAllAndDumpCalculatedAnswers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+input=$1
+years=${input:="2015 2016 2017 2018 2019 2020 2021 2022 synacor-challenge"}
+
+for x in $years; do
+    echo "========="
+    echo Year $x
+    echo "========="
+    cd $x
+    ./runAll.sh > calculatedAnswers.txt
+    cd -
+done
