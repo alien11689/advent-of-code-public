fec5a5277dd51213806589dc8a3765e01be8d626
Dominik Przybysz <dpr@touk.pl>
2023-12-02 09:12:34 +0100

    Simplify 1 of 2/2023

--- 2023/src/main/kotlin/dpr/aoc2023/Day02.kt
+++ 2023/src/main/kotlin/dpr/aoc2023/Day02.kt
@@ -8,25 +8,22 @@ object Day02 {
         println(part2(lines))
     }
 
+    val limits = mapOf(
+        "green" to 13,
+        "red" to 12,
+        "blue" to 14,
+    )
+
     private fun part1(lines: List<String>): Any {
         return lines.sumOf { line ->
             val (header, rest) = line.split(": ")
             val rounds = rest.split("; ")
-            var res = header.split(" ")[1].toInt()
-            for (round in rounds) {
-                val minorResults = round.split(Regex("[ ,]+"))
-                var i = 0
-                val m = mutableMapOf<String, Int>()
-                while (i < minorResults.size) {
-                    m[minorResults[i + 1]] = minorResults[i].toInt()
-                    i += 2
-                }
-                if ((m["green"] ?: 0) > 13 || (m["red"] ?: 0) > 12 || (m["blue"] ?: 0) > 14) {
-                    res = 0
-                    break
-                }
+            val gameId = header.split(" ")[1].toInt()
+            val anyRoundExceedsLimits = rounds.any { round ->
+                val m = round.split(Regex("[ ,]+")).chunked(2) { l -> l[1] to l[0].toInt() }.toMap()
+                limits.any { (color, limit) -> (m[color] ?: 0) > limit }
             }
-            res
+            if (anyRoundExceedsLimits) 0 else gameId
         }
     }
 
