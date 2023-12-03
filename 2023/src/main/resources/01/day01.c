369d9fbc710a307fc27e3a840b34c30a65398bef
Dominik Przybysz <dpr@touk.pl>
2023-12-03 14:48:50 +0100

    Reformat c code in 1/2023

--- 2023/src/main/resources/01/day01.c
+++ 2023/src/main/resources/01/day01.c
@@ -2,31 +2,36 @@
 #include<string.h>
 #include<stdlib.h>
 
-void strrev(char * str) {
-    int len = strlen(str);
-    int i = 0;
-    for (i = 0; i < len / 2; ++i) {
-        char tmp = str[i];
-        str[i] = str[len - 1 - i];
-        str[len - 1 - i] = tmp;
-    }
+void strrev(char *str) {
+	int len = strlen(str);
+	int i = 0;
+	for (i = 0; i < len / 2; ++i) {
+		char tmp = str[i];
+		str[i] = str[len - 1 - i];
+		str[len - 1 - i] = tmp;
+	}
+}
+
+int part1(char *fileName) {
+	FILE *f = fopen(fileName, "r");
+	int length = 256;
+	char buffer[256];
+	char keys[] = "1234567890";
+	int result = 0;
+	while (fgets(buffer, length, f)) {
+		int firstDigitIdx = strcspn(buffer, keys);
+		char firstDigit = buffer[firstDigitIdx];
+		strrev(buffer);
+		int lastDigitIdx = strcspn(buffer, keys);
+		char lastDigit = buffer[lastDigitIdx];
+		result += (firstDigit - '0') * 10 + (lastDigit - '0');
+	}
+	fclose(f);
+	return result;
 }
 
 int main() {
-  FILE * f = fopen("./input.txt", "r");
-  int length = 256;
-  char buffer[256];
-  char keys[] = "1234567890";
-  int result = 0;
-  while(fgets(buffer, length, f)) {
-    int firstDigitIdx = strcspn(buffer, keys);
-    char firstDigit = buffer[firstDigitIdx];
-    strrev(buffer);
-    int lastDigitIdx = strcspn(buffer, keys);
-    char lastDigit = buffer[lastDigitIdx];
-    result += (firstDigit - '0') * 10 + (lastDigit - '0');
-  }
-  fclose(f);
-  printf("Part 1\n%d\n", result);
-  return 0;
+	int result1 = part1("./input.txt");
+	printf("Part 1\n%d\n", result1);
+	return 0;
 }
