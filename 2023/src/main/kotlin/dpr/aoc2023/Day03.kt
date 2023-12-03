ea49495eaecbe5640c5997419b704a9c7e3095cf
Dominik Przybysz <dpr@touk.pl>
2023-12-03 09:09:05 +0100

    Part 1 of 3/2023

--- 2023/src/main/kotlin/dpr/aoc2023/Day03.kt
+++ 2023/src/main/kotlin/dpr/aoc2023/Day03.kt
@@ -4,12 +4,65 @@ object Day03 {
     @JvmStatic
     fun main(args: Array<String>) = Util.measureTime {
         val lines = Util.getNotEmptyLinesFromFile("/03/input.txt")
+//        val lines = Util.getNotEmptyLinesFromFile("/03/test1.txt")
         println(part1(lines))
         println(part2(lines))
     }
 
+    data class Point2D(val x: Int, val y: Int) {
+        fun adjacents(): Set<Point2D> = setOf(
+            Point2D(x - 1, y - 1),
+            Point2D(x + 1, y - 1),
+            Point2D(x - 1, y + 1),
+            Point2D(x + 1, y + 1),
+            Point2D(x - 1, y),
+            Point2D(x + 1, y),
+            Point2D(x, y - 1),
+            Point2D(x, y + 1),
+        )
+    }
+
     private fun part1(lines: List<String>): Any {
-        TODO()
+        val signes = mutableSetOf<Point2D>()
+        val numbers = mutableListOf<Pair<Int, Set<Point2D>>>()
+        var curNumber = 0
+        var curPoints = mutableSetOf<Point2D>()
+        lines.forEachIndexed { curY, line ->
+            line.forEachIndexed { curX, c ->
+                when {
+                    c == '.' -> {
+                        if (curNumber != 0) {
+                            numbers.add(curNumber to curPoints)
+                            curNumber = 0
+                            curPoints = mutableSetOf()
+                        }
+                    }
+
+                    c.isDigit() -> {
+                        curNumber = curNumber * 10 + c.digitToInt()
+                        curPoints.add(Point2D(curX, curY))
+                    }
+
+                    else -> {
+                        signes.add(Point2D(curX, curY))
+                        if (curNumber != 0) {
+                            numbers.add(curNumber to curPoints)
+                            curNumber = 0
+                            curPoints = mutableSetOf()
+                        }
+                    }
+                }
+            }
+            if (curNumber != 0) {
+                numbers.add(curNumber to curPoints)
+                curNumber = 0
+                curPoints = mutableSetOf()
+            }
+            println("Breaking line with $")
+        }
+        val adjacents = signes.flatMap { p -> p.adjacents() }.toSet()
+        return numbers.filter { it.second.any { it in adjacents } }.sumOf { it.first }
+        // 316475 is wrong
     }
 
     private fun part2(lines: List<String>): Any {
