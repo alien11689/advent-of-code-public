bec4d4ea3eecc2943f2f039a949419733fbbda56
Dominik Przybysz <dpr@touk.pl>
2023-12-05 20:33:06 +0100

    Refactor 5/2023

--- 2023/src/main/kotlin/dpr/aoc2023/Day05.kt
+++ 2023/src/main/kotlin/dpr/aoc2023/Day05.kt
@@ -1,6 +1,9 @@
 package dpr.aoc2023
 
 object Day05 {
+    private const val INITIAL_STATE = "seed"
+    private const val FINAL_STATE = "location"
+
     @JvmStatic
     fun main(args: Array<String>) = Util.measureTime {
         val lines = Util.getNotEmptyLinesFromFile("/05/input.txt")
@@ -16,66 +19,59 @@ object Day05 {
             null
         }
 
-        fun translateRange(curSeed: SeedRange): Pair<List<SeedRange>, List<SeedRange>> {
-            if (curSeed.max < sourceMin || curSeed.min > sourceMax) {
-                // ----
-                //      ----
-                // or
-                //
-                // ----
-                return emptyList<SeedRange>() to listOf(curSeed)
+        fun translateRange(curSeed: SeedRange): Pair<SeedRange?, List<SeedRange>> {
+            when {
+                curSeed.max < sourceMin || curSeed.min > sourceMax -> {
+                    // ----
+                    //      ----
+                    // or
+                    //      ----
+                    // ----
+                    return null to listOf(curSeed)
+                }
+
+                curSeed.min >= sourceMin && curSeed.max <= sourceMax -> {
+                    // ----
+                    // ----
+                    // or
+                    // ---
+                    // ----
+                    // or
+                    //  ---
+                    // ----
+                    // or
+                    //   ---
+                    // --------
+                    return SeedRange(translate(curSeed.min)!!, translate(curSeed.max)!!) to emptyList()
+                }
+
+                curSeed.min <= sourceMin && curSeed.max >= sourceMax -> {
+                    // ---
+                    // --
+                    // or
+                    // ---
+                    //  --
+                    // or
+                    // -------
+                    //   ---
+                    return SeedRange(translate(sourceMin)!!, translate(sourceMax)!!) to listOf(
+                        SeedRange(curSeed.min, sourceMin - 1),
+                        SeedRange(sourceMax + 1, curSeed.max)
+                    ).filter { it.min <= it.max }
+                }
+
+                curSeed.min < sourceMin -> {
+                    // ---
+                    //  ---
+                    return SeedRange(translate(sourceMin)!!, translate(curSeed.max)!!) to listOf(SeedRange(curSeed.min, sourceMin - 1))
+                }
+
+                else -> {
+                    //  ---
+                    // ---
+                    return SeedRange(translate(curSeed.min)!!, translate(sourceMax)!!) to listOf(SeedRange(sourceMax + 1, curSeed.max))
+                }
             }
-            if (curSeed.min == sourceMin && curSeed.max == sourceMax) {
-                // ----
-                // ----
-                return listOf(SeedRange(translate(curSeed.min)!!, translate(curSeed.max)!!)) to emptyList()
-            }
-            if (curSeed.min == sourceMin && curSeed.max < sourceMax) {
-                // ---
-                // ----
-                return listOf(SeedRange(translate(curSeed.min)!!, translate(curSeed.max)!!)) to emptyList()
-            }
-            if (curSeed.min == sourceMin && curSeed.max > sourceMax) {
-                // ---
-                // --
-                return listOf(SeedRange(translate(curSeed.min)!!, translate(sourceMax)!!)) to listOf(SeedRange(sourceMax + 1, curSeed.max))
-            }
-            if (curSeed.min > sourceMin && curSeed.max == sourceMax) {
-                //  --
-                // ---
-                return listOf(SeedRange(translate(curSeed.min)!!, translate(sourceMax)!!)) to emptyList()
-            }
-            if (curSeed.min < sourceMin && curSeed.max == sourceMax) {
-                // ---
-                //  --
-                return listOf(SeedRange(translate(sourceMin)!!, translate(sourceMax)!!)) to listOf(SeedRange(curSeed.min, sourceMin - 1))
-            }
-            if (curSeed.min < sourceMin && curSeed.max > sourceMax) {
-                // -------
-                //   ---
-                return listOf(SeedRange(translate(sourceMin)!!, translate(sourceMax)!!)) to listOf(
-                    SeedRange(curSeed.min, sourceMin - 1),
-                    SeedRange(sourceMax + 1, curSeed.max)
-                )
-            }
-            if (curSeed.min > sourceMin && curSeed.max < sourceMax) {
-                //   ---
-                // --------
-                return listOf(SeedRange(translate(curSeed.min)!!, translate(curSeed.max)!!)) to emptyList()
-            }
-            if (curSeed.min < sourceMin && curSeed.max < sourceMax) {
-                // ---
-                //  ---
-                return listOf(SeedRange(translate(sourceMin)!!, translate(curSeed.max)!!)) to listOf(SeedRange(curSeed.min, sourceMin - 1))
-            }
-            if (curSeed.min > sourceMin && curSeed.max > sourceMax) {
-                //  ---
-                // ---
-                return listOf(SeedRange(translate(curSeed.min)!!, translate(sourceMax)!!)) to listOf(
-                    SeedRange(sourceMax + 1, curSeed.max)
-                )
-            }
-            TODO("Not yet implemented")
         }
     }
 
@@ -87,7 +83,7 @@ object Day05 {
                 val newCurSeeds = mutableListOf<SeedRange>()
                 curSeeds.forEach { curSeed ->
                     val (translated, passed) = transition.translateRange(curSeed)
-                    newState.addAll(translated)
+                    translated?.let { newState.add(it) }
                     newCurSeeds.addAll(passed)
                 }
                 curSeeds = newCurSeeds
@@ -102,52 +98,23 @@ object Day05 {
     private fun part1(lines: List<String>): Any {
         val seeds = lines[0].split(":")[1].trim().split(" ").map { it.toLong() }
         val mappings = parseMappings(lines)
-        var minimal = Long.MAX_VALUE
-        for (s in seeds) {
-            var curValue = s
-            var curState = "seed"
-//            println("Seed $curValue")
-            while (curState != "location") {
-                val (curMapping, transitions) = mappings.filter { it.key.first == curState }.entries.first()
-//                println(" using mapping $transitions")
-//                println(" applied transitions ${transitions.map { it.translate(curValue) }}")
-                curState = curMapping.second
-                curValue = transitions.firstNotNullOfOrNull { it.translate(curValue) } ?: curValue
-//                println(" goes to $curState $curValue")
-            }
-//            println("Seed $s -> Location $curValue")
-            if (curValue < minimal) {
-                minimal = curValue
-            }
+        var curSeeds = seeds
+        var curState = INITIAL_STATE
+        while (curState != FINAL_STATE) {
+            val (curMapping, transitions) = mappings.filter { it.key.first == curState }.entries.first()
+            curState = curMapping.second
+            curSeeds = curSeeds.map { curValue -> transitions.firstNotNullOfOrNull { it.translate(curValue) } ?: curValue }
         }
-        return minimal
-    }
-
-    private fun parseMappings(lines: List<String>): MutableMap<Pair<String, String>, MutableSet<Translation>> {
-        var i = 1
-        val mappings = mutableMapOf<Pair<String, String>, MutableSet<Translation>>()
-        var currentMapping: Pair<String, String> = "" to ""
-        while (i < lines.size) {
-            if (lines[i].endsWith(":")) {
-                val (from, _, to) = lines[i].split(Regex("[ :-]"))
-                currentMapping = from to to
-                mappings[currentMapping] = mutableSetOf()
-            } else {
-                val (dest, source, range) = lines[i].split(" ").map { it.toLong() }
-                mappings[currentMapping]!!.add(Translation(source, source + range - 1, dest, range))
-            }
-            ++i
-        }
-        return mappings
+        return curSeeds.min()
     }
 
     private fun part2(lines: List<String>): Any {
         val seedsRanges = lines[0].split(":")[1].trim().split(" ").map { it.toLong() }.chunked(2).map { SeedRange(it[0], it[0] + it[1] - 1) }
         val mappings = parseMappings(lines)
         var curSeedRanges = seedsRanges
-        var curState = "seed"
-        while (curState != "location") {
-            println("Cur state is $curState and seedsRanges number is ${curSeedRanges.size} (seeds ${seedsRanges.sumOf { it.length }})")
+        var curState = INITIAL_STATE
+        while (curState != FINAL_STATE) {
+//            println("Cur state is $curState and seedsRanges number is ${curSeedRanges.size} (seeds ${seedsRanges.sumOf { it.length }})")
             val (curMapping, transitions) = mappings.filter { it.key.first == curState }.entries.first()
 //                println(" using mapping $transitions")
 //                println(" applied transitions ${transitions.map { it.translate(curValue) }}")
@@ -157,5 +124,20 @@ object Day05 {
         }
         return curSeedRanges.minOf { it.min }
     }
-}
 
+    private fun parseMappings(lines: List<String>): MutableMap<Pair<String, String>, MutableSet<Translation>> {
+        val mappings = mutableMapOf<Pair<String, String>, MutableSet<Translation>>()
+        var currentMapping: Pair<String, String> = "" to ""
+        lines.drop(1).forEach { line ->
+            if (line.endsWith(":")) {
+                val (from, _, to) = line.split(Regex("[ :-]"))
+                currentMapping = from to to
+                mappings[currentMapping] = mutableSetOf()
+            } else {
+                val (dest, source, range) = line.split(" ").map { it.toLong() }
+                mappings[currentMapping]!!.add(Translation(source, source + range - 1, dest, range))
+            }
+        }
+        return mappings
+    }
+}
