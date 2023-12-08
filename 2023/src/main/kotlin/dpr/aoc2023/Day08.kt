fa92b3c34f00fd8fbbbffce743febffa2b72b84f
Dominik Przybysz <dpr@touk.pl>
2023-12-08 16:30:02 +0100

    Part 1 of 8/2024

--- 2023/src/main/kotlin/dpr/aoc2023/Day08.kt
+++ 2023/src/main/kotlin/dpr/aoc2023/Day08.kt
@@ -9,7 +9,20 @@ object Day08 {
     }
 
     private fun part1(lines: List<String>): Any {
-        TODO()
+        val directions = lines[0]
+        val mapping = mutableMapOf<String, Pair<String, String>>()
+        lines.drop(1).forEach {
+            val split = it.split(Regex("[ = (,)]+"))
+            mapping[split[0]] = split[1] to split[2]
+        }
+        var cur = "AAA"
+        var steps = 0
+        while (cur != "ZZZ") {
+            val curMapping = mapping[cur]!!
+            cur = if (directions[steps % directions.length] == 'L') curMapping.first else curMapping.second
+            ++steps
+        }
+        return steps
     }
 
     private fun part2(lines: List<String>): Any {
