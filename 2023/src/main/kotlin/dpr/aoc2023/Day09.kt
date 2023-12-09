a0a14314e1b77ae0edc01132d5dc396109b66b95
Dominik Przybysz <dpr@touk.pl>
2023-12-09 08:41:12 +0100

    Part 1 of 9/2023

--- 2023/src/main/kotlin/dpr/aoc2023/Day09.kt
+++ 2023/src/main/kotlin/dpr/aoc2023/Day09.kt
@@ -9,7 +9,20 @@ object Day09 {
     }
 
     private fun part1(lines: List<String>): Any {
-        TODO()
+        return lines.sumOf { line ->
+            val values = line.split(" ").map { it.toLong() }
+            findNextValue(values)
+        }
+    }
+
+    private fun findNextValue(values: List<Long>): Long {
+        if (values.toSet().size == 1) {
+            return values[0]
+        } else {
+            val lowerList = values.windowed(2, 1, false) { it[1] - it[0] }
+            val increment = findNextValue(lowerList)
+            return values.last() + increment
+        }
     }
 
     private fun part2(lines: List<String>): Any {
