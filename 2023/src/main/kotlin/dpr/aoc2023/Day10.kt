8bce7d631198e0b4a3189d3317c817c95f7e5b1d
Dominik Przybysz <dpr@touk.pl>
2023-12-10 11:02:55 +0100

    Part 2 of 10/2023

--- 2023/src/main/kotlin/dpr/aoc2023/Day10.kt
+++ 2023/src/main/kotlin/dpr/aoc2023/Day10.kt
@@ -1,5 +1,6 @@
 package dpr.aoc2023
 
+import java.util.LinkedList
 import java.util.PriorityQueue
 
 object Day10 {
@@ -11,16 +12,29 @@ object Day10 {
     }
 
     private fun part1(lines: List<String>): Any {
+        val mapa = readMapa(lines)
+        val start = mapa.filter { it.value == Sign.S }.keys.single()
+        val realStartSign = detectRealStartSign(mapa, start)
+        mapa[start] = realStartSign
+
+        val (max, visited) = travel(start, mapa)
+        return max
+    }
+
+    private fun readMapa(lines: List<String>): MutableMap<Point2D, Sign> {
         val mapa = mutableMapOf<Point2D, Sign>()
         lines.forEachIndexed { y, line ->
             line.forEachIndexed { x, c ->
                 mapa[Point2D(x, y)] = Sign.from(c)
             }
         }
-        val start = mapa.filter { it.value == Sign.S }.keys.single()
-        val realStartSign = detectRealStartSign(mapa, start)
-        mapa[start] = realStartSign
+        return mapa
+    }
 
+    private fun travel(
+        start: Point2D,
+        mapa: MutableMap<Point2D, Sign>
+    ): Pair<Long, MutableSet<Point2D>> {
         var max = 0L
         val visited = mutableSetOf<Point2D>()
         val toVisit = PriorityQueue<CurPos>()
@@ -48,24 +62,21 @@ object Day10 {
                 toVisit.offer(CurPos(cur.p.right(), cur.steps + 1))
             }
         }
-        return max
+        return Pair(max, visited)
     }
 
     private fun detectRealStartSign(mapa: MutableMap<Point2D, Sign>, start: Point2D): Sign {
         val possibleStartSign = mutableSetOf(Sign.L, Sign.J, Sign.UP_DOWN, Sign.MINUS, Sign._7, Sign.F)
-        println(possibleStartSign)
         if (mapa[start.up()]!!.canDown()) {
             possibleStartSign.removeAll(possibleStartSign.filter { !it.canUp() })
         } else {
             possibleStartSign.removeAll(possibleStartSign.filter { it.canUp() })
         }
-        println(possibleStartSign)
         if (mapa[start.down()]!!.canUp()) {
             possibleStartSign.removeAll(possibleStartSign.filter { !it.canDown() })
         } else {
             possibleStartSign.removeAll(possibleStartSign.filter { it.canDown() })
         }
-        println(possibleStartSign)
         // TODO check other directions to be generic
         val realStartSign = possibleStartSign.single()
         return realStartSign
@@ -109,8 +120,118 @@ object Day10 {
         }
     }
 
+    enum class Dir {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT;
+
+        fun turn(nextSign: Sign): Dir = when (this) {
+            UP -> when (nextSign) {
+                Sign._7 -> LEFT
+                Sign.F -> RIGHT
+                else -> this
+            }
+
+            DOWN -> when (nextSign) {
+                Sign.J -> LEFT
+                Sign.L -> RIGHT
+                else -> this
+            }
+
+            LEFT -> when (nextSign) {
+                Sign.F -> DOWN
+                Sign.L -> UP
+                else -> this
+            }
+
+            RIGHT -> when (nextSign) {
+                Sign.J -> UP
+                Sign._7 -> DOWN
+                else -> this
+            }
+        }
+    }
+
     private fun part2(lines: List<String>): Any {
-        TODO()
+        val mapa = readMapa(lines)
+        val start = mapa.filter { it.value == Sign.S }.keys.single()
+        val realStartSign = detectRealStartSign(mapa, start)
+        mapa[start] = realStartSign
+
+        val visited = mutableSetOf<Point2D>()
+        var cur = start
+        var curDir = Dir.RIGHT // assume we are minus on start
+        var curInteriorDir = setOf(Dir.UP) // assume interior is above
+        val possibleInterior = mutableSetOf<Point2D>()
+        possibleInterior.add(cur.up())
+        while (true) {
+            visited.add(cur)
+            val next = when (curDir) {
+                Dir.UP -> cur.up()
+                Dir.DOWN -> cur.down()
+                Dir.LEFT -> cur.left()
+                Dir.RIGHT -> cur.right()
+            }
+            if (next in visited) {
+                break
+            }
+            val nextSign = mapa[next]!!
+            val nextDir = curDir.turn(nextSign)
+            curInteriorDir = when {
+                nextSign == Sign.MINUS -> curInteriorDir.filter { it in setOf(Dir.UP, Dir.DOWN) }.toSet()
+                nextSign == Sign.UP_DOWN -> curInteriorDir.filter { it in setOf(Dir.UP, Dir.DOWN) }.toSet()
+                curDir == Dir.RIGHT && nextSign == Sign._7 -> if (Dir.UP in curInteriorDir) setOf(Dir.UP, Dir.RIGHT) else setOf(Dir.DOWN, Dir.LEFT)
+                curDir == Dir.UP && nextSign == Sign._7 -> if (Dir.RIGHT in curInteriorDir) setOf(Dir.UP, Dir.RIGHT) else setOf(Dir.DOWN, Dir.LEFT)
+                curDir == Dir.RIGHT && nextSign == Sign.J -> if (Dir.UP in curInteriorDir) setOf(Dir.UP, Dir.LEFT) else setOf(Dir.DOWN, Dir.RIGHT)
+                curDir == Dir.DOWN && nextSign == Sign.J -> if (Dir.LEFT in curInteriorDir) setOf(Dir.UP, Dir.LEFT) else setOf(Dir.DOWN, Dir.RIGHT)
+                curDir == Dir.DOWN && nextSign == Sign.L -> if (Dir.LEFT in curInteriorDir) setOf(Dir.DOWN, Dir.LEFT) else setOf(Dir.UP, Dir.RIGHT)
+                curDir == Dir.LEFT && nextSign == Sign.L -> if (Dir.DOWN in curInteriorDir) setOf(Dir.DOWN, Dir.LEFT) else setOf(Dir.UP, Dir.RIGHT)
+                curDir == Dir.UP && nextSign == Sign.F -> if (Dir.RIGHT in curInteriorDir) setOf(Dir.DOWN, Dir.RIGHT) else setOf(Dir.UP, Dir.LEFT)
+                curDir == Dir.LEFT && nextSign == Sign.F -> if (Dir.DOWN in curInteriorDir) setOf(Dir.DOWN, Dir.RIGHT) else setOf(Dir.UP, Dir.LEFT)
+                else -> throw RuntimeException()
+            }
+            cur = next
+            curDir = nextDir
+            curInteriorDir.forEach { dir ->
+                val posInterior = when (dir) {
+                    Dir.UP -> cur.up()
+                    Dir.DOWN -> cur.down()
+                    Dir.LEFT -> cur.left()
+                    Dir.RIGHT -> cur.right()
+                }
+                possibleInterior.add(posInterior)
+            }
+        }
+        println("Possible after road: " + possibleInterior.size)
+        possibleInterior.removeAll(visited)
+        println("Possible without visited: " + possibleInterior.size)
+        val knownInterior = mutableSetOf<Point2D>()
+        for (dot in possibleInterior) {
+            if (dot in knownInterior) {
+                continue
+            }
+            val localKnownEmpty = LinkedList<Point2D>()
+            localKnownEmpty.add(dot)
+            val localVisited = mutableSetOf<Point2D>()
+            while (localKnownEmpty.isNotEmpty()) {
+                val current = localKnownEmpty.pop()
+                if (current in localVisited) {
+                    continue
+                }
+                if(cur !in mapa){
+                    throw RuntimeException("Bla")
+                }
+                localVisited.add(current)
+                current.adjacentPoints().filter { it !in visited }.forEach { localKnownEmpty.push(it) }
+            }
+            knownInterior.addAll(localVisited)
+        }
+        // 518 is too high
+        // 28 is wrong
+        // 13904 is too high
+        // 306 is wrong
+        return knownInterior.size
     }
 }
 
