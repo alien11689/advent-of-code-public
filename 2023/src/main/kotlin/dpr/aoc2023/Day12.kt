2f01d79b56f8bcd93923cf35af133e4d4f4a4fc5
Dominik Przybysz <dpr@touk.pl>
2023-12-12 19:29:48 +0100

    Part 2 of 12/2023

--- 2023/src/main/kotlin/dpr/aoc2023/Day12.kt
+++ 2023/src/main/kotlin/dpr/aoc2023/Day12.kt
@@ -13,44 +13,71 @@ object Day12 {
         return lines.sumOf { line ->
             val (pattern, nums) = line.split(" ")
             val orderedNumbers = nums.split(",").map { it.toInt() }
-            val res = getPossibleOptions(pattern.replace(".", " ").trim(), orderedNumbers)
-            println("$line -> $res")
+            val memory = mutableMapOf<Pair<String, List<Int>>, Long>()
+            val res = getPossibleOptions(pattern.replace(".", " ").trim(), orderedNumbers, memory)
+//            println("$line -> $res")
             res
         }
     }
 
-    private fun getPossibleOptions(pattern: String, orderedNumbers: List<Int>): Int {
-        println("Checking '$pattern' against $orderedNumbers")
+    private fun getPossibleOptions(pattern: String, orderedNumbers: List<Int>, memory: MutableMap<Pair<String, List<Int>>, Long>): Long {
+//        println("Checking '$pattern' against $orderedNumbers")
+        val key = pattern to orderedNumbers
+        if (key in memory) {
+            return memory[key]!!
+        }
         if (orderedNumbers.isEmpty()) {
-            return if (pattern.contains("#")) 0 else 1
+            val res: Long = if (pattern.contains("#")) 0 else 1
+            memory[key] = res
+            return res
         }
         val patternLength = pattern.length
         val orderedNumbersMinSize = orderedNumbers.sum() + orderedNumbers.size - 1
+        if (orderedNumbers.sum() > pattern.count { it != ' ' }) {
+            memory[key] = 0
+//            println("Shortcut")
+            return 0
+        }
         if (patternLength < orderedNumbersMinSize) {
             return 0
         }
-        var res = 0
+        var res = 0L
         val regex = if (orderedNumbers.size > 1) "^[?#]{${orderedNumbers[0]}}[? ].*" else "^[?#]{${orderedNumbers[0]}}.*"
-        println("Regex: $regex")
-        if (pattern.matches(Regex(regex))) {
+//        println("Regex: $regex")
+        if (pattern.startsWith(" ")) {
+            res += getPossibleOptions(pattern.substring(1), orderedNumbers, memory)
+        } else {
+            if (pattern.matches(Regex(regex))) {
 //            println("Match!!")
-            // use first number
-            val newPattern = pattern.substring(orderedNumbers[0] + if (orderedNumbers.size > 1) 1 else 0)
-            res += getPossibleOptions(newPattern, orderedNumbers.drop(1))
+                // use first number
+                val newPattern = pattern.substring(orderedNumbers[0] + if (orderedNumbers.size > 1) 1 else 0)
+                res += getPossibleOptions(newPattern, orderedNumbers.drop(1), memory)
+            }
+            if (pattern.startsWith("?")) {
+                // ? as space
+                res += getPossibleOptions(pattern.substring(1), orderedNumbers, memory)
+            }
         }
-        if (pattern.startsWith(" ") || pattern.startsWith("?")) {
-            res += getPossibleOptions(pattern.substring(1), orderedNumbers)
-        }
-        println("Result checking '$pattern' against $orderedNumbers -> $res")
+//        println("Result checking '$pattern' against $orderedNumbers -> $res")
 //        println(pattern)
 //        println(patternLength)
 //        println(orderedNumbersMinSize)
+        memory[key] = res
         return res
-        TODO("Not yet implemented")
     }
 
     private fun part2(lines: List<String>): Any {
-        TODO()
+        return lines.sumOf { line ->
+            val (pattern, nums) = line.split(" ")
+            val orderedNumbers = nums.split(",").map { it.toInt() }
+            val realPattern = listOf(pattern, pattern, pattern, pattern, pattern).joinToString(separator = "?")
+            val realNumbers = listOf(orderedNumbers, orderedNumbers, orderedNumbers, orderedNumbers, orderedNumbers).flatten()
+            val memory = mutableMapOf<Pair<String, List<Int>>, Long>()
+            val res = getPossibleOptions(realPattern.replace(".", " ").trim(), realNumbers, memory)
+//            println("$realPattern with $realNumbers -> $res")
+            res
+        }
+//        TODO()
     }
 }
 
