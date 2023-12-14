128b59fb4de28e38b65663570a8469bccf3abaa1
Dominik Przybysz <dpr@touk.pl>
2023-12-14 16:43:49 +0100

    Part 1 of 14/2023

--- 2023/src/main/kotlin/dpr/aoc2023/Day14.kt
+++ 2023/src/main/kotlin/dpr/aoc2023/Day14.kt
@@ -9,7 +9,34 @@ object Day14 {
     }
 
     private fun part1(lines: List<String>): Any {
-        TODO()
+        val rocks = mutableSetOf<Point2D>()
+        var sum = 0L
+        lines.forEachIndexed { y, line ->
+            line.forEachIndexed { x, c ->
+                when (c) {
+                    '#' -> rocks.add(Point2D(x, y))
+                    '.' -> {}
+                    'O' -> {
+                        val i = insertRock(Point2D(x, y), rocks)
+                        sum += lines.size - i
+                    }
+                }
+            }
+        }
+        return sum
+    }
+
+    private fun insertRock(rock: Point2D, rocks: MutableSet<Point2D>): Int {
+        var cur = rock
+        while (cur.y > 0) {
+            val up = cur.up()
+            if (up in rocks) {
+                break
+            }
+            cur = up
+        }
+        rocks.add(cur)
+        return cur.y
     }
 
     private fun part2(lines: List<String>): Any {
