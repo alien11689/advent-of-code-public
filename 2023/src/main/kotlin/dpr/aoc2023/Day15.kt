e23700ab9499d8d8a3a266b75870650f00383e92
Dominik Przybysz <dpr@touk.pl>
2023-12-15 15:42:09 +0100

    Part 2 of 15/2023

--- 2023/src/main/kotlin/dpr/aoc2023/Day15.kt
+++ 2023/src/main/kotlin/dpr/aoc2023/Day15.kt
@@ -21,8 +21,38 @@ object Day15 {
 //            .also { println("For $s -> $it") }
     }
 
+    data class Lens(val name: String, var value: Int)
+
+    data class Box(val lenses: MutableList<Lens> = mutableListOf())
+
     private fun part2(lines: List<String>): Any {
-        TODO()
+        val boxes = mutableMapOf<Int, Box>()
+        repeat(256) { i -> boxes[i] = Box() }
+        lines.forEach { line ->
+            val operations = line.split(",")
+            operations.forEach {
+                when {
+                    it.endsWith("-") -> {
+                        val name = it.substring(0, it.length - 1)
+                        val h = hash(name).toInt()
+                        boxes[h]!!.lenses.removeIf { it.name == name }
+                    }
+
+                    it.contains("=") -> {
+                        val (name, value) = it.split("=")
+                        val h = hash(name).toInt()
+                        val lenses = boxes[h]!!.lenses
+                        val lens = lenses.firstOrNull { it.name == name }
+                        if (lens != null) {
+                            lens.value = value.toInt()
+                        } else {
+                            lenses.add(Lens(name, value.toInt()))
+                        }
+                    }
+                }
+            }
+        }
+        return (0..255).sumOf { boxId -> boxes[boxId]!!.lenses.mapIndexed { lId, lens -> 1L * (boxId + 1) * (lId + 1) * lens.value }.sum() }
     }
 }
 
