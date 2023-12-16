52db1bdf9c1b41e233c14a10c51bcd668f52cff2
Dominik Przybysz <dpr@touk.pl>
2023-12-16 08:58:03 +0100

    Simplify fetch input script usage

--- fetchInput.sh
+++ fetchInput.sh
@@ -2,7 +2,9 @@
 
 year=$1
 day=$2
-dest=$3
+
 session=`cat ~/.adventofcode`
+BASEDIR=$(dirname "$0")
+dest=${BASEDIR}/$year/src/main/resources/`printf '%02d' $day`/input.txt
 
 curl https://adventofcode.com/$year/day/$day/input --cookie "session=$session" -o $dest
