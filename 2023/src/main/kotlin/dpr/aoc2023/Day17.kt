fe800962f5041876df2f20a478cfd22dc747356c
Dominik Przybysz <dpr@touk.pl>
2023-12-17 17:58:17 +0100

    Optimize 17/2023

--- 2023/src/main/kotlin/dpr/aoc2023/Day17.kt
+++ 2023/src/main/kotlin/dpr/aoc2023/Day17.kt
@@ -10,14 +10,14 @@ object Day17 {
     fun main(args: Array<String>) = Util.measureTime {
         val lines = Util.getNotEmptyLinesFromFile("/17/input.txt")
 //        val lines = Util.getNotEmptyLinesFromFile("/17/test1.txt")
-        val (board, maxX, maxY) = readBoard(lines)
-        println(part1(board, maxX, maxY))
-        println(part2(board, maxX, maxY))
+        val (board, target) = readBoard(lines)
+        println(part1(board, target))
+        println(part2(board, target))
     }
 
-    data class Current(val pos: Point2D, val heat: Int, val dir: Dir, val maxX: Int, val maxY: Int) : Comparable<Current> {
+    data class Current(val pos: Point2D, val heat: Int, val dir: Dir) : Comparable<Current> {
 
-        private val score = heat + maxX + maxY - pos.x - pos.y
+        private val score = heat - pos.x - pos.y
         override fun compareTo(other: Current): Int = score.compareTo(other.score)
         fun nextMoves(board: Map<Point2D, Int>, minSteps: Int, maxSteps: Int): List<Current> {
             var i = 0
@@ -39,30 +39,29 @@ object Day17 {
 
     }
 
-    private fun part1(board: MutableMap<Point2D, Int>, maxX: Int, maxY: Int): Any {
-        return traverse(board, maxX, maxY, 1, 3)
+    private fun part1(board: Map<Point2D, Int>, target: Point2D): Any {
+        return traverse(board, target, 1, 3)
     }
 
-    private fun part2(board: MutableMap<Point2D, Int>, maxX: Int, maxY: Int): Any {
-        return traverse(board, maxX, maxY, 4, 10)
+    private fun part2(board: Map<Point2D, Int>, target: Point2D): Any {
+        return traverse(board, target, 4, 10)
     }
 
-    private fun readBoard(lines: List<String>): Triple<MutableMap<Point2D, Int>, Int, Int> {
+    private fun readBoard(lines: List<String>): Pair<Map<Point2D, Int>, Point2D> {
         val board = mutableMapOf<Point2D, Int>()
         lines.forEachIndexed { y, line ->
             line.forEachIndexed { x, c ->
                 board[Point2D(x, y)] = c.digitToInt()
             }
         }
-        return Triple(board, lines[0].length - 1, lines.size - 1)
+        return Pair(board, Point2D(lines[0].length - 1, lines.size - 1))
     }
 
-    private fun traverse(board: MutableMap<Point2D, Int>, maxX: Int, maxY: Int, minSteps: Int, maxSteps: Int): Int {
+    private fun traverse(board: Map<Point2D, Int>, target: Point2D, minSteps: Int, maxSteps: Int): Int {
         val pq = PriorityQueue<Current>()
-        pq.offer(Current(Point2D(0, 0), 0, Dir.E, maxX, maxY))
-        pq.offer(Current(Point2D(0, 0), 0, Dir.S, maxX, maxY))
+        pq.offer(Current(Point2D(0, 0), 0, Dir.E))
+        pq.offer(Current(Point2D(0, 0), 0, Dir.S))
         val mem = mutableSetOf<Pair<Point2D, Dir>>()
-        val target = Point2D(maxX, maxY)
         while (pq.isNotEmpty()) {
             val cur = pq.poll()
             if (cur.pos == target) {
