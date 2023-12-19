97e23e8b2115531bb484bfdf1cdf5d33ed362274
Dominik Przybysz <dpr@touk.pl>
2023-12-19 07:15:10 +0100

    Part 1 of 19/2023

--- 2023/src/main/kotlin/dpr/aoc2023/Day19.kt
+++ 2023/src/main/kotlin/dpr/aoc2023/Day19.kt
@@ -1,6 +1,7 @@
 package dpr.aoc2023
 
 import dpr.commons.Util
+import java.util.UUID
 
 object Day19 {
     @JvmStatic
@@ -11,8 +12,83 @@ object Day19 {
         println(part2(lines))
     }
 
+    enum class Sign {
+        LT,
+        GT,
+        ALWAYS
+    }
+
+    data class Rule(val part: String, val sign: Sign, val value: Long, val target: String) {
+        fun accepts(item: Map<String, Long>): Boolean {
+            return when (sign) {
+                Sign.ALWAYS -> true
+                Sign.LT -> item[part]!! < value
+                else -> item[part]!! > value
+            }
+        }
+
+    }
+
     private fun part1(lines: List<String>): Any {
-        TODO()
+        val ruleLists = mutableMapOf<String, List<Rule>>()
+        val items = mutableListOf<Map<String, Long>>()
+        lines.forEach { line ->
+            if (line.startsWith("{")) {
+                items.add(parseItem(line))
+            } else {
+                val (name, rulesList) = parseRuleList(line)
+                ruleLists[name] = rulesList
+            }
+        }
+        return items.sumOf { if (pipeline(it, ruleLists)) it.values.sum() else 0L }
+    }
+
+    private fun pipeline(item: Map<String, Long>, ruleLists: MutableMap<String, List<Rule>>): Boolean {
+        var cur = "in"
+        while (true) {
+            val result = applyOn(item, ruleLists[cur]!!)
+            when (result) {
+                "A" -> return true
+                "R" -> return false
+                else -> {
+                    cur = result
+                }
+            }
+        }
+    }
+
+    private fun applyOn(item: Map<String, Long>, rules: List<Rule>): String {
+        for (r in rules) {
+            if(r.accepts(item)){
+                return r.target
+            }
+        }
+        throw RuntimeException("Item $item and rules $rules")
+    }
+
+    private fun parseItem(line: String): Map<String, Long> {
+        return line.split(Regex("[{}=,]+"))
+            .drop(1)
+            .chunked(2)
+            .filter { it.size == 2 }
+            .associate { it[0] to it[1].toLong() }
+    }
+
+    private fun parseRuleList(line: String): Pair<String, List<Rule>> {
+        val (name, rulesString) = line.split(Regex("[{}]"))
+        return name to parseRuleListString(rulesString)
+    }
+
+    private fun parseRuleListString(rulesString: String): List<Rule> {
+        return rulesString.split(",").map { ruleString ->
+            if (ruleString.contains(":")) {
+                val sign = if (ruleString.contains("<")) Sign.LT else Sign.GT
+                val (name, value, target) = ruleString.split(Regex("[<>:]"))
+                Rule(name, sign, value.toLong(), target)
+            } else {
+                Rule(UUID.randomUUID().toString(), Sign.ALWAYS, 0, ruleString)
+            }
+        }
     }
 
     private fun part2(lines: List<String>): Any {
