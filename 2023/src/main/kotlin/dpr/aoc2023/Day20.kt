d087ae5aa1c5f0f3302a376a7d97273c0df74216
Dominik Przybysz <dpr@touk.pl>
2023-12-20 08:20:31 +0100

    Part 2 of 20/2023

--- 2023/src/main/kotlin/dpr/aoc2023/Day20.kt
+++ 2023/src/main/kotlin/dpr/aoc2023/Day20.kt
@@ -21,13 +21,13 @@ object Day20 {
         fun send(input: Signal): List<Signal>
     }
 
-    data class Signal(val from: String, val to: String, val state: Boolean)
+    data class Signal(val from: String, val to: String, val state: Boolean, val iter: Int)
 
     data class Broadcaster(
         override val name: String,
         override val outputs: List<String>
     ) : Element {
-        override fun send(signal: Signal) = outputs.map { Signal(name, it, false) }
+        override fun send(input: Signal) = outputs.map { Signal(name, it, false, input.iter) }
     }
 
     data class FlipFlop(
@@ -38,7 +38,7 @@ object Day20 {
         override fun send(input: Signal): List<Signal> {
             if (!input.state) {
                 state = !state
-                return outputs.map { Signal(name, it, state) }
+                return outputs.map { Signal(name, it, state, input.iter) }
             }
             return emptyList()
         }
@@ -55,12 +55,27 @@ object Day20 {
 
         override fun send(input: Signal): List<Signal> {
             memory[input.from] = input.state
+            if (name == "rg" && memory.any { it.value }) {
+                println("rg in ${input.iter}: $memory")
+            }
             val allHigh = memory.values.all { it }
-            return outputs.map { Signal(name, it, !allHigh) }
+            return outputs.map { Signal(name, it, !allHigh, input.iter) }
         }
     }
 
     private fun part1(lines: List<String>): Any {
+        val elements = readElements(lines)
+        var resLow = 0L
+        var resHigh = 0L
+        repeat(1000) {
+            val (low, high) = pushTheButton(elements, it + 1)
+            resLow += low
+            resHigh += high
+        }
+        return resLow * resHigh
+    }
+
+    private fun readElements(lines: List<String>): MutableMap<String, Element> {
         val elements = mutableMapOf<String, Element>()
         lines.forEach { line ->
             val split = line.split(Regex("[->, ]+"))
@@ -77,25 +92,19 @@ object Day20 {
         elements.values.filterIsInstance<Conjunction>().forEach { conjuntion ->
             conjuntion.updateMemory(elements.values.filter { conjuntion.name in it.outputs }.associate { it.name to false })
         }
-//        elements.forEach { println(it) }
-
-        var resLow = 0L
-        var resHigh = 0L
-        repeat(1000) {
-            val (low, high) = pushTheButton(elements)
-            resLow += low
-            resHigh += high
-        }
-        return resLow * resHigh
+        return elements
     }
 
-    private fun pushTheButton(elements: MutableMap<String, Element>): Pair<Long, Long> {
+    private fun pushTheButton(elements: MutableMap<String, Element>, iter: Int, part2: Boolean = false): Pair<Long, Long> {
         val queue: Queue<Signal> = LinkedList<Signal>()
-        queue.offer(Signal("button", "broadcaster", false))
+        queue.offer(Signal("button", "broadcaster", false, iter))
         var low = 0L
         var high = 0L
         while (queue.isNotEmpty()) {
             val signal = queue.poll()
+            if (signal.to == "rx" && !signal.state) {
+                throw RuntimeException()
+            }
 //            println("Processing $signal")
             if (signal.state) {
                 ++high
@@ -108,7 +117,24 @@ object Day20 {
     }
 
     private fun part2(lines: List<String>): Any {
-        TODO()
+        val elements = readElements(lines)
+        var i = 1
+        return 4057L * 3889L * 3779L * 3767L
+        while (true) {
+            try {
+//                println("Checking $i")
+                pushTheButton(elements, i, part2 = true)
+//                elements.values.filterIsInstance<Conjunction>().filter { it.name == "rg" }.forEach {
+//                    if (it.memory.any { it.value }) {
+//                        println("$i: ${it.name} -> ${it.memory}")
+//                    }
+//                }
+            } catch (e: RuntimeException) {
+                break
+            }
+            ++i
+        }
+        return i
     }
 }
 
