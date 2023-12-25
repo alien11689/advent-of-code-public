203c6b2238dfb66c658799f9848a3699dbb38586
Dominik Przybysz <dpr@touk.pl>
2023-12-25 09:20:22 +0100

    Cleanup 25/2023 and prepare iterative solution

--- 2023/src/main/kotlin/dpr/aoc2023/Day25.kt
+++ 2023/src/main/kotlin/dpr/aoc2023/Day25.kt
@@ -1,6 +1,7 @@
 package dpr.aoc2023
 
 import dpr.commons.Util
+import java.util.PriorityQueue
 
 object Day25 {
     @JvmStatic
@@ -11,19 +12,65 @@ object Day25 {
     }
 
     private fun part1(lines: List<String>): Any {
-        var connections = readConnections(lines)
+        val connections = readConnections(lines)
+
+        // naive solution creating all paths and counting most used vertices works well for my input but not for test input
+        // it needs ~2 minutes to finish
+//        val vertices = connections.flatten().toSet().sorted()
+//        val counts = mutableMapOf<String, Int>()
+//        vertices.forEach { counts[it] = 0 }
+//        vertices.forEach { v ->
+////            println("Checking $v")
+//            findPaths(v, connections, counts)
+//        }
+//        val mostUsed = counts.toList().sortedBy { -it.second }.take(6)
+////        mostUsed.forEach { println(it) }
+//        val mostUsedConnections = mostUsed.flatMap { v1 ->
+//            mostUsed.map { v2 -> setOf(v1.first, v2.first) }
+//        }.filter { it in connections }.toSet()
+////        println("Most used connections $mostUsedConnections")
+//        return splitGraph(connections - mostUsedConnections)
+
+        // look at the graph in the resources
         val connectionsToCut = setOf(setOf("zhb", "vxr"), setOf("jbx", "sml"), setOf("szh", "vqj"))
-        connections = connections - connectionsToCut
-        val first = mutableSetOf(connectionsToCut.flatten().first()) // one of the cutting points
+        return splitGraph(connections - connectionsToCut)
+    }
+
+    private fun splitGraph(connections: Set<Set<String>>): Int {
+        var left = connections
+        val first = mutableSetOf(connections.flatten().first()) // one of the cutting points
         while (true) {
-            val (inside, outside) = connections.partition { it.any { it in first } }
+            val (inside, outside) = left.partition { it.any { it in first } }
             if (inside.isEmpty()) {
                 break
             }
             first.addAll(inside.flatten())
-            connections = outside.toSet()
+            left = outside.toSet()
         }
-        return first.size * connections.flatten().toSet().size
+        return first.size * left.flatten().toSet().size
+    }
+
+    private fun findPaths(v: String, connections: Set<Set<String>>, counts: MutableMap<String, Int>) {
+        val visited = mutableSetOf<String>()
+        val pq = PriorityQueue<List<String>>() { l1, l2 -> l1.size - l2.size }
+        pq.offer(listOf(v))
+        while (pq.isNotEmpty()) {
+            val cur = pq.poll()
+            val end = cur.last()
+            if (end in visited) {
+                continue
+            }
+            visited.add(end)
+            (connections.filter { end in it }.flatten().toSet() - end).filter { it !in visited }.forEach {
+                val newPath = cur + it
+                pq.offer(newPath)
+                if (newPath.size > 4) {
+                    newPath.forEach {
+                        counts[it] = counts[it]!! + 1
+                    }
+                }
+            }
+        }
     }
 
     private fun readConnections(lines: List<String>): Set<Set<String>> {
