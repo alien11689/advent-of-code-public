8bf7b36af90377e80b49706b8621c7668a9580e8
Dominik Przybysz <dpr@touk.pl>
2023-12-25 06:40:37 +0100

    Finish 25/2023 by drawing graph

--- 2023/src/main/kotlin/dpr/aoc2023/Day25.kt
+++ 2023/src/main/kotlin/dpr/aoc2023/Day25.kt
@@ -11,7 +11,36 @@ object Day25 {
     }
 
     private fun part1(lines: List<String>): Any {
-        return "TODO"
+        var connections = readConnections(lines)
+        val connectionsToCut = setOf(setOf("zhb", "vxr"), setOf("jbx", "sml"), setOf("szh", "vqj"))
+        connections = connections - connectionsToCut
+        val first = mutableSetOf(connectionsToCut.flatten().first()) // one of the cutting points
+        while (true) {
+            val (inside, outside) = connections.partition { it.any { it in first } }
+            if (inside.isEmpty()) {
+                break
+            }
+            first.addAll(inside.flatten())
+            connections = outside.toSet()
+        }
+        return first.size * connections.flatten().toSet().size
+    }
+
+    private fun readConnections(lines: List<String>): Set<Set<String>> {
+        val connections = mutableSetOf<Set<String>>()
+        //        println("strict graph {")
+        lines.forEach { line ->
+            val (source, rest) = line.split(":").map { it.trim() }
+            val targets = rest.split(" ").map { it.trim() }
+            targets.forEach { target ->
+                //                println("$source -- $target")
+                connections.add(setOf(source, target))
+            }
+        }
+        //        println("}")
+//        println(connections.size)
+//        println(connections.size)
+        return connections
     }
 }
 
