ab7a217c7772b7f07eb8a07fc6d1792e4978a475
Dominik Przybysz <dpr@touk.pl>
2024-12-01 06:28:46 +0100

    Solve 2024/1

--- 2024/src/main/java/dpr/aoc2024/Day01.java
+++ 2024/src/main/java/dpr/aoc2024/Day01.java
@@ -2,6 +2,9 @@ package dpr.aoc2024;
 
 import dpr.commons.Util;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 class Day01 implements Day {
@@ -13,6 +16,7 @@ class Day01 implements Day {
     public void execute(String... args) {
         Util.measureTime(() -> {
             var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/input.txt", dayNum()));
+//            var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/test1.txt", dayNum()));
             System.out.println(part1(lines));
             System.out.println(part2(lines));
         });
@@ -24,10 +28,39 @@ class Day01 implements Day {
     }
 
     private Object part1(List<String> lines) {
-        return null;
+        var lefts = new ArrayList<Integer>();
+        var rights = new ArrayList<Integer>();
+        lines.forEach(line -> {
+            var parts = line.split(" +");
+            int left = Integer.parseInt(parts[0]);
+            int right = Integer.parseInt(parts[1]);
+            lefts.add(left);
+            rights.add(right);
+        });
+        Collections.sort(lefts);
+        Collections.sort(rights);
+        var res = 0;
+        for(int i = 0; i < lefts.size(); i++) {
+            res += Math.abs(lefts.get(i) - rights.get(i));
+        }
+        return res;
     }
 
     private Object part2(List<String> lines) {
-        return null;
+        var lefts = new ArrayList<Integer>();
+        var rights = new ArrayList<Integer>();
+        lines.forEach(line -> {
+            var parts = line.split(" +");
+            int left = Integer.parseInt(parts[0]);
+            int right = Integer.parseInt(parts[1]);
+            lefts.add(left);
+            rights.add(right);
+        });
+        var res = 0L;
+        for (int left : lefts) {
+            long count = rights.stream().filter(r -> r.equals(left)).count();
+            res += left * count;
+        }
+        return res;
     }
 }
