abf8df1307dd2cd787b1385769004b05bb0d2fc2
Dominik Przybysz <dpr@touk.pl>
2024-12-01 08:51:00 +0100

    Solve 2024/01 part 1 in rust

--- rust-aoc/src/year2024/day202401.rs
+++ rust-aoc/src/year2024/day202401.rs
@@ -1,9 +1,21 @@
+use crate::helper::read_file_lines;
+use regex::Regex;
 use std::vec::Vec;
 
-use crate::helper::{read_file_lines, reverse_string};
-
-fn solve_part1(lines: &Vec<String>) -> u32 {
-    todo!()
+fn solve_part1(lines: &Vec<String>) -> i32 {
+    let regex = Regex::new(r" +").expect("Invalid regex");
+    let mut lefts: Vec<i32> = Vec::new();
+    let mut rights: Vec<i32> = Vec::new();
+    for line in lines {
+        let parts: Vec<&str> = regex.split(line).collect();
+        let left: i32 = parts[0].parse().unwrap();
+        let right: i32 = parts[1].parse().unwrap();
+        lefts.push(left);
+        rights.push(right);
+    }
+    lefts.sort();
+    rights.sort();
+    lefts.iter().zip(rights.iter()).map(|(a,b) | (a-b).abs() ).sum()
 }
 
 fn solve_part2(lines: &Vec<String>) -> u32 {
@@ -28,12 +40,12 @@ mod tests {
     #[test]
     fn should_part1_pass_test_input1() {
         let lines = read_file_lines("./resources/2024/01/test1.txt");
-        assert_eq!(solve_part1(&lines), 142);
+        assert_eq!(solve_part1(&lines), 11);
     }
 
     #[test]
     fn should_part2_pass_test_input2() {
-        let lines = read_file_lines("./resources/2024/01/test2.txt");
-        assert_eq!(solve_part2(&lines), 281);
+        let lines = read_file_lines("./resources/2024/01/test1.txt");
+        assert_eq!(solve_part2(&lines), 31);
     }
 }
