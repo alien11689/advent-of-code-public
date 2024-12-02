60782f9ec976a9d4ba57d5740b25367a7e72ecd2
Dominik Przybysz <alien11689@gmail.com>
2024-12-02 09:07:23 +0100

    Solve 2024/02 part 1 in Rust

--- rust-aoc/src/year2024/day202402.rs
+++ rust-aoc/src/year2024/day202402.rs
@@ -1,9 +1,52 @@
+use crate::helper::read_file_lines;
+use regex::Regex;
+use std::collections::HashSet;
 use std::vec::Vec;
 
-use crate::helper::read_file_lines;
-
 fn solve_part1(lines: &Vec<String>) -> i32 {
-    0 // TODO
+    let regex = Regex::new(r" +").unwrap();
+    let numbers: Vec<Vec<i32>> = lines
+        .iter()
+        .map(|line| {
+            regex
+                .split(line)
+                .map(|n| n.parse::<i32>().expect("valid number"))
+                .collect::<Vec<i32>>()
+        })
+        .collect();
+    let mut count = 0;
+    for set in numbers {
+        // println!("{:?}", set);
+        if is_safe(set) {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn is_safe(vec: Vec<i32>) -> bool {
+    let mut diffs: Vec<i32> = Vec::new();
+    let mut prev: Option<i32> = None;
+    for cur in vec {
+        match prev {
+            None => {
+                prev = Some(cur);
+            }
+            Some(p) => {
+                diffs.push(p - cur);
+                prev = Some(cur);
+            }
+        }
+    }
+    // println!("{:?}", diffs);
+    let mut valid: HashSet<i32> = HashSet::new();
+    valid.insert(1);
+    valid.insert(2);
+    valid.insert(3);
+    let correct = (diffs.iter().all(|i| *i > 0) || diffs.iter().all(|i| *i < 0))
+        && diffs.iter().map(|i| i.abs()).all(|i| valid.contains(&i));
+    // println!("{:?}", correct);
+    correct
 }
 
 fn solve_part2(lines: &Vec<String>) -> i32 {
@@ -26,12 +69,12 @@ mod tests {
     #[test]
     fn should_part1_pass_test_input1() {
         let lines = read_file_lines("./resources/2024/02/test1.txt");
-        assert_eq!(solve_part1(&lines), 0);
+        assert_eq!(solve_part1(&lines), 2);
     }
 
     #[test]
     fn should_part2_pass_test_input2() {
         let lines = read_file_lines("./resources/2024/02/test1.txt");
-        assert_eq!(solve_part2(&lines), 0);
+        assert_eq!(solve_part2(&lines), 4);
     }
 }
