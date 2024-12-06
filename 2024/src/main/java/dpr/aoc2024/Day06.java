b626821527c5004903cc201a5a332b0550e8e130
Dominik Przybysz <alien11689@gmail.com>
2024-12-06 20:17:17 +0100

    Solve 2024/06

--- 2024/src/main/java/dpr/aoc2024/Day06.java
+++ 2024/src/main/java/dpr/aoc2024/Day06.java
@@ -1,8 +1,15 @@
 package dpr.aoc2024;
 
+import dpr.commons.Dir;
+import dpr.commons.Point2D;
 import dpr.commons.Util;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 class Day06 implements Day {
     public static void main(String... args) {
@@ -13,8 +20,8 @@ class Day06 implements Day {
     public void execute(String... args) {
         Util.measureTime(() -> {
             var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/input.txt", dayNum()));
-            System.out.println(part1(lines));
-            System.out.println(part2(lines));
+//            var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/test1.txt", dayNum()));
+            part1And2(lines);
         });
     }
 
@@ -23,11 +30,67 @@ class Day06 implements Day {
         return 6;
     }
 
-    private Object part1(List<String> lines) {
-        return null;
+    record Position(Point2D point, Dir dir) {
     }
 
-    private Object part2(List<String> lines) {
-        return null;
+    private void part1And2(List<String> lines) {
+        Point2D current = null;
+        Set<Point2D> blocks = new HashSet<>();
+        for (int y = 0; y < lines.size(); y++) {
+            String line = lines.get(y);
+            for (int x = 0; x < line.length(); x++) {
+                if (line.charAt(x) == '#') {
+                    blocks.add(new Point2D(x, y));
+                } else if (line.charAt(x) == '^') {
+                    current = new Point2D(x, y);
+                }
+            }
+        }
+        Position starting = new Position(current, Dir.N);
+        Set<Point2D> visited = travel(lines, current, blocks);
+        System.out.println(visited.size());
+        Set<Point2D> possibleObstacle = visited.stream()
+                .filter(p -> !p.equals(starting.point) && !blocks.contains(p))
+                .collect(Collectors.toSet());
+        int count = 0;
+        for (Point2D obstacle : possibleObstacle) {
+            HashSet<Point2D> newBlocks = new HashSet<>(blocks);
+            newBlocks.add(obstacle);
+            Set<Point2D> newVisited = travel(lines, current, newBlocks);
+            if (newVisited.isEmpty()) {
+                ++count;
+//                System.out.println("Obstacle: " + obstacle);
+            }
+        }
+        System.out.println(count);
+    }
+
+    @NotNull
+    private static Set<Point2D> travel(List<String> lines, Point2D current, Set<Point2D> blocks) {
+        Dir dir = Dir.N;
+        Set<Point2D> visited = new HashSet<>();
+        visited.add(current);
+        Set<Position> memory = new HashSet<>();
+        int xLimit = lines.get(1).length();
+        int yLimit = lines.size();
+        while (true) {
+            Position p = new Position(current, dir);
+            if (memory.contains(p)) {
+                return Collections.emptySet();
+            }
+            memory.add(p);
+            Point2D next = current.move(dir, 1);
+            if (blocks.contains(next)) {
+                dir = dir.turnRight();
+            } else {
+                if (next.getX() < 0 || next.getY() < 0 || next.getX() >= xLimit || next.getY() >= yLimit) {
+                    break;
+                } else {
+                    visited.add(next);
+                    current = next;
+                }
+            }
+        }
+        return visited;
     }
 }
