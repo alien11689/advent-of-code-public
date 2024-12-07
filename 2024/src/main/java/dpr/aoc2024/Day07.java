676957d3fb58f82eb30ec233f19b096c04e8d454
Dominik Przybysz <alien11689@gmail.com>
2024-12-07 12:49:55 +0100

    Solve 2024/07

--- 2024/src/main/java/dpr/aoc2024/Day07.java
+++ 2024/src/main/java/dpr/aoc2024/Day07.java
@@ -2,7 +2,10 @@ package dpr.aoc2024;
 
 import dpr.commons.Util;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 class Day07 implements Day {
     public static void main(String... args) {
@@ -13,6 +16,7 @@ class Day07 implements Day {
     public void execute(String... args) {
         Util.measureTime(() -> {
             var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/input.txt", dayNum()));
+//            var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/test1.txt", dayNum()));
             System.out.println(part1(lines));
             System.out.println(part2(lines));
         });
@@ -24,10 +28,50 @@ class Day07 implements Day {
     }
 
     private Object part1(List<String> lines) {
-        return null;
+        return lines.stream()
+                .mapToLong(l -> calculatePart1(l, false))
+                .sum();
+    }
+
+    record Partial(long first, List<Long> rest) {
+    }
+
+    private long calculatePart1(String line, boolean part2) {
+//        System.out.println("Checking "+ line);
+        String[] parts = line.split(": ");
+        long target = Long.parseLong(parts[0]);
+        List<Long> values = Arrays.stream(parts[1].split(" "))
+                .map(Long::parseLong)
+                .toList();
+        Queue<Partial> q = new LinkedList<>();
+        q.offer(new Partial(values.getFirst(), values.stream().skip(1).toList()));
+        while (!q.isEmpty()) {
+            Partial p = q.poll();
+//            System.out.println("Checking "+ p + " for " + target);
+            if (p.first > target) {
+                continue;
+            }
+            if (p.rest.isEmpty()) {
+                if (p.first == target) {
+                    return target;
+                }
+                continue;
+            }
+            long next = p.rest.getFirst();
+            List<Long> nextRest = p.rest.stream().skip(1).toList();
+            q.offer(new Partial(p.first + next, nextRest));
+            q.offer(new Partial(p.first * next, nextRest));
+            if (part2) {
+                q.offer(new Partial(Long.parseLong(p.first + "" + next), nextRest));
+            }
+        }
+        return 0L;
     }
 
     private Object part2(List<String> lines) {
-        return null;
+        return lines.stream()
+                .mapToLong(l -> calculatePart1(l, true))
+                .sum();
     }
+
 }
