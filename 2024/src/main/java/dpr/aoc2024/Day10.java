c625f850709f75fe2520d13676d4b09b329c128c
Dominik Przybysz <alien11689@gmail.com>
2024-12-10 20:36:01 +0100

    Solve 2024/10

--- 2024/src/main/java/dpr/aoc2024/Day10.java
+++ 2024/src/main/java/dpr/aoc2024/Day10.java
@@ -1,8 +1,14 @@
 package dpr.aoc2024;
 
+import dpr.commons.Point2D;
 import dpr.commons.Util;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 
 class Day10 implements Day {
     public static void main(String... args) {
@@ -12,9 +18,9 @@ class Day10 implements Day {
     @Override
     public void execute(String... args) {
         Util.measureTime(() -> {
+//            var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/test1.txt", dayNum()));
             var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/input.txt", dayNum()));
-            System.out.println(part1(lines));
-            System.out.println(part2(lines));
+            part1And2(lines);
         });
     }
 
@@ -23,11 +29,45 @@ class Day10 implements Day {
         return 10;
     }
 
-    private Object part1(List<String> lines) {
-        return null;
+    record Position(Point2D loc, int num, Point2D start) {
     }
 
-    private Object part2(List<String> lines) {
-        return null;
+    private void part1And2(List<String> lines) {
+        List<List<Integer>> map = lines.stream().map(line -> {
+                    List<Integer> l = new ArrayList<>();
+                    char[] chars = line.toCharArray();
+                    for (char aChar : chars) {
+                        l.add(aChar - '0');
+                    }
+                    return l;
+                }
+        ).toList();
+        Queue<Position> q = new LinkedList<>();
+        for (int y = 0; y < map.size(); ++y) {
+            for (int x = 0; x < map.get(y).size(); ++x) {
+                if (map.get(y).get(x) == 0) {
+                    Point2D start = new Point2D(x, y);
+                    q.add(new Position(start, 0, start));
+                }
+            }
+        }
+
+        Set<Position> part1 = new HashSet<>();
+        List<Position> part2 = new ArrayList<>();
+        while (!q.isEmpty()) {
+            Position position = q.poll();
+            if (position.num == 9) {
+                part1.add(position);
+                part2.add(position);
+                continue;
+            }
+            position.loc.neighboursCross()
+                    .stream()
+                    .filter(n -> n.getX() >= 0 && n.getY() >= 0 && n.getY() < map.size() && n.getX() < map.get(0).size())
+                    .filter(n -> map.get(n.getY()).get(n.getX()) == position.num + 1)
+                    .forEach(p -> q.offer(new Position(p, position.num + 1, position.start)));
+        }
+        System.out.println(part1.size());
+        System.out.println(part2.size());
     }
 }
