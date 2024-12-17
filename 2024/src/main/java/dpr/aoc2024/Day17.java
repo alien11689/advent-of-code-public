89cec0507257839a4e695d75433380abfae9ee6f
Dominik Przybysz <alien11689@gmail.com>
2024-12-17 22:03:56 +0100

    Solve 2024/17 part 2

--- 2024/src/main/java/dpr/aoc2024/Day17.java
+++ 2024/src/main/java/dpr/aoc2024/Day17.java
@@ -1,6 +1,7 @@
 package dpr.aoc2024;
 
 import dpr.commons.Util;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,6 +18,7 @@ class Day17 implements Day {
         Util.measureTime(() -> {
             var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/input.txt", dayNum()));
 //            var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/test1.txt", dayNum()));
+//            var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/test2.txt", dayNum()));
             System.out.println(part1(lines));
             System.out.println(part2(lines));
         });
@@ -31,17 +33,45 @@ class Day17 implements Day {
         int a = Integer.parseInt(lines.get(0).split(": ")[1]);
         int b = Integer.parseInt(lines.get(1).split(": ")[1]);
         int c = Integer.parseInt(lines.get(2).split(": ")[1]);
-        List<Integer> program = Arrays.stream(lines.get(3).split(": ")[1].split(",")).map(Integer::parseInt).toList();
-//        System.out.println(a);
-//        System.out.println(b);
-//        System.out.println(c);
-//        System.out.println(program);
+        List<Long> program = Arrays.stream(lines.get(3).split(": ")[1].split(",")).map(Long::parseLong).toList();
+        List<Long> result = runProgram(program, a, b, c);
+        //1,1,6,6,2,6,3,2,4 wrong
+        return result.stream().map(String::valueOf).collect(Collectors.joining(","));
+    }
+
+    private Object part2(List<String> lines) {
+//        int a = Integer.parseInt(lines.get(0).split(": ")[1]);
+        long initB = Integer.parseInt(lines.get(1).split(": ")[1]);
+        long initC = Integer.parseInt(lines.get(2).split(": ")[1]);
+        List<Long> program = Arrays.stream(lines.get(3).split(": ")[1].split(",")).map(Long::parseLong).toList();
+        long initA = 0L;
+        int suffix = 4;
+        List<Long> subProgram = program.subList(program.size() - suffix, program.size());
+        while (true) {
+            long a = ++initA;
+            List<Long> result = runProgram(program, a, initB, initC);
+            if (result.equals(subProgram)) {
+//                String collect = result.stream().map(String::valueOf).collect(Collectors.joining(","));
+//                System.out.println("Checking a = " + initA + " gives " + collect);
+                if (result.equals(program)) {
+                    return initA;
+                }
+                ++suffix;
+                subProgram = program.subList(program.size() - suffix, program.size());
+                initA *= 8L;
+                initA -= 1;
+            }
+        }
+    }
+
+    @NotNull
+    private static List<Long> runProgram(List<Long> program, long a, long b, long c) {
         int i = 0;
-        List<Integer> result = new ArrayList<>();
+        List<Long> result = new ArrayList<>();
         while (i < program.size()) {
-            final int command = program.get(i);
-            final int operand = program.get(i + 1);
-            int realOperand;
+            final long command = program.get(i);
+            final long operand = program.get(i + 1);
+            long realOperand;
             if (operand == 4) {
                 realOperand = a;
             } else if (operand == 5) {
@@ -66,7 +96,7 @@ class Day17 implements Day {
             } else if (command == 3) {
 //                System.out.println("Running jnz " + operand);
                 if (a != 0) {
-                    i = operand;
+                    i = (int) operand;
                     continue;
                 }
             } else if (command == 4) {
@@ -74,7 +104,7 @@ class Day17 implements Day {
                 b = b ^ c;
             } else if (command == 5) {
 //                System.out.println("Running out " + operand);
-                int val = realOperand % 8;
+                long val = realOperand % 8;
 //                System.out.println("Printing " + val);
                 result.add(val);
             } else if (command == 6) {
@@ -88,11 +118,18 @@ class Day17 implements Day {
             }
             i += 2;
         }
-        //1,1,6,6,2,6,3,2,4 wrong
-        return result.stream().map(String::valueOf).collect(Collectors.joining(","));
-    }
-
-    private Object part2(List<String> lines) {
-        return null;
+        return result;
     }
 }
+
+/*
+while (a !=0) {
+b = b % a; // initially 0, then always in 0..7
+b = b ^ 1 // last significant bit switched -> -1 if odd, +1 if even
+c = a / 2^b //
+a = a / 8 // a looses 3 most significant bits <--- the most important
+b = b ^ 4  // 2 peniultimate bits are switched
+b = b^c // b is 0 when b == c
+out b
+}
+*/
