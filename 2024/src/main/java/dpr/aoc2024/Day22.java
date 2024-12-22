252dd12b67ecc209349c9b3865e266a2e344e52e
Dominik Przybysz <alien11689@gmail.com>
2024-12-22 10:36:40 +0100

    Speed up 2024/22

--- 2024/src/main/java/dpr/aoc2024/Day22.java
+++ 2024/src/main/java/dpr/aoc2024/Day22.java
@@ -2,13 +2,13 @@ package dpr.aoc2024;
 
 import dpr.commons.Util;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 class Day22 implements Day {
     public static void main(String... args) {
@@ -32,35 +32,30 @@ class Day22 implements Day {
     }
 
     private void part1And2(List<String> lines) {
-        long part1 = 0;
-        Map<String, Integer> prices = new HashMap<>();
-        for (String line : lines) {
-            long init = Long.parseLong(line);
-            part1 += calculate(init, 2000, prices);
-        }
+        Map<String, Integer> prices = new ConcurrentHashMap<>();
+        long part1 = lines.stream().parallel().mapToLong(line -> calculate(Long.parseLong(line), 2000, prices)).sum();
         System.out.println(part1);
-        long part2 = prices.values().stream().mapToLong(l -> l).max().orElse(Long.MIN_VALUE);
+        int part2 = prices.values().stream().mapToInt(l -> l).max().orElse(Integer.MIN_VALUE);
         System.out.println(part2);
-        ;
     }
 
     private long calculate(long init, int iter, Map<String, Integer> prices) {
         long cur = init;
-        long last = cur % 10;
-        Queue<Long> q = new LinkedList<>();
+        int last = (int) (cur % 10);
+        Queue<Integer> q = new LinkedList<>();
         Set<String> seen = new HashSet<>();
         for (int i = 1; i <= iter; i++) {
             cur = (cur ^ (cur << 6)) % 16777216;
             cur = (cur ^ (cur >> 5)) % 16777216;
             cur = (cur ^ (cur << 11)) % 16777216;
-            long newlast = cur % 10;
+            int newlast = (int) (cur % 10);
             if (q.size() == 4) {
                 q.poll();
             }
             q.add(newlast - last);
             String key = q.toString();
             if (!seen.contains(key) && q.size() == 4) {
-                prices.put(key, prices.getOrDefault(key, 0) + (int) newlast);
+                prices.compute(key, (k, v) -> v == null ? newlast : v + newlast);
                 seen.add(key);
             }
 //            System.out.println(init + " in iteration " + i + " " + key + " -> " + newlast);
