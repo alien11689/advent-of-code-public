6256898084fde6c1f52ae1d3a2a6faa959d88e0c
Dominik Przybysz <alien11689@gmail.com>
2024-12-22 08:50:09 +0100

    Solve 2024/22 part 1

--- 2024/src/main/java/dpr/aoc2024/Day22.java
+++ 2024/src/main/java/dpr/aoc2024/Day22.java
@@ -13,6 +13,8 @@ class Day22 implements Day {
     public void execute(String... args) {
         Util.measureTime(() -> {
             var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/input.txt", dayNum()));
+//            var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/test1.txt", dayNum()));
+//            var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/test2.txt", dayNum()));
             System.out.println(part1(lines));
             System.out.println(part2(lines));
         });
@@ -24,7 +26,24 @@ class Day22 implements Day {
     }
 
     private Object part1(List<String> lines) {
-        return null;
+        long sum = 0;
+        for (String line : lines) {
+            long init = Long.parseLong(line);
+            long secret = calculatePseudo(init, 2000);
+            System.out.println("For " + init + " secret: " + secret);
+            sum += secret;
+        }
+        return sum;
+    }
+
+    private long calculatePseudo(long init, int iter) {
+        long cur = init;
+        for (int i = 1; i <= iter; i++) {
+            cur = (cur ^ (cur << 6)) % 16777216;
+            cur = (cur ^ (cur >> 5)) % 16777216;
+            cur = (cur ^ (cur << 11)) % 16777216;
+        }
+        return cur;
     }
 
     private Object part2(List<String> lines) {
