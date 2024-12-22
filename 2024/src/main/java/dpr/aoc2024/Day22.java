8f4a6126da773afdeb2c254e573d59cd53cd96e4
Dominik Przybysz <alien11689@gmail.com>
2024-12-22 08:51:09 +0100

    Solve 2024/22 part 2

--- 2024/src/main/java/dpr/aoc2024/Day22.java
+++ 2024/src/main/java/dpr/aoc2024/Day22.java
@@ -2,7 +2,14 @@ package dpr.aoc2024;
 
 import dpr.commons.Util;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 class Day22 implements Day {
     public static void main(String... args) {
@@ -15,6 +22,7 @@ class Day22 implements Day {
             var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/input.txt", dayNum()));
 //            var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/test1.txt", dayNum()));
 //            var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/test2.txt", dayNum()));
+//            var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/test3.txt", dayNum()));
             System.out.println(part1(lines));
             System.out.println(part2(lines));
         });
@@ -30,7 +38,6 @@ class Day22 implements Day {
         for (String line : lines) {
             long init = Long.parseLong(line);
             long secret = calculatePseudo(init, 2000);
-            System.out.println("For " + init + " secret: " + secret);
             sum += secret;
         }
         return sum;
@@ -47,6 +54,45 @@ class Day22 implements Day {
     }
 
     private Object part2(List<String> lines) {
-        return null;
+        Set<String> keys = new HashSet<>();
+        List<Map<String, Integer>> pricesList = new ArrayList<>();
+        for (String line : lines) {
+            long init = Long.parseLong(line);
+            Map<String, Integer> prices = calculatePseudo2(init, 2000);
+            pricesList.add(prices);
+            keys.addAll(prices.keySet());
+        }
+        long best = Long.MIN_VALUE;
+        for (String key : keys) {
+            long sum = pricesList.stream().mapToLong(prices -> prices.getOrDefault(key, 0)).sum();
+            if (sum > best) {
+                best = sum;
+            }
+        }
+        return best;
+    }
+
+    private Map<String, Integer> calculatePseudo2(long init, int iter) {
+        long cur = init;
+        long last = cur % 10;
+        BlockingQueue<Long> q = new ArrayBlockingQueue<>(4);
+        Map<String, Integer> prices = new HashMap<>();
+        for (int i = 1; i <= iter; i++) {
+            cur = (cur ^ (cur << 6)) % 16777216;
+            cur = (cur ^ (cur >> 5)) % 16777216;
+            cur = (cur ^ (cur << 11)) % 16777216;
+            long newlast = cur % 10;
+            if (q.size() == 4) {
+                q.poll();
+            }
+            q.add(newlast - last);
+            String key = q.toString();
+            if (!prices.containsKey(key)) {
+                prices.put(key, (int) newlast);
+            }
+//            System.out.println(init + " in iteration " + i + " " + key + " -> " + newlast);
+            last = newlast;
+        }
+        return prices;
     }
 }
