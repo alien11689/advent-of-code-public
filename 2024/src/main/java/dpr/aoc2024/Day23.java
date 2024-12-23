e50606c64b31f4b1edd324785a1a3831da0b391a
Dominik Przybysz <alien11689@gmail.com>
2024-12-23 09:35:03 +0100

    Solve 2024/23 part 1

--- 2024/src/main/java/dpr/aoc2024/Day23.java
+++ 2024/src/main/java/dpr/aoc2024/Day23.java
@@ -2,7 +2,14 @@ package dpr.aoc2024;
 
 import dpr.commons.Util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 class Day23 implements Day {
     public static void main(String... args) {
@@ -13,6 +20,7 @@ class Day23 implements Day {
     public void execute(String... args) {
         Util.measureTime(() -> {
             var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/input.txt", dayNum()));
+//            var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/test1.txt", dayNum()));
             System.out.println(part1(lines));
             System.out.println(part2(lines));
         });
@@ -24,7 +32,34 @@ class Day23 implements Day {
     }
 
     private Object part1(List<String> lines) {
-        return null;
+        Set<String> edges = new HashSet<>();
+        Set<List<String>> connections = lines.stream().map(line -> {
+            String[] parts = line.split("-");
+            List<String> list = Arrays.asList(parts);
+            Collections.sort(list);
+            edges.addAll(list);
+            return list;
+        }).collect(Collectors.toSet());
+        Set<List<String>> lans = new HashSet<>();
+        edges.stream().filter(e -> e.startsWith("t")).forEach(edge -> {
+//            System.out.println(edge);
+            List<String> connected = connections.stream().filter(c -> c.contains(edge)).flatMap(Collection::stream).filter(e -> !e.equals(edge)).sorted().toList();
+            for (int i = 0; i < connected.size() - 1; i++) {
+                for (int j = i + 1; j < connected.size(); j++) {
+                    String a = connected.get(i);
+                    String b = connected.get(j);
+//                    System.out.println("Checking " + a + " and " + b);
+                    List<String> list = new ArrayList<>(Arrays.asList(a, b));
+                    if (connections.contains(list)) {
+                        list.add(edge);
+                        Collections.sort(list);
+                        lans.add(list);
+                    }
+                }
+            }
+        });
+
+        return lans.size();
     }
 
     private Object part2(List<String> lines) {
