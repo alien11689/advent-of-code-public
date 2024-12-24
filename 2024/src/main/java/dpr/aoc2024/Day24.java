36721546db763a32d52ce88e6b8389bad965387f
Dominik Przybysz <alien11689@gmail.com>
2024-12-24 12:21:43 +0100

    Simplify 2024/24

--- 2024/src/main/java/dpr/aoc2024/Day24.java
+++ 2024/src/main/java/dpr/aoc2024/Day24.java
@@ -33,6 +33,10 @@ class Day24 implements Day {
 
     interface Apply {
         boolean calculate(Map<String, Boolean> inputs);
+
+        String target();
+
+        Apply changeTarget(String target);
     }
 
     record And(String a, String b, String target) implements Apply {
@@ -43,6 +47,11 @@ class Day24 implements Day {
                 return true;
             }
             return false;
+        }
+
+        @Override
+        public Apply changeTarget(String target) {
+            return new And(a, b, target);
         }
     }
 
@@ -55,6 +64,11 @@ class Day24 implements Day {
             }
             return false;
         }
+
+        @Override
+        public Apply changeTarget(String target) {
+            return new Or(a, b, target);
+        }
     }
 
     record Xor(String a, String b, String target) implements Apply {
@@ -66,12 +80,23 @@ class Day24 implements Day {
             }
             return false;
         }
+
+        @Override
+        public Apply changeTarget(String target) {
+            return new Xor(a, b, target);
+        }
     }
 
     private Object part1(List<String> lines) {
         Set<Apply> rules = new HashSet<>();
         Map<String, Boolean> inputs = new HashMap<>();
-        Set<String> zTargets = new HashSet<>();
+        parseInput(lines, inputs, rules);
+        calculate(rules, inputs);
+        return readNum(inputs, "z");
+    }
+
+    private static void parseInput(List<String> lines, Map<String, Boolean> inputs, Set<Apply> rules) {
+        //        System.out.println("strict digraph {");
         lines.forEach(line -> {
             if (line.contains(":")) {
                 String[] parts = line.split(": ");
@@ -87,14 +112,13 @@ class Day24 implements Day {
                     case "XOR" -> new Xor(left, right, target);
                     default -> throw new IllegalStateException("Unexpected value: " + parts[1]);
                 };
-                if (target.startsWith("z")) {
-                    zTargets.add(target);
-                }
+//                System.out.println(left + " -> " + parts[1] + "_" + left + "_" + right);
+//                System.out.println(right + " -> " + parts[1] + "_" + left + "_" + right);
+//                System.out.println(parts[1] + "_" + left + "_" + right + " -> " + target);
                 rules.add(rule);
             }
         });
-        calculate(rules, inputs);
-        return readNum(inputs, "z");
+        //        System.out.println("}");
     }
 
     private static long readNum(Map<String, Boolean> inputs, String num) {
@@ -108,68 +132,23 @@ class Day24 implements Day {
     private Object part2(List<String> lines) {
         Set<Apply> rules = new HashSet<>();
         Map<String, Boolean> inputs = new HashMap<>();
-        Set<String> zTargets = new HashSet<>();
-//        System.out.println("strict digraph {");
-        lines.forEach(line -> {
-            if (line.contains(":")) {
-                String[] parts = line.split(": ");
-                inputs.put(parts[0], "1".equals(parts[1]));
-            } else {
-                String[] parts = line.split("[ \\->]+");
-                String left = parts[0];
-                String right = parts[2];
-                String target = parts[3];
-                if (target.equals("z07")) {
-                    target = "vmv";
-                } else if (target.equals("vmv")) {
-                    target = "z07";
-                } else if (target.equals("z20")) {
-                    target = "kfm";
-                } else if (target.equals("kfm")) {
-                    target = "z20";
-                } else if (target.equals("z28")) {
-                    target = "hnv";
-                } else if (target.equals("hnv")) {
-                    target = "z28";
-                } else if (target.equals("hth")) {
-                    target = "tqr";
-                } else if (target.equals("tqr")) {
-                    target = "hth";
-                }
-                var rule = switch (parts[1]) {
-                    case "AND" -> new And(left, right, target);
-                    case "OR" -> new Or(left, right, target);
-                    case "XOR" -> new Xor(left, right, target);
-                    default -> throw new IllegalStateException("Unexpected value: " + parts[1]);
-                };
-//                System.out.println(left + " -> " + parts[1] + "_" + left + "_" + right);
-//                System.out.println(right + " -> " + parts[1] + "_" + left + "_" + right);
-//                System.out.println(parts[1] + "_" + left + "_" + right + " -> " + target);
-                if (target.startsWith("z")) {
-                    zTargets.add(target);
-                }
-                rules.add(rule);
-            }
-        });
-//        System.out.println("}");
+        parseInput(lines, inputs, rules);
+        // all Z outputs come from XOR
+        // single OR joins partial adders
+        // XOR cannot go to OR
+        // no two AND in row
+        rules = swapOutputs(rules, "z07", "vmv");
+        rules = swapOutputs(rules, "z20", "kfm");
+        rules = swapOutputs(rules, "z28", "hnv");
+        rules = swapOutputs(rules, "tqr", "hth");
         calculate(rules, inputs);
         long y = readNum(inputs, "y");
         long x = readNum(inputs, "x");
         long z = readNum(inputs, "z");
 //        show(x, y, z);
-        List<String> list = Arrays.asList(
-                "z07",
-                "vmv",
-
-                "z20",
-                "kfm",
-
-                "z28",
-                "hnv",
-
-                "hth",
-                "tqr"
-        );
+        if (x + y != z) {
+            throw new IllegalStateException("Unexpected value: " + x + " + " + y + " = " + z + " (expected " + (x + y));
+        }
         // input 2
         Map<String, Boolean> inputs2 = new HashMap<>();
         inputs.entrySet().forEach(e -> {
@@ -185,23 +164,27 @@ class Day24 implements Day {
         y = readNum(inputs2, "y");
         z = readNum(inputs2, "z");
 //        show(x, y, z);
+        if (x + y != z) {
+            throw new IllegalStateException("Unexpected value: " + x + " + " + y + " = " + z + " (expected " + (x + y));
+        }
+        List<String> list = Arrays.asList(
+                "z07",
+                "vmv",
 
-        // input 3
-        Map<String, Boolean> inputs3 = new HashMap<>();
-        inputs.entrySet().forEach(e -> {
-            if (e.getKey().startsWith("x")) {
-                inputs3.put(e.getKey(), false);
-            }
-            if (e.getKey().startsWith("y")) {
-                inputs3.put(e.getKey(), false);
-            }
-        });
-        calculate(rules, inputs3);
-        x = readNum(inputs3, "x");
-        y = readNum(inputs3, "y");
-        z = readNum(inputs3, "z");
-//        show(x, y, z);
+                "z20",
+                "kfm",
+
+                "z28",
+                "hnv",
+
+                "hth",
+                "tqr"
+        );
         return list.stream().sorted().collect(Collectors.joining(","));
+    }
+
+    private Set<Apply> swapOutputs(Set<Apply> rules, String out1, String out2) {
+        return rules.stream().map(r -> r.target().equals(out1) ? r.changeTarget(out2) : (r.target().equals(out2) ? r.changeTarget(out1) : r)).collect(Collectors.toSet());
     }
 
     private static void show(long x, long y, long z) {
