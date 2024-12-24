6c437344c021c950f17cb30b57fa2dcfb648f44f
Dominik Przybysz <alien11689@gmail.com>
2024-12-24 21:12:09 +0100

    Detect wrong target automatically in 2024/24

--- 2024/src/main/java/dpr/aoc2024/Day24.java
+++ 2024/src/main/java/dpr/aoc2024/Day24.java
@@ -1,8 +1,8 @@
 package dpr.aoc2024;
 
 import dpr.commons.Util;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -19,6 +19,7 @@ class Day24 implements Day {
     public void execute(String... args) {
         Util.measureTime(() -> {
             var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/input.txt", dayNum()));
+//            var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/input2.txt", dayNum()));
 //            var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/test1.txt", dayNum()));
 //            var lines = Util.getNotEmptyLinesFromFile(String.format("/%02d/test2.txt", dayNum()));
             System.out.println(part1(lines));
@@ -31,72 +32,44 @@ class Day24 implements Day {
         return 24;
     }
 
-    interface Apply {
-        boolean calculate(Map<String, Boolean> inputs);
-
-        String target();
-
-        Apply changeTarget(String target);
-    }
-
-    record And(String a, String b, String target) implements Apply {
-        @Override
-        public boolean calculate(Map<String, Boolean> inputs) {
+    record Rule(Type type, String a, String b, String target) {
+        boolean calculate(Map<String, Boolean> inputs) {
             if (inputs.containsKey(a) && inputs.containsKey(b) && !inputs.containsKey(target)) {
-                inputs.put(target, inputs.get(a) && inputs.get(b));
+//                System.out.println("Calculated " + type + ": " + a + " " + b + " ->" + target);
+                switch (type) {
+                    case AND -> inputs.put(target, inputs.get(a) && inputs.get(b));
+                    case OR -> inputs.put(target, inputs.get(a) || inputs.get(b));
+                    case XOR -> inputs.put(target, inputs.get(a) && !inputs.get(b) || !inputs.get(a) && inputs.get(b));
+                }
                 return true;
             }
             return false;
         }
 
-        @Override
-        public Apply changeTarget(String target) {
-            return new And(a, b, target);
+        Rule changeTarget(String target) {
+            return new Rule(type, a, b, target);
         }
+
+        boolean hasInputs(String i, String j) {
+            return a().equals(i) && b().equals(j) || a().equals(j) && b().equals(i);
+        }
+
     }
 
-    record Or(String a, String b, String target) implements Apply {
-        @Override
-        public boolean calculate(Map<String, Boolean> inputs) {
-            if (inputs.containsKey(a) && inputs.containsKey(b) && !inputs.containsKey(target)) {
-                inputs.put(target, inputs.get(a) || inputs.get(b));
-                return true;
-            }
-            return false;
-        }
-
-        @Override
-        public Apply changeTarget(String target) {
-            return new Or(a, b, target);
-        }
-    }
-
-    record Xor(String a, String b, String target) implements Apply {
-        @Override
-        public boolean calculate(Map<String, Boolean> inputs) {
-            if (inputs.containsKey(a) && inputs.containsKey(b) && !inputs.containsKey(target)) {
-                inputs.put(target, inputs.get(a) && !inputs.get(b) || !inputs.get(a) && inputs.get(b));
-                return true;
-            }
-            return false;
-        }
-
-        @Override
-        public Apply changeTarget(String target) {
-            return new Xor(a, b, target);
-        }
+    enum Type {
+        AND, OR, XOR
     }
 
     private Object part1(List<String> lines) {
-        Set<Apply> rules = new HashSet<>();
+        Set<Rule> rules = new HashSet<>();
         Map<String, Boolean> inputs = new HashMap<>();
         parseInput(lines, inputs, rules);
         calculate(rules, inputs);
         return readNum(inputs, "z");
     }
 
-    private static void parseInput(List<String> lines, Map<String, Boolean> inputs, Set<Apply> rules) {
-        //        System.out.println("strict digraph {");
+    private static void parseInput(List<String> lines, Map<String, Boolean> inputs, Set<Rule> rules) {
+//        System.out.println("strict digraph {");
         lines.forEach(line -> {
             if (line.contains(":")) {
                 String[] parts = line.split(": ");
@@ -106,19 +79,14 @@ class Day24 implements Day {
                 final String left = parts[0];
                 final String right = parts[2];
                 String target = parts[3];
-                var rule = switch (parts[1]) {
-                    case "AND" -> new And(left, right, target);
-                    case "OR" -> new Or(left, right, target);
-                    case "XOR" -> new Xor(left, right, target);
-                    default -> throw new IllegalStateException("Unexpected value: " + parts[1]);
-                };
+                var type = Type.valueOf(parts[1]);
 //                System.out.println(left + " -> " + parts[1] + "_" + left + "_" + right);
 //                System.out.println(right + " -> " + parts[1] + "_" + left + "_" + right);
 //                System.out.println(parts[1] + "_" + left + "_" + right + " -> " + target);
-                rules.add(rule);
+                rules.add(new Rule(type, left, right, target));
             }
         });
-        //        System.out.println("}");
+//        System.out.println("}");
     }
 
     private static long readNum(Map<String, Boolean> inputs, String num) {
@@ -130,13 +98,12 @@ class Day24 implements Day {
     }
 
     private Object part2(List<String> lines) {
-        Set<Apply> rules = new HashSet<>();
+        Set<Rule> rules = new HashSet<>();
         Map<String, Boolean> inputs = new HashMap<>();
         parseInput(lines, inputs, rules);
-        // all Z outputs come from XOR
-        // single OR joins partial adders
-        // XOR cannot go to OR
-        // no two AND in row
+        long zTargetCounts = rules.stream().filter(r -> r.target().startsWith("z")).count();
+        Set<String> wrongTargets = findWrongTargets(rules, zTargetCounts);
+        // rules swap detected manually
         rules = swapOutputs(rules, "z07", "vmv");
         rules = swapOutputs(rules, "z20", "kfm");
         rules = swapOutputs(rules, "z28", "hnv");
@@ -149,16 +116,9 @@ class Day24 implements Day {
         if (x + y != z) {
             throw new IllegalStateException("Unexpected value: " + x + " + " + y + " = " + z + " (expected " + (x + y));
         }
-        // input 2
-        Map<String, Boolean> inputs2 = new HashMap<>();
-        inputs.entrySet().forEach(e -> {
-            if (e.getKey().startsWith("x")) {
-                inputs2.put(e.getKey(), true);
-            }
-            if (e.getKey().startsWith("y")) {
-                inputs2.put(e.getKey(), true);
-            }
-        });
+        // input 2 - only 1
+        long maxX = Long.parseLong("1".repeat((int) zTargetCounts - 1), 2);
+        Map<String, Boolean> inputs2 = generateInput(maxX, maxX, zTargetCounts - 1);
         calculate(rules, inputs2);
         x = readNum(inputs2, "x");
         y = readNum(inputs2, "y");
@@ -167,23 +127,94 @@ class Day24 implements Day {
         if (x + y != z) {
             throw new IllegalStateException("Unexpected value: " + x + " + " + y + " = " + z + " (expected " + (x + y));
         }
-        List<String> list = Arrays.asList(
-                "z07",
-                "vmv",
-
-                "z20",
-                "kfm",
-
-                "z28",
-                "hnv",
-
-                "hth",
-                "tqr"
-        );
-        return list.stream().sorted().collect(Collectors.joining(","));
+        return wrongTargets.stream().sorted().collect(Collectors.joining(","));
     }
 
-    private Set<Apply> swapOutputs(Set<Apply> rules, String out1, String out2) {
+    @NotNull
+    private static Set<String> findWrongTargets(Set<Rule> rules, long zTargetCounts) {
+        // all Z outputs come from XOR
+        // OR inputs are ANDs
+        // no two AND in a row
+        // no three XOR in a row - the middle one is invalid
+        // single OR joins partial adders
+        Set<String> wrongTargets = new HashSet<>();
+        rules.forEach(rule -> {
+            // last z can be OR
+            if (rule.target.startsWith("z") && !rule.target().equals("z" + (zTargetCounts - 1))) {
+                if (rule.type != Type.XOR) {
+//                    System.out.println(rule + " has wrong target");
+                    wrongTargets.add(rule.target());
+                }
+            }
+            if (rule.type == Type.OR) {
+                Rule ruleA = rules.stream().filter(r -> r.target().equals(rule.a)).findFirst().get();
+                Rule ruleB = rules.stream().filter(r -> r.target().equals(rule.b)).findFirst().get();
+                if (ruleA.type != Type.AND) {
+//                    System.out.println(ruleA + " has wrong target");
+                    wrongTargets.add(ruleA.target());
+                }
+                if (ruleB.type != Type.AND) {
+//                    System.out.println(ruleB + " has wrong target");
+                    wrongTargets.add(ruleB.target());
+                }
+            }
+            if (rule.type == Type.AND) {
+                // first AND can be used twice
+                rules.stream().filter(r -> r.target().equals(rule.a)).findFirst().ifPresent(r -> {
+                    if (r.type == Type.AND && !r.a.endsWith("00") && !r.b.endsWith("00")) {
+//                        System.out.println(r + " has wrong target");
+                        wrongTargets.add(r.target());
+                    }
+                });
+                rules.stream().filter(r -> r.target().equals(rule.b)).findFirst().ifPresent(r -> {
+                    if (r.type == Type.AND && !r.a.endsWith("00") && !r.b.endsWith("00")) {
+//                        System.out.println(r + " has wrong target");
+                        wrongTargets.add(r.target());
+                    }
+                });
+            }
+            if (rule.type == Type.XOR) {
+                rules.stream().filter(r -> r.target().equals(rule.a)).findFirst().ifPresent(r -> {
+                    if (r.type == Type.XOR) {
+                        rules.stream().filter(ra -> ra.target().equals(r.a)).findFirst().ifPresent(ra -> {
+//                            System.out.println(r + " has wrong target");
+                            wrongTargets.add(r.target());
+                        });
+                        rules.stream().filter(ra -> ra.target().equals(r.b)).findFirst().ifPresent(ra -> {
+//                            System.out.println(r + " has wrong target");
+                            wrongTargets.add(r.target());
+                        });
+                    }
+                });
+                rules.stream().filter(r -> r.target().equals(rule.b)).findFirst().ifPresent(r -> {
+                    if (r.type == Type.XOR) {
+                        rules.stream().filter(ra -> ra.target().equals(r.a)).findFirst().ifPresent(ra -> {
+//                            System.out.println(r + " has wrong target");
+                            wrongTargets.add(r.target());
+                        });
+                        rules.stream().filter(ra -> ra.target().equals(r.b)).findFirst().ifPresent(ra -> {
+//                            System.out.println(r + " has wrong target");
+                            wrongTargets.add(r.target());
+                        });
+                    }
+                });
+            }
+        });
+        return wrongTargets;
+    }
+
+    private Map<String, Boolean> generateInput(long x, long y, long length) {
+        String xBinary = new StringBuilder(Long.toBinaryString(x)).reverse().toString();
+        String yBinary = new StringBuilder(Long.toBinaryString(y)).reverse().toString();
+        Map<String, Boolean> result = new HashMap<>();
+        for (int i = 0; i < length; i++) {
+            result.put("x%02d".formatted(i), xBinary.length() > i && xBinary.charAt(i) == '1');
+            result.put("y%02d".formatted(i), yBinary.length() > i && xBinary.charAt(i) == '1');
+        }
+        return result;
+    }
+
+    private Set<Rule> swapOutputs(Set<Rule> rules, String out1, String out2) {
         return rules.stream().map(r -> r.target().equals(out1) ? r.changeTarget(out2) : (r.target().equals(out2) ? r.changeTarget(out1) : r)).collect(Collectors.toSet());
     }
 
@@ -195,10 +226,10 @@ class Day24 implements Day {
         System.out.println(Long.toBinaryString(z));
     }
 
-    private static void calculate(Set<Apply> rules, Map<String, Boolean> inputs) {
+    private static void calculate(Set<Rule> rules, Map<String, Boolean> inputs) {
         while (true) {
 //            System.out.println(rules);
-            if (rules.stream().noneMatch(apply -> apply.calculate(inputs))) {
+            if (rules.stream().noneMatch(rule -> rule.calculate(inputs))) {
 //                System.out.println(inputs);
 //                System.out.println("Breaking");
                 break;
