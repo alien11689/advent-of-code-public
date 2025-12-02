1104a273c9dc6cfe84d377fa14949df5db0ce122
Dominik Przybysz <alien11689@gmail.com>
2025-12-02 19:39:43 +0100

    Optimize 2025/02

--- 2025/src/main/java/dpr/aoc2025/Day02.java
+++ 2025/src/main/java/dpr/aoc2025/Day02.java
@@ -1,6 +1,5 @@
 package dpr.aoc2025;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -22,55 +21,69 @@ class Day02 implements Day {
     }
 
     long part1(List<String> lines) {
-        return Arrays.stream(lines.get(0).split(","))
-                .map(p -> p.split("-"))
-                .mapToLong(p -> checkRange1(Long.parseLong(p[0]), Long.parseLong(p[1])))
-                .sum();
+        long sum = 0L;
+        for (String p : lines.get(0).split(",")) {
+            String[] split = p.split("-");
+            sum += checkRange1(split[0], split[1]);
+        }
+        return sum;
     }
 
-    private long checkRange1(long left, long right) {
+    private long checkRange1(String leftString, String rightString) {
+        long left = Long.parseLong(leftString);
+        long right = Long.parseLong(rightString);
+        int leftBeginLength = leftString.length() / 2;
+        var minBegin = leftBeginLength < 1 ? 1L : Long.parseLong(leftString.substring(0, leftBeginLength));
         long sum = 0;
-        long cur = left;
-        while (cur <= right) {
-            String s = String.valueOf(cur);
-            if (s.length() % 2 == 0) {
-                var begin = s.substring(0, s.length() / 2);
-                var end = s.substring(s.length() / 2);
-                if (begin.equals(end)) {
-                    sum += cur;
-                }
+        while (true) {
+            long l = Long.parseLong(minBegin + "" + minBegin);
+            if (l > right) {
+                break;
             }
-            ++cur;
+            if (l >= left) {
+                sum += l;
+            }
+            minBegin++;
         }
         return sum;
     }
 
     long part2(List<String> lines) {
-        return Arrays.stream(lines.get(0).split(","))
-                .map(p -> p.split("-"))
-                .mapToLong(p -> checkRange2(Long.parseLong(p[0]), Long.parseLong(p[1])))
-                .sum();
+        long sum = 0L;
+        for (String p : lines.get(0).split(",")) {
+            String[] split = p.split("-");
+            sum += checkRange2(Long.parseLong(split[0]), Long.parseLong(split[1]));
+        }
+        return sum;
     }
 
     private long checkRange2(long left, long right) {
         long sum = 0;
         long cur = left;
-        Set<String> found = new HashSet<>();
+        Set<Long> found = new HashSet<>();
+        Set<String> checkedPrefix = new HashSet<>();
+        int minDigits = String.valueOf(left).length();
+        int maxDigits = String.valueOf(right).length();
         while (cur <= right) {
             String s = String.valueOf(cur);
-            int maxDigits = s.length();
             int substringSize = 1;
-            while (substringSize <= maxDigits / 2) {
+            while (substringSize * 2 <= maxDigits) {
                 var begin = s.substring(0, substringSize);
-                StringBuilder sb = new StringBuilder(begin);
-                while (sb.length() + substringSize <= maxDigits) {
-                    sb.append(begin);
+                if (checkedPrefix.contains(begin)) {
+                    ++substringSize;
+                    continue;
                 }
-                if (sb.toString().equals(s)) {
-                    if(!found.contains(s)) {
-                        found.add(s);
-                        sum += cur;
+                checkedPrefix.add(begin);
+                StringBuilder sb = new StringBuilder(begin);
+                while (sb.length() <= maxDigits) {
+                    if (sb.length() >= minDigits && sb.length() > 1) {
+                        long l = Long.parseLong(sb.toString());
+                        if (l >= left && l <= right && !found.contains(l)) {
+                            found.add(l);
+                            sum += l;
+                        }
                     }
+                    sb.append(begin);
                 }
                 ++substringSize;
             }
