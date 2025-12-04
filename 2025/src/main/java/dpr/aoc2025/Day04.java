8c0c2eb7c1f1dab19c26c67ab17f474535df7843
Dominik Przybysz <alien11689@gmail.com>
2025-12-04 06:38:39 +0100

    Solve 2025/04

--- 2025/src/main/java/dpr/aoc2025/Day04.java
+++ 2025/src/main/java/dpr/aoc2025/Day04.java
@@ -1,8 +1,13 @@
 package dpr.aoc2025;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import org.jetbrains.annotations.NotNull;
 
 import dpr.commons.Day;
+import dpr.commons.Point2D;
 import dpr.commons.Util;
 
 class Day04 implements Day {
@@ -19,11 +24,47 @@ class Day04 implements Day {
     }
 
     int part1(List<String> lines) {
-        return 0;
+        var points = readBoard(lines);
+        return getRollsToRemove(points).size();
     }
 
     int part2(List<String> lines) {
-        return 0;
+        var points = readBoard(lines);
+        int result = 0;
+        while (true) {
+            var toRemove = getRollsToRemove(points);
+            result += toRemove.size();
+            points.removeAll(toRemove);
+            if (toRemove.isEmpty()) {
+                break;
+            }
+        }
+        return result;
+    }
+
+    @NotNull
+    private static Set<Point2D> getRollsToRemove(Set<Point2D> points) {
+        Set<Point2D> toRemove = new HashSet<>();
+        for (Point2D cur : points) {
+            long count = cur.adjacentPoints().stream().filter(points::contains).count();
+            if (count < 4) {
+                toRemove.add(cur);
+            }
+        }
+        return toRemove;
+    }
+
+    @NotNull
+    private static Set<Point2D> readBoard(List<String> lines) {
+        Set<Point2D> points = new HashSet<>();
+        for (int i = 0; i < lines.size(); i++) {
+            for (int j = 0; j < lines.get(i).length(); j++) {
+                if (lines.get(i).charAt(j) == '@') {
+                    points.add(new Point2D(i, j));
+                }
+            }
+        }
+        return points;
     }
 
 
--- 2025/src/test/java/dpr/aoc2025/Day04Test.java
+++ 2025/src/test/java/dpr/aoc2025/Day04Test.java
@@ -13,13 +13,13 @@ class Day04Test {
     @Test
     void part1() {
         var lines = Util.getNotEmptyLinesFromFile(day.dayNum(), "test1.txt");
-        assertEquals(0, day.part1(lines));
+        assertEquals(13, day.part1(lines));
     }
 
     @Test
     void part2() {
         var lines = Util.getNotEmptyLinesFromFile(day.dayNum(), "test1.txt");
-        assertEquals(0, day.part2(lines));
+        assertEquals(43, day.part2(lines));
     }
 
 }
