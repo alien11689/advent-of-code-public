19f937d3ba87f74b7ad95ae283e3a54636d14ae9
Dominik Przybysz <alien11689@gmail.com>
2025-12-25 20:25:20 +0100

    Add test for 2018/03

---
+++ 2018/src/test/java/dpr/aoc2018/Day03Test.java
@@ -0,0 +1,23 @@
+package dpr.aoc2018;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import dpr.commons.Util;
+
+class Day03Test {
+    @Test
+    void testPart1() {
+        var input = Util.getNotEmptyLinesFromFile("/03/test1.txt");
+        var result = Day03.part1(input);
+        assertEquals(4, result);
+    }
+
+    @Test
+    void testPart2() {
+        var input = Util.getNotEmptyLinesFromFile("/03/test1.txt");
+        var result = Day03.part2(input);
+        assertEquals("3", result);
+    }
+}
