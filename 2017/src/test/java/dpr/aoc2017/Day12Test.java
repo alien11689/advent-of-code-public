03aa19f2880f402150cd7fc58c11f611c603cad4
Dominik Przybysz <alien11689@gmail.com>
2025-12-25 11:48:41 +0100

    Add test for 2017/12

---
+++ 2017/src/test/java/dpr/aoc2017/Day12Test.java
@@ -0,0 +1,23 @@
+package dpr.aoc2017;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import dpr.commons.Util;
+
+class Day12Test {
+    @Test
+    void testPart1() {
+        var input = Util.getNotEmptyLinesFromFile("/12/test1.txt");
+        var result = Day12.part1(input);
+        assertEquals(6, result);
+    }
+
+    @Test
+    void testPart2() {
+        var input = Util.getNotEmptyLinesFromFile("/12/test1.txt");
+        var result = Day12.part2(input);
+        assertEquals(2, result);
+    }
+}
