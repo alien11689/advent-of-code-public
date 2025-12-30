5f79dbc8c637bb97ce40c2ceda3da8cf47d20944
Dominik Przybysz <alien11689@gmail.com>
2025-12-30 22:03:36 +0100

    Add test for 2019/09

--- 2019/src/test/java/dpr/aoc2019/intcode/IntCodeComputerTest.java
+++ 2019/src/test/java/dpr/aoc2019/intcode/IntCodeComputerTest.java
@@ -14,6 +14,7 @@ public class IntCodeComputerTest {
 
     @ParameterizedTest
     @MethodSource("day5Parameters")
+    @MethodSource("day9Parameters")
     void testRun(String program, Collection<Long> input, List<Long> expected) {
         var output = IntCodeComputer.run(program, input).stream().toList();
         assertEquals(expected, output);
@@ -48,6 +49,15 @@ public class IntCodeComputerTest {
                 Arguments.of("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31," +
                         "1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104," +
                         "999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", List.of(9L), List.of(1001L))
+        );
+    }
+
+    private static Stream<Arguments> day9Parameters() {
+        return Stream.of(
+                Arguments.of("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99", List.of(),
+                        List.of(109L, 1L, 204L, -1L, 1001L, 100L, 1L, 100L, 1008L, 100L, 16L, 101L, 1006L, 101L, 0L, 99L)),
+                Arguments.of("1102,34915192,34915192,7,4,7,99,0", List.of(), List.of(1219070632396864L)),
+                Arguments.of("104,1125899906842624,99", List.of(), List.of(1125899906842624L))
         );
     }
 }
