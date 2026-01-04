27fc3988c64c978b707f3bc22a82d69236e144fe
Dominik Przybysz <alien11689@gmail.com>
2026-01-04 15:31:23 +0100

    Add test for 2021/05

---
+++ 2021/src/test/java/dpr/aoc2021/Day05Test.java
@@ -0,0 +1,23 @@
+package dpr.aoc2021;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import dpr.commons.Util;
+
+class Day05Test {
+    @Test
+    void testPart1() {
+        var input = Util.getNotEmptyLinesFromFile("/05/test1.txt");
+        var result = Day05.part1(input);
+        assertEquals(5, result);
+    }
+
+    @Test
+    void testPart2() {
+        var input = Util.getNotEmptyLinesFromFile("/05/test1.txt");
+        var result = Day05.part2(input);
+        assertEquals(12, result);
+    }
+}
