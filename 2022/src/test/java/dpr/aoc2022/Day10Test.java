ad53101ac629fa1c7018b3a51fb40dfe8991fbba
Dominik Przybysz <alien11689@gmail.com>
2026-01-05 22:15:52 +0100

    Add test for 2022/10

---
+++ 2022/src/test/java/dpr/aoc2022/Day10Test.java
@@ -0,0 +1,29 @@
+package dpr.aoc2022;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import dpr.commons.Util;
+
+class Day10Test {
+    @Test
+    void testPart1() {
+        var input = Util.getNotEmptyLinesFromFile("/10/test1.txt");
+        var result = Day10.part1(input);
+        assertEquals(13140, result);
+    }
+
+    @Test
+    void testPart2() {
+        var input = Util.getNotEmptyLinesFromFile("/10/test1.txt");
+        var result = Day10.part2(input);
+        assertEquals("""
+                ##..##..##..##..##..##..##..##..##..##..
+                ###...###...###...###...###...###...###.
+                ####....####....####....####....####....
+                #####.....#####.....#####.....#####.....
+                ######......######......######......####
+                #######.......#######.......#######""", result.replaceAll(" ", "."));
+    }
+}
